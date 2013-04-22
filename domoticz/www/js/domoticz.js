8c8ed4db25f21f2bdfea6725df2202468fa59859
gizmocuz <gizmocuz@f5da7d14-956e-45c6-856f-fe31d67bfda7>
2013-04-22 06:23:45 +0000

    - Fixed: Sensor Timeout status update in web interface

--- domoticz/www/js/domoticz.js
+++ domoticz/www/js/domoticz.js
@@ -833,3 +833,15 @@ function generate_noty(ntype, ntext, ntimeout) {
 		theme: 'defaultTheme'
 	});
 }
+
+function rgb2hex(rgb) {
+	if (  rgb.search("rgb") == -1 ) {
+			return rgb;
+	} else {
+			rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/);
+			function hex(x) {
+					 return ("0" + parseInt(x).toString(16)).slice(-2);
+			}
+			return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+	}
+}
