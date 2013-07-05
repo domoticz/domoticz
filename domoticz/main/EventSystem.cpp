07def178e303e7c412c014d5e7572432b3685ee6
copycat73 <copycat73@f5da7d14-956e-45c6-856f-fe31d67bfda7>
2013-07-05 12:19:43 +0000

    Eventsystem preparations for lua link

--- domoticz/main/EventSystem.cpp
+++ domoticz/main/EventSystem.cpp
@@ -3,8 +3,12 @@
 #include "mainworker.h"
 #include "localtime_r.h"
 #include "Logger.h"
+#include <iostream>
+#include <ctime>
+#include <boost/algorithm/string.hpp>
 
 extern "C" {
+#include "../lua/src/lua.h"    
 #include "../lua/src/lualib.h"
 #include "../lua/src/lauxlib.h"
 }
@@ -55,33 +59,29 @@ void CEventSystem::LoadEvents()
 {
 	boost::lock_guard<boost::mutex> l(eventMutex);
 	m_events.clear();
-/*
+
 	std::stringstream szQuery;
 	std::vector<std::vector<std::string> > result;
-	szQuery << "SELECT Var1,Var2 FROM Database ORDER BY ID";
+	szQuery << "SELECT ID,Name,Conditions,Actions FROM Events ORDER BY ID";
 	result=m_pMain->m_sql.query(szQuery.str());
 	if (result.size()>0)
 	{
+		
 		std::vector<std::vector<std::string> >::const_iterator itt;
 		for (itt=result.begin(); itt!=result.end(); ++itt)
 		{
 			std::vector<std::string> sd=*itt;
-		}
+            
+			_tEventItem eitem;
+			std::stringstream s_str( sd[0] );
+			s_str >> eitem.ID;
+            eitem.Name		= sd[1];
+            eitem.Conditions	= sd[2];
+			eitem.Actions		= sd[3];
+            m_events.push_back(eitem);
+        }
+        _log.Log(LOG_NORM,"Events (re)loaded");
 	}
-*/
-
-	//test
-	_tEventItem eitem;
-	eitem.ID=1234;
-	eitem.szRules="IfThenElse";
-
-	m_events.push_back(eitem);
-
-	//test2
-	eitem.ID=5678;
-	eitem.szRules="If>Or<Set";
-
-	m_events.push_back(eitem);
 }
 
 void CEventSystem::Do_Work()
@@ -105,26 +105,69 @@ void CEventSystem::Do_Work()
 
 }
 
+
 void CEventSystem::ProcessMinute()
 {
-	boost::lock_guard<boost::mutex> l(eventMutex);
+    
+	EvaluateEvent("Time", 0);
 
-	std::vector<_tEventItem>::iterator itt;
-	for (itt=m_events.begin(); itt!=m_events.end(); ++itt)
-	{
-		if (itt->ID==1234)
-		{
-
-		}
-	}
 }
+
+void CEventSystem::GetCurrentStates()
+{
+    
+    boost::lock_guard<boost::mutex> l(deviceStateMutex);
+    m_devicestates.clear();
+    
+	std::stringstream szQuery;
+	std::vector<std::vector<std::string> > result;
+	szQuery << "SELECT ID,nValue,sValue FROM DeviceStatus WHERE (Used = '1') ORDER BY ID DESC";
+	result=m_pMain->m_sql.query(szQuery.str());
+	if (result.size()>0)
+	{
+		std::vector<std::vector<std::string> >::const_iterator itt;
+		for (itt=result.begin(); itt!=result.end(); ++itt)
+		{
+            std::vector<std::string> sd=*itt;
+            _tDeviceStatus sitem;
+			std::stringstream s_str( sd[0] );
+			s_str >> sitem.ID;
+            std::stringstream nv_str( sd[1] );
+			nv_str >> sitem.nValue;
+            sitem.sValue	= sd[2];
+            m_devicestates.push_back(sitem);
+        }
+        //_log.Log(LOG_NORM,"Events: device states (re)loaded");
+	}
+    
+    
+}
+
 
 bool CEventSystem::ProcessDevice(const int HardwareID, const unsigned long long ulDevID, const unsigned char unit, const unsigned char devType, const unsigned char subType, const unsigned char signallevel, const unsigned char batterylevel, const int nValue, const char* sValue, const std::string devname)
 {
-	boost::lock_guard<boost::mutex> l(eventMutex);
-
-	//do something
-
+    EvaluateEvent("Device", ulDevID);
+    
 	return true;
+}
+
+void CEventSystem::EvaluateEvent(const std::string reason, const unsigned long long DeviceID)
+{
+
+    time_t theTime = time(NULL);
+    aTime = localtime(&theTime);
+    char timeBuffer[100];
+
+    sprintf(timeBuffer, "%d:%d", aTime->tm_hour, aTime->tm_min);
+    
+    if (reason=="Time") {_log.Log(LOG_NORM,"EventSystem %s trigger: %s",reason.c_str(),timeBuffer);};
+    if (reason=="Device") {_log.Log(LOG_NORM,"EventSystem %s trigger: deviceno: %d",reason.c_str(),DeviceID);};
+   
+    GetCurrentStates();
+    
+    // pass conditions and actions over to lua here
+    // trigger custom lua scripts here
+    
+
 }
 
--- domoticz/main/EventSystem.h
+++ domoticz/main/EventSystem.h
@@ -12,8 +12,21 @@ class CEventSystem
 	struct _tEventItem
 	{
 		unsigned long long ID;
-		std::string szRules;
+        std::string Name;
+        std::string Conditions;
+        std::string Actions;
+        
 	};
+
+	struct _tDeviceStatus
+	{
+		unsigned long long ID;
+        unsigned long long nValue;
+        std::string sValue;
+        
+	};
+
+
 public:
 	CEventSystem(void);
 	~CEventSystem(void);
@@ -27,14 +40,18 @@ private:
 	lua_State	*m_pLUA;
 	MainWorker *m_pMain;
 	boost::mutex eventMutex;
+    boost::mutex deviceStateMutex;
 	volatile bool m_stoprequested;
 	boost::shared_ptr<boost::thread> m_thread;
 	unsigned char m_secondcounter;
-
-	//our thread
+    struct tm *aTime;
+	
+    //our thread
 	void Do_Work();
 	void ProcessMinute();
-
+    void GetCurrentStates();
+    void EvaluateEvent(const std::string reason, const unsigned long long DeviceID);
 	std::vector<_tEventItem> m_events;
+    std::vector<_tDeviceStatus> m_devicestates;
 };
 
