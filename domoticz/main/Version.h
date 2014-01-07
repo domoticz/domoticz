6cdfe31a424f3dd58eb99636e0b3f1d6b5610474
copycat73 <copycat73@f5da7d14-956e-45c6-856f-fe31d67bfda7>
2014-01-07 07:34:34 +0000

    Implemented: Windows executable versioning based on svn build

---
+++ domoticz/main/Version.h
@@ -0,0 +1,33 @@
+#include "../svnversion.h"
+ 
+#define STRINGIZE2(s) #s
+#define STRINGIZE(s) STRINGIZE2(s)
+ 
+#define VERSION_MAJOR               1
+#define VERSION_MINOR               0
+#define VERSION_REVISION            0
+#define VERSION_BUILD               SVNVERSION
+ 
+#define VER_FILE_DESCRIPTION_STR    "Domoticz binary"
+#define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
+#define VER_FILE_VERSION_STR        STRINGIZE(VERSION_MAJOR)        \
+                                    "." STRINGIZE(VERSION_MINOR)    \
+                                    "." STRINGIZE(VERSION_REVISION) \
+                                    "." STRINGIZE(VERSION_BUILD)    \
+ 
+#define VER_PRODUCTNAME_STR         "Domoticz"
+#define VER_PRODUCT_VERSION         VER_FILE_VERSION
+#define VER_PRODUCT_VERSION_STR     VER_FILE_VERSION_STR
+#define VER_ORIGINAL_FILENAME_STR   VER_PRODUCTNAME_STR ".exe"
+#define VER_INTERNAL_NAME_STR       VER_ORIGINAL_FILENAME_STR
+#define VER_COPYRIGHT_STR           "Copyright (C) 2011"
+ 
+#ifdef _DEBUG
+  #define VER_VER_DEBUG             VS_FF_DEBUG
+#else
+  #define VER_VER_DEBUG             0
+#endif
+ 
+#define VER_FILEOS                  VOS_NT_WINDOWS32
+#define VER_FILEFLAGS               VER_VER_DEBUG
+#define VER_FILETYPE                VFT_APP
