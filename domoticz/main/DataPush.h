9767bdc67ae6bd8518a0a30231fe4f6fd71b5c20
copycat73 <copycat73@f5da7d14-956e-45c6-856f-fe31d67bfda7>
2014-04-27 15:26:34 +0000

    Fixed: removed redundant qualification on datapush method

--- domoticz/main/DataPush.h
+++ domoticz/main/DataPush.h
@@ -7,7 +7,7 @@ public:
 	void SetMainWorker(MainWorker *pMainWorker);
 	void DoWork(const unsigned long long DeviceRowIdxIn);
 	std::vector<std::string> DropdownOptions(const unsigned long long DeviceRowIdxIn);
-	std::string CDataPush::DropdownOptionsValue(const unsigned long long DeviceRowIdxIn,int pos);
+	std::string DropdownOptionsValue(const unsigned long long DeviceRowIdxIn,int pos);
 
 private:
 	unsigned long long DeviceRowIdx;
