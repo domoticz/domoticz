c3637706a5540f28981db5013b1924ee8cef6081
gizmocuz <gizmocuz@f5da7d14-956e-45c6-856f-fe31d67bfda7>
2014-10-09 08:05:42 +0000

    Fixed: Setting default security network key for openzwave

--- domoticz/main/SQLHelper.cpp
+++ domoticz/main/SQLHelper.cpp
@@ -1217,6 +1217,10 @@ bool CSQLHelper::OpenDatabase()
 	{
 		UpdatePreferencesVar("ZWaveEnableDebug", 0);
 	}
+	if (!GetPreferencesVar("ZWaveNetworkKey", sValue))
+	{
+		UpdatePreferencesVar("ZWaveNetworkKey", "0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10");
+	}
 	if (!GetPreferencesVar("BatteryLowNotification", nValue))
 	{
 		UpdatePreferencesVar("BatteryLowNotification", 0); //default disabled
--- domoticz/main/Version.h
+++ domoticz/main/Version.h
@@ -3,7 +3,7 @@
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
  
-#define VERSION_MAJOR               1
+#define VERSION_MAJOR               2
 #define VERSION_MINOR               0
 #define VERSION_REVISION            0
 #define VERSION_BUILD               SVNVERSION
