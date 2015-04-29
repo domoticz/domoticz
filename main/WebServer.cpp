b4d8234880eef2f9b74feb6206f9b6a70c3ddd33
gizmocuz <gizmocuz@users.noreply.github.com>
2015-04-29 15:31:11 +0000

    better fix for wind graph calculation

--- main/WebServer.cpp
+++ main/WebServer.cpp
@@ -14849,31 +14849,37 @@ namespace http {
 							totalvalues++;
 						}
 
-						if (totalvalues>0)
+						totalvalues = 0;
+						for (int jj = 0; jj < 7; jj++)
 						{
-							for (int jj = 0; jj < 7; jj++)
-							{
-								root["result_speed"][jj]["label"] = szLegendLabels[jj];
+							root["result_speed"][jj]["label"] = szLegendLabels[jj];
 
-								for (ii = 0; ii < 16; ii++)
-								{
-									float svalue = (100.0f / totalvalues)*wdirtabletemp[ii][jj];
-									wdirtable[jj][ii] = svalue;
-									sprintf(szTmp, "%.2f", svalue);
-									root["result_speed"][jj]["sp"][ii] = szTmp;
-								}
-							}
-							ii = 0;
-							for (idir = 0; idir < 360 + 1; idir++)
+							for (ii = 0; ii < 16; ii++)
 							{
-								if (_directions[idir] != 0)
+								float svalue = 0;
+								if (totalvalues > 0)
 								{
-									root["result"][ii]["dig"] = idir;
-									float percentage = (float(100.0 / float(totalvalues))*float(_directions[idir]));
-									sprintf(szTmp, "%.2f", percentage);
-									root["result"][ii]["div"] = szTmp;
-									ii++;
+									svalue = (100.0f / totalvalues)*wdirtabletemp[ii][jj];
 								}
+								wdirtable[jj][ii] = svalue;
+								sprintf(szTmp, "%.2f", svalue);
+								root["result_speed"][jj]["sp"][ii] = szTmp;
+							}
+						}
+						ii = 0;
+						for (idir = 0; idir < 360 + 1; idir++)
+						{
+							if (_directions[idir] != 0)
+							{
+								root["result"][ii]["dig"] = idir;
+								float percentage = 0;
+								if (totalvalues > 0)
+								{
+									percentage = (float(100.0 / float(totalvalues))*float(_directions[idir]));
+								}
+								sprintf(szTmp, "%.2f", percentage);
+								root["result"][ii]["div"] = szTmp;
+								ii++;
 							}
 						}
 					}
