7897ebd6415f7711e6c08bd1b86aa7e49d75f344
gizmocuz <gizmocuz@f5da7d14-956e-45c6-856f-fe31d67bfda7>
2015-05-15 06:46:40 +0000

    Possible fix for OTGW outside temperature

--- trunk/hardware/OTGWBase.cpp
+++ trunk/hardware/OTGWBase.cpp
@@ -359,7 +359,7 @@ bool OTGWBase::GetOutsideTemperatureFromDomoticz(float &tvalue)
 	Json::Value tempjson;
 	std::stringstream sstr;
 	sstr << m_OutsideTemperatureIdx;
-	m_webserver.GetJSonDevices(tempjson, "", "temp","ID",sstr.str(),"","",true,0);
+	m_webserver.GetJSonDevices(tempjson, "", "temp","ID",sstr.str(),"","",true,0,true);
 
 	size_t tsize=tempjson.size();
 	if (tsize<1)
