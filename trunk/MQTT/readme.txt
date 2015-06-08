e1a1e97bd6c298e7c54a259da64c1625ba791173
gizmocuz <gizmocuz@f5da7d14-956e-45c6-856f-fe31d67bfda7>
2015-06-08 16:52:34 +0000

    new MQTT library

---
+++ trunk/MQTT/readme.txt
@@ -0,0 +1,21 @@
+Mosquitto
+=========
+
+Mosquitto is an open source implementation of a server for version 3.1 and
+3.1.1 of the MQTT protocol.
+
+See the following links for more information on MQTT:
+
+http://mqtt.org/
+http://www.ibm.com/developerworks/webservices/library/ws-mqtt/index.html
+
+Mosquitto project information is available at the following locations:
+
+http://mosquitto.org/ (main homepage)
+https://bugs.eclipse.org/bugs/buglist.cgi?product=Mosquitto (Find bugs)
+https://bugs.eclipse.org/bugs/enter_bug.cgi?product=Mosquitto (Submit bugs)
+http://git.eclipse.org/c/mosquitto/org.eclipse.mosquitto.git/ (source code repository, web interface
+
+There is also a public test server available at http://test.mosquitto.org/
+
+Mosquitto was written by Roger Light <roger@atchoo.org>
--- trunk/hardware/MQTT.h
+++ trunk/hardware/MQTT.h
@@ -1,18 +1,23 @@
 #pragma once
 
 #include "MySensorsBase.h"
+#include "../MQTT/mosquittopp.h"
 
-class MQTT : public MySensorsBase
+class MQTT : public MySensorsBase, mosqpp::mosquittopp
 {
 public:
 	MQTT(const int ID, const std::string IPAddress, const unsigned short usIPPort);
 	~MQTT(void);
 	bool isConnected(){ return m_IsConnected; };
-	void OnConnect();
+
+	void on_connect(int rc);
+	void on_disconnect(int rc);
+	void on_message(const struct mosquitto_message *message);
+	void on_subscribe(int mid, int qos_count, const int *granted_qos);
+
 	void OnMQTTMessage(char *topicName, int topicLen,  void *pMessage);
 	void SendMessage(const std::string &Topic, const std::string &Message);
 
-	bool m_bDoRestart;
 	bool m_bDoReconnect;
 	bool m_disc_finished;
 	bool m_IsConnected;
@@ -30,14 +35,10 @@ private:
 protected:
 	std::string m_szIPAddress;
 	unsigned short m_usIPPort;
-
-	void *m_mqtt_client;
-
+	boost::mutex m_mqtt_mutex;
 	void StopMQTT();
-
 	void Do_Work();
 	void OnData(const unsigned char *pData, size_t length);
-
 	boost::shared_ptr<boost::thread> m_thread;
 	volatile bool m_stoprequested;
 	boost::signals2::connection m_sConnection;
