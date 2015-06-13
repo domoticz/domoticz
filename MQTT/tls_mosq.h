61894ac0c782aed0cc4398a44a8aa1c95048f09c
gizmocuz <gizmocuz@users.noreply.github.com>
2015-06-13 09:31:40 +0000

    added MQTT TLS build support

--- MQTT/tls_mosq.h
+++ MQTT/tls_mosq.h
@@ -17,7 +17,7 @@ Contributors:
 #ifndef _TLS_MOSQ_H_
 #define _TLS_MOSQ_H_
 
-#ifdef WITH_TLS
+#ifdef NS_ENABLE_SSL
 
 #include <openssl/ssl.h>
 #ifdef WITH_TLS_PSK
@@ -31,6 +31,6 @@ Contributors:
 int _mosquitto_server_certificate_verify(int preverify_ok, X509_STORE_CTX *ctx);
 int _mosquitto_verify_certificate_hostname(X509 *cert, const char *hostname);
 
-#endif /* WITH_TLS */
+#endif /* NS_ENABLE_SSL */
 
 #endif
