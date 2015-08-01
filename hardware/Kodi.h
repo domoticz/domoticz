1254e3d9e7f4312341272d50c8226cbabbfdd130
dnpwwo <dnpwwo@users.noreply.github.com>
2015-08-01 05:46:58 +0000

    Add Kodi command interface to web server

--- hardware/Kodi.h
+++ hardware/Kodi.h
@@ -19,9 +19,11 @@ class CKodi : public CDomoticzHardwareBase
 		time_t			LastOK;
 		_eMediaStatus	nStatus;
 		std::string		sStatus;
+//		boost::mutex	mutDevice;
 	};
 public:
 	CKodi(const int ID, const int PollIntervalsec, const int PingTimeoutms);
+	CKodi(const int ID);
 	~CKodi(void);
 	bool WriteToHardware(const char *pdata, const unsigned char length);
 	void AddNode(const std::string &Name, const std::string &IPAddress, const int Port);
@@ -30,6 +32,7 @@ public:
 	void RemoveAllNodes();
 	void SetSettings(const int PollIntervalsec, const int PingTimeoutms);
 	void Restart();
+	void SendCommand(const int ID, const std::string &command);
 private:
 	void Do_Work();
 
@@ -38,13 +41,13 @@ private:
 	Json::Value Query(std::string sIP, int iPort, std::string sQuery);
 	void Do_Node_Work(const KodiNode &Node);
 	void UpdateNodeStatus(const KodiNode &Node, const _eMediaStatus nStatus, const std::string sStatus, const bool bPingOK);
-
 	void ReloadNodes();
+
+	std::vector<KodiNode> m_nodes;
 
 	int m_iThreadsRunning;
 	int m_iPollInterval;
 	int m_iPingTimeoutms;
-	std::vector<KodiNode> m_nodes;
 	boost::shared_ptr<boost::thread> m_thread;
 	volatile bool m_stoprequested;
 	boost::mutex m_mutex;
