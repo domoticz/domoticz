6a0162d07c8bc5e6be044508b3b906c5ea54f39b
gizmocuz <gizmocuz@users.noreply.github.com>
2015-08-05 10:56:09 +0000

    now using escape/unescape in html/js fixed possible crash in rtrim/ltrim function fixed possible error in browser cache error message display

--- main/Helper.cpp
+++ main/Helper.cpp
@@ -215,13 +215,25 @@ float pressureSeaLevelFromAltitude(float altitude, float atmospheric, float temp
 
 std::string &stdstring_ltrim(std::string &s)
 {
-	s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+	while (!s.empty())
+	{
+		if (s[0] != ' ')
+			return s;
+		s = s.substr(1);
+	}
+	//	s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
 	return s;
 }
 
 std::string &stdstring_rtrim(std::string &s)
 {
-	s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+	while (!s.empty())
+	{
+		if (s[s.size() - 1] != ' ')
+			return s;
+		s = s.substr(0, s.size() - 1);
+	}
+	//s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
 	return s;
 }
 
