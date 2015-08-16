806fc2ac7ab797845ac430b5ce46a393c0e880a6
Rene Kluwen <rene@kluwen.com>
2015-08-16 19:11:28 +0200

    Other test.

--- webserver/proxyclient.cpp
+++ webserver/proxyclient.cpp
@@ -18,7 +18,7 @@ CProxyClient::CProxyClient(boost::asio::io_service& io_service, boost::asio::ssl
 
 void CProxyClient::Reconnect()
 {
-	_log.Log(LOG_NORM, "PROXY: Connecting....");
+	_log.Log(LOG_NORM, "PROXY: Connecting...");
 
 	std::string address = "my.domoticz.com";
 	std::string port = "9999";
