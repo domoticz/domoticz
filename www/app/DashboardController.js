0963eed27d5bf297aaf694590e8ce55a8060c648
gizmocuz <info@domoticz.com>
2015-08-24 20:34:52 +0200

    - Implemented: Icons for RFXMeter Gas/Water

--- www/app/DashboardController.js
+++ www/app/DashboardController.js
@@ -3044,11 +3044,24 @@ define(['app'], function (app) {
 						xhtm+='\t      <td id="img"><img src="images/';
 						var status="";
 						if (typeof item.Counter != 'undefined') {
-							if ((item.Type == "P1 Smart Meter")&&(item.SubType=="Energy")) {
-								xhtm+='counter.png" class="lcursor" onclick="ShowSmartLog(\'#dashcontent\',\'ShowFavorites\',' + item.idx + ',\'' + escape(item.Name) + '\', ' + item.SwitchTypeVal + ');" height="40" width="40"></td>\n';
+							if (item.Type == "RFXMeter") {
+								if (item.SwitchTypeVal==1) {
+									xhtm+='override.png" class="lcursor" onclick="ShowCounterLog(\'#dashcontent\',\'ShowFavorites\',' + item.idx + ',\'' + escape(item.Name) + '\', ' + item.SwitchTypeVal + ');" height="40" width="40"></td>\n';
+								}
+								else if (item.SwitchTypeVal==2) {
+									xhtm+='Water48_On.png" class="lcursor" onclick="ShowCounterLog(\'#dashcontent\',\'ShowFavorites\',' + item.idx + ',\'' + escape(item.Name) + '\', ' + item.SwitchTypeVal + ');" height="40" width="40"></td>\n';
+								}
+								else {
+									xhtm+='counter.png" class="lcursor" onclick="ShowCounterLog(\'#dashcontent\',\'ShowFavorites\',' + item.idx + ',\'' + escape(item.Name) + '\', ' + item.SwitchTypeVal + ');" height="40" width="40"></td>\n';
+								}
 							}
 							else {
-								xhtm+='counter.png" class="lcursor" onclick="ShowCounterLog(\'#dashcontent\',\'ShowFavorites\',' + item.idx + ',\'' + escape(item.Name) + '\', ' + item.SwitchTypeVal + ');" height="40" width="40"></td>\n';
+								if ((item.Type == "P1 Smart Meter")&&(item.SubType=="Energy")) {
+									xhtm+='counter.png" class="lcursor" onclick="ShowSmartLog(\'#dashcontent\',\'ShowFavorites\',' + item.idx + ',\'' + escape(item.Name) + '\', ' + item.SwitchTypeVal + ');" height="40" width="40"></td>\n';
+								}
+								else {
+									xhtm+='counter.png" class="lcursor" onclick="ShowCounterLog(\'#dashcontent\',\'ShowFavorites\',' + item.idx + ',\'' + escape(item.Name) + '\', ' + item.SwitchTypeVal + ');" height="40" width="40"></td>\n';
+								}
 							}
 							if ((item.SubType!="Gas")&&(item.SubType != "RFXMeter counter")) {
 								status='' + $.t("Usage") + ': ' + item.CounterToday;
