61b016f1ffc65d660e0590db2a5a974a9a803e3b
EddyK69 <github@kuijt-evers.net>
2015-08-27 14:06:58 +0200

    Added json functions for enable/disable (scene)timers

--- main/WebServer.h
+++ main/WebServer.h
@@ -147,10 +147,14 @@ private:
 	void Cmd_AddTimer(Json::Value &root);
 	void Cmd_UpdateTimer(Json::Value &root);
 	void Cmd_DeleteTimer(Json::Value &root);
+	void Cmd_EnableTimer(Json::Value & root);
+	void Cmd_DisableTimer(Json::Value & root);
 	void Cmd_ClearTimers(Json::Value &root);
 	void Cmd_AddSceneTimer(Json::Value &root);
 	void Cmd_UpdateSceneTimer(Json::Value &root);
 	void Cmd_DeleteSceneTimer(Json::Value &root);
+	void Cmd_EnableSceneTimer(Json::Value & root);
+	void Cmd_DisableSceneTimer(Json::Value & root);
 	void Cmd_ClearSceneTimers(Json::Value &root);
 	void Cmd_SetSceneCode(Json::Value &root);
 	void Cmd_RemoveSceneCode(Json::Value &root);
