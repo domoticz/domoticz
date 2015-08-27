fb528ed9549292a2f595ab5a325b0bc95a776781
EddyK69 <github@kuijt-evers.net>
2015-08-27 12:59:11 +0200

    Revert "Implemented: json enable/disable (scene)timers"
    This reverts commit 238f085ca818f8b8d0fbd6e8165c7300c5cadcce.

--- main/Scheduler.cpp
+++ main/Scheduler.cpp
@@ -798,46 +798,6 @@ namespace http {
 			m_mainworker.m_scheduler.ReloadSchedules();
 		}
 
-		void CWebServer::Cmd_EnableTimer(Json::Value &root)
-		{
-			if (m_pWebEm->m_actualuser_rights != 2)
-			{
-				//No admin user, and not allowed to be here
-				return;
-			}
-
-			std::string idx = m_pWebEm->FindValue("idx");
-			if (idx == "")
-				return;
-			root["status"] = "OK";
-			root["title"] = "EnableTimer";
-			m_sql.safe_query(
-				"UPDATE Timers SET Active=1 WHERE (ID == '%q')",
-				idx.c_str()
-				);
-			m_mainworker.m_scheduler.ReloadSchedules();
-		}
-
-		void CWebServer::Cmd_DisableTimer(Json::Value &root)
-		{
-			if (m_pWebEm->m_actualuser_rights != 2)
-			{
-				//No admin user, and not allowed to be here
-				return;
-			}
-
-			std::string idx = m_pWebEm->FindValue("idx");
-			if (idx == "")
-				return;
-			root["status"] = "OK";
-			root["title"] = "DisableTimer";
-			m_sql.safe_query(
-				"UPDATE Timers SET Active=0 WHERE (ID == '%q')",
-				idx.c_str()
-				);
-			m_mainworker.m_scheduler.ReloadSchedules();
-		}
-
 		void CWebServer::Cmd_ClearTimers(Json::Value &root)
 		{
 			if (m_pWebEm->m_actualuser_rights != 2)
@@ -1291,46 +1251,6 @@ namespace http {
 			root["title"] = "DeleteSceneTimer";
 			m_sql.safe_query(
 				"DELETE FROM SceneTimers WHERE (ID == '%q')",
-				idx.c_str()
-				);
-			m_mainworker.m_scheduler.ReloadSchedules();
-		}
-
-		void CWebServer::Cmd_EnableSceneTimer(Json::Value &root)
-		{
-			if (m_pWebEm->m_actualuser_rights != 2)
-			{
-				//No admin user, and not allowed to be here
-				return;
-			}
-
-			std::string idx = m_pWebEm->FindValue("idx");
-			if (idx == "")
-				return;
-			root["status"] = "OK";
-			root["title"] = "EnableSceneTimer";
-			m_sql.safe_query(
-				"UPDATE SceneTimers SET Active=1 WHERE (ID == '%q')",
-				idx.c_str()
-				);
-			m_mainworker.m_scheduler.ReloadSchedules();
-		}
-
-		void CWebServer::Cmd_DisableSceneTimer(Json::Value &root)
-		{
-			if (m_pWebEm->m_actualuser_rights != 2)
-			{
-				//No admin user, and not allowed to be here
-				return;
-			}
-
-			std::string idx = m_pWebEm->FindValue("idx");
-			if (idx == "")
-				return;
-			root["status"] = "OK";
-			root["title"] = "DisableSceneTimer";
-			m_sql.safe_query(
-				"UPDATE SceneTimers SET Active=0 WHERE (ID == '%q')",
 				idx.c_str()
 				);
 			m_mainworker.m_scheduler.ReloadSchedules();
