4ff736e7517d7ef4ea29d93f59fed85f66850e4c
Sébastien Bouchex Bellomié <sbouchex@gmail.com>
2015-11-19 17:23:34 +0100

    Fixed an issue with smtp client (Removed hostname, it's handled by curl)

--- smtpclient/SMTPClient.cpp
+++ smtpclient/SMTPClient.cpp
@@ -126,21 +126,9 @@ bool SMTPClient::SendEmail()
 		slist1 = curl_slist_append(slist1, (*itt).c_str());
 	}
 
-	// Get the hostname from the server
-	std::string hostname;
-	char hn[256];
-	if (gethostname(hn, sizeof(hn)) != SOCKET_ERROR) {
-		hostname = hn;
-		std::transform(hostname.begin(), hostname.end(), hostname.begin(), ::tolower);
-	}
-	else {
-		// No hostname found, default to a fixed one
-		hostname = "domoticz";
-	}
-
 	std::stringstream sstr;
-	sstr << "smtp://" << m_Server << ":" << m_Port << "/" << hostname;
-	std::string szURL=sstr.str();//"smtp://"+MailServer+"/domoticz";
+	sstr << "smtp://" << m_Server << ":" << m_Port;
+	std::string szURL=sstr.str();//"smtp://"+MailServer;
 
 	try
 	{
