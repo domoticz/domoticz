c47a9f5157e92fed277f036d0003a368b50cea63
Ryan Hunt <admin@nayr.net>
2016-01-12 23:22:42 -0700

    custom-icons: try tmp and exclude win32, cleaner

--- main/SQLHelper.cpp
+++ main/SQLHelper.cpp
@@ -6597,14 +6597,14 @@ bool CSQLHelper::InsertCustomIconFromZip(const std::string &szZip, std::string &
 	std::ofstream outfile;
 	outfile.open(outputfile.c_str(), std::ios::out | std::ios::binary | std::ios::trunc);
 	if (!outfile.is_open())
-	{
 #ifndef WIN32
+	{
 		//path might not be writable on linux, fall back to good old /tmp
 		outputfile = "/tmp/custom_icons.zip";
 		outfile.clear();
 		outfile.open(outputfile.c_str(), std::ios::out | std::ios::binary | std::ios::trunc);
-#endif
 		if (!outfile.is_open())
+#endif
 		{
 			ErrorMessage = "Error writing zip to disk";
 			return false;
