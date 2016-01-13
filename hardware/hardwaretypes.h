61fcb4725a278f1e28c32b20ce402e062ace7a24
Stuntteam <frankzirrone@gmail.com>
2016-01-13 13:03:23 +0100

    RFLink: additional manual devices

--- hardware/hardwaretypes.h
+++ hardware/hardwaretypes.h
@@ -126,9 +126,8 @@
 #define sSwitchTypeR546				0x40
 #define sSwitchTypeDiya				0x41
 #define sSwitchTypeX10secu			0x42
-#define sSwitchTypeX10secu			0x42
 #define sSwitchTypeAtlantic			0x43
-//#define sSwitchTypeSilvercrest		0x44 //Already defined above!?
+#define sSwitchTypeSilvercrestDB	0x44 
 #define sSwitchTypeMedionDB			0x45
 #define sSwitchTypeVMC				0x46
 
