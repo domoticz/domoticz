b88806f35bafab773b24253b349664b7f8ca2b23
gaudryc <chris.m.gaudry@gmail.com>
2016-02-08 16:06:24 +0100

    Fix Javascript syntax and format

--- www/app/LogoutController.js
+++ www/app/LogoutController.js
@@ -1,40 +1,38 @@
 define(['app'], function (app) {
-	app.controller('LogoutController', [ 'permissions', '$scope', '$rootScope', '$location', '$http', '$interval', function(permissions,$scope,$rootScope,$location,$http,$interval) {
+	app.controller('LogoutController', [ 'permissions', '$scope', '$rootScope', '$location', '$http', '$interval', function (permissions, $scope, $rootScope, $location, $http, $interval) {
 
-		init();
-
-		function init()
-		{
+		(function init() {
 			var permissionList = {
-					isloggedin: true,
-					rights: -1
+				isloggedin: true,
+				rights: -1
 			};
 			permissions.setPermissions(permissionList);
 			$.ajax({
-			 url: "json.htm?type=command&param=dologout",
-			 async: true, 
-			 dataType: 'json',
-			 success: function(data) {
-				$.ajax({
-				 url: "json.htm?type=command&param=getauth",
-				 async: false, 
-				 dataType: 'json',
-				 success: function(data) {
-					if (data.status == "OK") {
-						if (data.user!="") {
-							permissionList.isloggedin=true;
+				url: "json.htm?type=command&param=dologout",
+				async: true, 
+				dataType: 'json',
+				success: function (data) {
+					$.ajax({
+						url: "json.htm?type=command&param=getauth",
+						async: false, 
+						dataType: 'json',
+						success: function (data) {
+							if (data.status === "OK") {
+								if (data.user !== "") {
+									permissionList.isloggedin = true;
+								}
+								permissionList.rights = parseInt(data.rights, 10);
+							}
 						}
-						permissionList.rights=parseInt(data.rights);
-					}
-				 }
-				});
-				permissions.setPermissions(permissionList);
-				window.location = '#/Dashboard';
-			 },
-			 error: function(){
-				window.location = '#/Dashboard';
-			 }     
+					});
+					permissions.setPermissions(permissionList);
+					window.location = '#/Dashboard';
+				},
+				error: function () {
+					window.location = '#/Dashboard';
+				}
 			});
-		};
-	} ]);
+		})();
+
+	}]);
 });
