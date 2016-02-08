c6ddbe652e62e2852d06f39eb73b086c57079a71
Stuntteam <frankzirrone@gmail.com>
2016-02-08 09:50:41 +0100

    RFLink: Added new switch types

--- main/RFXNames.cpp
+++ main/RFXNames.cpp
@@ -770,6 +770,10 @@ const char *RFX_Type_SubType_Desc(const unsigned char dType, const unsigned char
 		{ pTypeGeneralSwitch, sSwitchTypeKeeloq, "Keeloq" },
 		{ pTypeGeneralSwitch, sSwitchCustomSwitch, "CustomSwitch" },
 		{ pTypeGeneralSwitch, sSwitchGeneralSwitch, "Switch" },
+		{ pTypeGeneralSwitch, sSwitchTypeKoch, "Koch" },
+		{ pTypeGeneralSwitch, sSwitchTypeKingpin, "Kingpin" },
+		{ pTypeGeneralSwitch, sSwitchTypeFunkbus, "Funkbus" },
+		{ pTypeGeneralSwitch, sSwitchTypeNice, "Nice" },
 		{  0,0,NULL }
 	};
 	return findTableID1ID2(Table, dType, sType);
