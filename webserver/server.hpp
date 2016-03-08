c8da7e1688931fdee520cb1df444ed018b2e4e9a
gaudryc <chris.m.gaudry@gmail.com>
2016-03-08 12:54:21 +0100

    cleanup

--- webserver/server.hpp
+++ webserver/server.hpp
@@ -33,9 +33,6 @@ public:
 	/// Stop the server.
 	void stop();
 
-	/// Check if the server is running
-	bool stopped();
-
 	/// Print server settings to string (debug purpose)
 	virtual std::string to_string() const {
 		return "'server_base[" + settings_.to_string() + "]'";
