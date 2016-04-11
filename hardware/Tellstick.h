aac7b0a45b17b9989a670158dcbdefa08f809784
Per Nilsson <perni@jcd.se>
2016-04-11 17:09:37 +0200

    Added basic sensor support for Temperature and Humidity sensors

--- hardware/Tellstick.h
+++ hardware/Tellstick.h
@@ -15,11 +15,20 @@ public:
 
 	void deviceEvent(int deviceId, int method, const char *data);
 	static void deviceEventCallback(int deviceId, int method, const char *data, int callbackId, void *context);
+
+	void sensorEvent(int deviceId, const char *protocol, const char *model, int dataType, const char *value);
+	static void sensorEventCallback(const char *protocol, const char *model, int id, int dataType, const char *value,
+					int timestamp, int callbackId, void *context);
+
 private:
 	bool AddSwitchIfNotExits(const int id, const char* devname, bool isDimmer);
 	void Init();
 	bool StartHardware();
 	bool StopHardware();
+
+	int deviceEventId;
+	int sensorEventId;
+
 };
 
 #endif //WITH_TELLSTICK
