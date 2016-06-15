c427a44b40aa7a0e6dff51d2aa13a3e2fa103a9d
gizmocuz <info@domoticz.com>
2016-06-15 07:52:37 +0200

    MQTT, Added 'getsceneinfo'

--- hardware/MQTT.h
+++ hardware/MQTT.h
@@ -39,6 +39,7 @@ private:
 	void WriteInt(const std::string &sendStr);
 	void ProcessMySensorsMessage(const std::string &MySensorsMessage);
 	void SendDeviceInfo(const int m_HwdID, const unsigned long long DeviceRowIdx, const std::string &DeviceName, const unsigned char *pRXCommand);
+	void SendSceneInfo(const unsigned long long SceneIdx, const std::string &SceneName);
 protected:
 	std::string m_szIPAddress;
 	unsigned short m_usIPPort;
