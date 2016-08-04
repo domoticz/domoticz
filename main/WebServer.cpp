baa3e8c0c4788fbb5fc917ff71faf980eca9e464
rkluwen <rene@kluwen.com>
2016-08-04 10:26:03 +0200

    Doesnt crash anymore upon stopping and webserver connections are still being stopped.

--- main/WebServer.cpp
+++ main/WebServer.cpp
@@ -601,7 +601,7 @@ namespace http {
 
 			//Start normal worker thread
 			m_bDoStop = false;
-			m_thread = boost::shared_ptr<boost::thread>(new boost::thread(boost::bind(&CWebServer::Do_Work, this)));
+			m_thread = boost::shared_ptr<boost::thread>(new boost::thread(boost::bind(&CWebServer::Do_Work, shared_from_this())));
 
 			return (m_thread != NULL);
 		}
