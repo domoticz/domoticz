f623f8534cd152117c0707743e253186ec5ef0ca
Danny Bloemendaal <danny@bloemeland.nl>
2016-08-14 16:15:11 +0200

    Transfering dzVents files to proper location.

---
+++ scripts/dzVents/runtime/examples/intruder alert.lua
@@ -0,0 +1,23 @@
+return {
+
+	active = true,
+
+	on = {
+		'PIR_*' -- all my motion detectors' name start with PIR_
+	},
+
+	execute = function(domoticz, detector)
+
+		if (detector.state == 'Motion' and domoticz.security ~= domoticz.SECURITY_DISARMED) then
+			-- o dear
+
+			domoticz.setScene('intruder alert', 'On')
+
+			-- send notification
+			domoticz.notify('Security breach', '',
+				domoticz.PRIORITY_EMERGENCY,
+				domoticz.SOUND_SIREN)
+		end
+
+	end
+}
---
+++ scripts/dzVents/runtime/tests/scripts/storage/__data_global_data.lua
@@ -0,0 +1,9 @@
+-- Persistent Data
+local multiRefObjects = {
+
+} -- multiRefObjects
+local obj1 = {
+	["h"] = true;
+	["g"] = 666;
+}
+return obj1
