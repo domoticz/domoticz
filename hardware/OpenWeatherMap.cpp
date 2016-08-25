f128a3b230b61cb10c81dac64167dc80cdd60e9d
Rob Peters <Rob@DVBControl.com>
2016-08-25 16:42:35 +0200

    OpenWeathermap: Fixed some code OZWCP: extra check for manager

--- hardware/OpenWeatherMap.cpp
+++ hardware/OpenWeatherMap.cpp
@@ -12,8 +12,40 @@
 #include "../main/SQLHelper.h"
 
 #ifdef _DEBUG
-	#define DEBUG_OPENWEATHERMAP
+	//	#define DEBUG_OPENWEATHERMAP
+	#define DEBUG_OPENWEATHERMAP_WRITE
 #endif
+
+#ifdef DEBUG_OPENWEATHERMAP_WRITE
+void SaveString2Disk(std::string str, std::string filename)
+{
+	FILE *fOut = fopen(filename.c_str(), "wb+");
+	if (fOut)
+	{
+		fwrite(str.c_str(), 1, str.size(), fOut);
+		fclose(fOut);
+	}
+}
+#endif
+#ifdef DEBUG_OPENWEATHERMAP_READ
+std::string ReadFile(std::string filename)
+{
+	std::ifstream file;
+	std::string sResult = "";
+	file.open(filename.c_str());
+	if (!file.is_open())
+		return "";
+	std::string sLine;
+	while (!file.eof())
+	{
+		getline(file, sLine);
+		sResult += sLine;
+	}
+	file.close();
+	return sResult;
+}
+#endif
+
 
 #define round(a) ( int ) ( a + .5 )
 
@@ -63,7 +95,7 @@ bool COpenWeatherMap::StopHardware()
 
 void COpenWeatherMap::Do_Work()
 {
-	int sec_counter = 585;
+	int sec_counter = 595;
 	while (!m_stoprequested)
 	{
 		sleep_seconds(1);
@@ -73,7 +105,14 @@ void COpenWeatherMap::Do_Work()
 		}
 		if (sec_counter % 600 == 0)
 		{
-			GetMeterDetails();
+			try
+			{
+				GetMeterDetails();
+			}
+			catch (...)
+			{
+				_log.Log(LOG_ERROR, "OpenWeatherMap: Error getting/parsing http data!");
+			}
 		}
 	}
 	_log.Log(LOG_STATUS,"OpenWeatherMap: Worker stopped...");
@@ -117,6 +156,9 @@ void COpenWeatherMap::GetMeterDetails()
 		return;
 	}
 
+#ifdef DEBUG_OPENWEATHERMAP_WRITE
+	SaveString2Disk(sResult, "E:\\OpenWeatherMap.json");
+#endif
 #ifdef DEBUG_OPENWEATHERMAP
 	_log.Log(LOG_STATUS, "OpenWeatherMap: Parsing json");
 #endif
@@ -136,14 +178,19 @@ void COpenWeatherMap::GetMeterDetails()
 		_log.Log(LOG_ERROR, "OpenWeatherMap: Empty data received!");
 		return;
 	}
-
-	if (root["cod"].asString() != "200")
+	if (root["cod"].empty())
 	{
-		_log.Log(LOG_ERROR, "OpenWeatherMap: Invalid cod received!");
+		_log.Log(LOG_ERROR, "OpenWeatherMap: Invalid data received!");
 		return;
 	}
 
-	float temp=0;
+	if (root["cod"].asInt() != 200)
+	{
+		_log.Log(LOG_ERROR, "OpenWeatherMap: Invalid data ('cod') received!");
+		return;
+	}
+
+	float temp=-999.9f;
 	int humidity=0;
 	int barometric=0;
 	int barometric_forcast=baroForecastNoInfo;
@@ -173,25 +220,28 @@ void COpenWeatherMap::GetMeterDetails()
 				barometric_forcast = baroForecastSunny;
 		}
 
-		if (barometric != 0)
+		if ((temp != -999.9f) && (humidity != 0) && (barometric != 0))
 		{
 			SendTempHumBaroSensor(1, 255, temp, humidity, static_cast<float>(barometric), barometric_forcast, "THB");
 		}
-		else if (humidity != 0)
+		else if ((temp != -999.9f) && (humidity != 0))
 		{
 			SendTempHumSensor(1, 255, temp, humidity, "TempHum");
 		}
 		else
 		{
-			SendTempSensor(1, 255, temp, "Temperature");
+			if (temp!=-999.9f)
+				SendTempSensor(1, 255, temp, "Temperature");
+			if (humidity != 0)
+				SendHumiditySensor(1, 255, humidity, "Humidity");
 		}
 	}
 
 	//Wind
 	if (!root["wind"].empty())
 	{
-		int wind_degrees = 0;
-		float windspeed_ms = 0;
+		int wind_degrees = -1;
+		float windspeed_ms = -1;
 
 		if (!root["wind"]["deg"].empty())
 		{
@@ -201,57 +251,62 @@ void COpenWeatherMap::GetMeterDetails()
 		{
 			windspeed_ms = root["wind"]["speed"].asFloat();
 		}
-
-		SendWind(1, 255, wind_degrees, windspeed_ms, 0, 0, 0, false, "Wind");
+		if ((wind_degrees != -1) && (windspeed_ms != -1))
+			SendWind(1, 255, wind_degrees, windspeed_ms, 0, 0, 0, false, "Wind");
 	}
 
 	//Rain
 	if (!root["rain"].empty())
 	{
+		if (!root["rain"]["3h"].empty())
+		{
 			float RainCount = static_cast<float>(atof(root["rain"]["3h"].asString().c_str()));
-			if (RainCount>=0.00f)
+			if (RainCount >= 0.00f)
 			{
 				RBUF tsen;
-				memset(&tsen,0,sizeof(RBUF));
-				tsen.RAIN.packetlength=sizeof(tsen.RAIN)-1;
-				tsen.RAIN.packettype=pTypeRAIN;
-				tsen.RAIN.subtype=sTypeRAINWU;
-				tsen.RAIN.battery_level=9;
-				tsen.RAIN.rssi=12;
-				tsen.RAIN.id1=0;
-				tsen.RAIN.id2=1;
+				memset(&tsen, 0, sizeof(RBUF));
+				tsen.RAIN.packetlength = sizeof(tsen.RAIN) - 1;
+				tsen.RAIN.packettype = pTypeRAIN;
+				tsen.RAIN.subtype = sTypeRAINWU;
+				tsen.RAIN.battery_level = 9;
+				tsen.RAIN.rssi = 12;
+				tsen.RAIN.id1 = 0;
+				tsen.RAIN.id2 = 1;
 
-				tsen.RAIN.rainrateh=0;
-				tsen.RAIN.rainratel=0;
+				tsen.RAIN.rainrateh = 0;
+				tsen.RAIN.rainratel = 0;
 
-				int tr10=int((float(RainCount)*10.0f));
-				tsen.RAIN.raintotal1=0;
-				tsen.RAIN.raintotal2=(BYTE)(tr10/256);
-				tr10-=(tsen.RAIN.raintotal2*256);
-				tsen.RAIN.raintotal3=(BYTE)(tr10);
+				int tr10 = int((float(RainCount)*10.0f));
+				tsen.RAIN.raintotal1 = 0;
+				tsen.RAIN.raintotal2 = (BYTE)(tr10 / 256);
+				tr10 -= (tsen.RAIN.raintotal2 * 256);
+				tsen.RAIN.raintotal3 = (BYTE)(tr10);
 
 				sDecodeRXMessage(this, (const unsigned char *)&tsen.RAIN, "Rain", 255);
 			}
+		}
 	}
 
 	//Visibility
 	if (!root["visibility"].empty())
 	{
-		float visibility = root["visibility"]["value"].asFloat();
-		if (visibility >= 0)
+		if (root["visibility"].isInt())
 		{
-			_tGeneralDevice gdevice;
-			gdevice.subtype = sTypeVisibility;
-			gdevice.floatval1 = visibility;
-			sDecodeRXMessage(this, (const unsigned char *)&gdevice, "Visibility", 255);
+			float visibility = ((float)root["visibility"].asInt())/1000.0f;
+			if (visibility >= 0)
+			{
+				_tGeneralDevice gdevice;
+				gdevice.subtype = sTypeVisibility;
+				gdevice.floatval1 = visibility;
+				sDecodeRXMessage(this, (const unsigned char *)&gdevice, "Visibility", 255);
+			}
 		}
 	}
 
-	// TODO - not working on this moment on www site
 	//Forecast URL
-	//if (!root["Link"].empty())
-	//{
-	//	m_ForecastURL = root["Link"].asString();
-	//}
+	if (!root["id"].empty())
+	{
+		m_ForecastURL = "http://openweathermap.org/city/" + root["id"].asString();
+	}
 }
 
