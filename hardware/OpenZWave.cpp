84a9e29ed25a152532ddab00044f57c1451e6a91
gizmocuz <info@domoticz.com>
2016-09-07 18:59:30 +0200

    OZW, reverted previous patch as it did not work

--- hardware/OpenZWave.cpp
+++ hardware/OpenZWave.cpp
@@ -1220,7 +1220,7 @@ bool COpenZWave::SwitchLight(const int nodeID, const int instanceID, const int c
 
 	bool bHandleAsBinary = false;
 	_tZWaveDevice *pDevice = FindDevice(nodeID, instanceID, 0, ZWaveBase::ZDTYPE_SWITCH_DIMMER);
-/*
+
 	if (pDevice)
 	{
 		if (
@@ -1239,8 +1239,9 @@ bool COpenZWave::SwitchLight(const int nodeID, const int instanceID, const int c
 			}
 		}
 	}
-*/
-	bHandleAsBinary = ((value == 0) || (value == 255));
+
+	//bHandleAsBinary = ((value == 0) || (value == 255));
+
 	if (!pDevice)
 		pDevice = FindDevice(nodeID, instanceID, 0);
 	if (!pDevice)
