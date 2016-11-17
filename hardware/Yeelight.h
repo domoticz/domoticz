b0fa08fde4c7cf5350cc7dcf7eb7496e9c11b4c0
Corbin <corbin@outlook.com>
2016-11-17 15:48:20 +1000

    reseting to use polling

--- hardware/Yeelight.h
+++ hardware/Yeelight.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "DomoticzHardware.h"
-#include "ASyncTCP.h"
 #include <deque>
 #include <iostream>
 #include <boost/asio.hpp>
@@ -13,8 +12,7 @@ public:
 	Yeelight(const int ID);
 	~Yeelight(void);
 	bool WriteToHardware(const char *pdata, const unsigned char length);
-	void InsertUpdateSwitch(const std::string &SketchName, const int &YeeType, const std::string &Location, const bool bIsOn, const std::string &yeelightBright, const std::string &yeelightHue);
-	void UpdateSwitch(const int &YeeType, const std::string &Location, const bool bIsOn, const std::string &yeelightBright, const std::string &yeelightHue, const std::string &yeelightRgb);
+	void InsertUpdateSwitch(const std::string &nodeID, const std::string &SketchName, const int &YeeType, const std::string &Location, const bool bIsOn, const std::string &yeelightBright, const std::string &yeelightHue);
 
 public:
 	//signals
@@ -29,11 +27,10 @@ protected:
 	boost::shared_ptr<boost::thread> m_thread;
 	volatile bool m_stoprequested;
 
-public:
-	class YeelightUDP
+	class udp_server
 	{
 	public:
-		YeelightUDP(boost::asio::io_service & io_service, int HardwareID);
+		udp_server(boost::asio::io_service & io_service, int m_HwdID);
 		boost::asio::ip::udp::socket socket_;
 		boost::asio::ip::udp::endpoint remote_endpoint_;
 		void start_send();
@@ -41,40 +38,6 @@ public:
 	private:
 		void start_receive();
 		bool HandleIncoming(const std::string &szData);
-
-	protected:
-		int m_HwdID;
-	};
-
-	class YeelightTCP : public ASyncTCP
-	{
-	public:
-		YeelightTCP(const std::string DeviceID, int HardwareID, int YeeType);
-		~YeelightTCP(void);
-		bool isConnected() { return mIsConnected; };
-		bool WriteInt(const std::string &sendString);
-		bool WriteInt(const uint8_t *pData, const size_t length);
-		bool Start();
-		bool Stop();
-		std::string m_szDeviceId;
-
-	public:
-		boost::signals2::signal<void()>	sDisconnected;
-
-	protected:
-		bool m_bDoRestart;
-		void Do_Work();
-		void OnConnect();
-		void OnDisconnect();
-		void OnData(const unsigned char *pData, size_t length);
-		void OnError(const std::exception e);
-		void OnError(const boost::system::error_code& error);
-		boost::shared_ptr<boost::thread> m_thread;
-		volatile bool m_stoprequested;
-		std::string m_szIPAddress;
-		int m_HwdID;
-		unsigned short m_usIPPort;
-		int m_Type;
 	};
 
 };
