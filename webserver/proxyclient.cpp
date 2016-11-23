c6b6495826a1c57b8e51cc1a66de378472dd59a2
rkluwen <rene@kluwen.com>
2016-11-23 17:04:19 +0100

    Changed proxy hostname.

--- webserver/proxyclient.cpp
+++ webserver/proxyclient.cpp
@@ -100,8 +100,8 @@ namespace http {
 
 		void CProxyClient::ContinueConnect(const boost::system::error_code& error)
 		{
-				std::string address = "myproxy.domoticz.com";
-				std::string port = "4443";
+				std::string address = "domoproxy.domoticz.com";
+				std::string port = "443";
 
 				_socket.reset(new boost::asio::ssl::stream<boost::asio::ip::tcp::socket>(_io_service, _context));
 				// set timeout timer
