b406e8a2c70140dc878a6a5f1362b6d9da99eb37
Rob Kuipers <RobKuipers@hotmail.com>
2016-12-22 16:25:25 +0100

    Show last set DHW Setpoint and/or CH Setpoint if OTGW-interface returns no value

--- hardware/OTGWBase.cpp
+++ hardware/OTGWBase.cpp
@@ -281,7 +281,14 @@ void OTGWBase::SendOutsideTemperature()
 void OTGWBase::SetSetpoint(const int idx, const float temp)
 {
 	char szCmd[30];
-	if (idx == 5)
+	if (idx == 1)
+	{
+		//Control Set Point (MsgID=1)
+		_log.Log(LOG_STATUS, "OTGW: Setting Control SetPoint to: %.1f", temp);
+		sprintf(szCmd, "CS=%.1f\r\n", temp);
+		WriteInt((const unsigned char*)&szCmd, (const unsigned char)strlen(szCmd));
+	}
+	else if (idx == 5)
 	{
 		//Room Set Point
 		//Make this a temporarily Set Point, this will be overridden when the thermostat changes/applying it's program
