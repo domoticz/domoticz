07e92fb679307f1b650ad9f2c233f921537206b3
Sneezydevil <pascal.github@sdgm-it.net>
2016-12-29 10:10:06 +0100

    Removed ) whitch should fix the hardware page

--- www/app/HardwareController.js
+++ www/app/HardwareController.js
@@ -1003,8 +1003,8 @@ define(['app'], function (app) {
 				(text.indexOf("Evohome") >= 0 && text.indexOf("script") >= 0) ||
 				(text.indexOf("Tellstick") >= 0) ||
 				(text.indexOf("Motherboard") >= 0) ||
-                		(text.indexOf("YeeLight") >= 0) ||               
-	                        (text.indexOf("Arilux AL-LC0x") >= 0) )
+				(text.indexOf("YeeLight") >= 0) ||               
+				(text.indexOf("Arilux AL-LC0x") >= 0)
 				)
             {
                 $.ajax({
