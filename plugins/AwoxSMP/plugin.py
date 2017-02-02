2aa66284c3810b36d895c8afc3d312f84e0e9bca
zaraki673 <azazel673@gmail.com>
2017-02-02 14:08:51 +0100

    Maj Plugins Awox

--- plugins/AwoxSMP/plugin.py
+++ plugins/AwoxSMP/plugin.py
@@ -18,6 +18,7 @@
 import Domoticz
 import binascii
 import struct
+import lib.pySmartPlugSmpB16
 from bluepy import btle
 
 START_OF_MESSAGE = b'\x0f'
@@ -25,86 +26,129 @@ END_OF_MESSAGE = b'\xff\xff'
 SMPstate = 0
 SMPconso = 0
 
+class BasePlugin:
+	enabled = False
+	pluginState = "Not Ready"
+	sessionCookie = ""
+	privateKey = b""
+	socketOn = "FALSE"
+	
+	def __init__(self):
+		return
 
-# Domoticz call back functions
+	def onStart(self):
+		global SMPstate, SMPconso
+		if Parameters["Mode6"] == "Debug":
+			Domoticz.Debugging(1)
+		if (len(Devices) == 0):
+			Domoticz.Device(Name="Status", Unit=1, Type=17, Switchtype=0).Create()
+			Domoticz.Device(Name="Conso", Unit=2, TypeName="Usage").Create()
+			Domoticz.Log("Devices created.")
+		else:
+			if (1 in Devices): SMPstate = Devices[1].nValue
+			if (2 in Devices): SMPconso = Devices[2].nValue
+		DumpConfigToLog()
+		Domoticz.Log("Plugin is started.")
+		Domoticz.Heartbeat(20)
+
+	def onStop(self):
+		Domoticz.Log("Plugin is stopping.")
+
+	def onConnect(self, Status, Description):
+		return
+
+	def onMessage(self, Data, Status, Extra):
+		return
+
+	def onCommand(self, Unit, Command, Level, Hue):
+		Domoticz.Debug("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))
+		Command = Command.strip()
+		action, sep, params = Command.partition(' ')
+		action = action.capitalize()
+		if (action == 'On'):
+			try:
+				plug = SmartPlug(Parameters["Address"])
+				plug.on()
+				UpdateDevice(1,1,'On')
+				plug.disconnect()
+			except btle.BTLEException as err:
+				Domoticz.Log('error when setting plug %s on (code %d)' % (Parameters["Address"], err.code))
+		elif (action == 'Off'):
+			try:
+				plug = SmartPlug(Parameters["Address"])
+				plug.off()
+				UpdateDevice(1,0,'Off')
+				plug.disconnect()
+			except btle.BTLEException as err:
+				Domoticz.Log('error when setting plug %s on (code %d)' % (Parameters["Address"], err.code))
+		return True
+
+	def onDisconnect(self):
+		return
+
+	def onHeartbeat(self):
+		global SMPstate, SMPconso
+		try:
+			plug = SmartPlug(Parameters["Address"])
+			(SMPstate, SMPconso) = plug.status_request()
+		except btle.BTLEException as err:
+			Domoticz.Log('error when requesting stat to plug %s (code %d)' % (Parameters["Address"], err.code))
+		plug.disconnect()
+		SMPstate = 'on' if SMPstate else 'off'
+		Domoticz.Log('plug state = %s' % SMPstate)
+		if (SMPstate == 'off'): UpdateDevice(1,0,'Off')
+		else: UpdateDevice(1,1,'On')
+		Domoticz.Log('plug power = %d W' % SMPconso)
+		UpdateDevice(2,0,str(SMPconso))
+		return True
+
+	def SetSocketSettings(self, power):
+		return
+
+	def GetSocketSettings(self):
+		return
+
+	def genericPOST(self, commandName):
+		return
+ 
+
+global _plugin
+_plugin = BasePlugin()
+
 def onStart():
-	global SMPstate, SMPconso
-	if Parameters["Mode6"] == "Debug":
-		Domoticz.Debugging(1)
-	if (len(Devices) == 0):
-		Domoticz.Device(Name="Status", Unit=1, Type=17, Switchtype=0).Create()
-		Domoticz.Device(Name="Conso", Unit=2, TypeName="Usage").Create()
-		Domoticz.Log("Devices created.")
-	else:
-		if (1 in Devices): SMPstate = Devices[1].nValue
-		if (2 in Devices): SMPconso = Devices[2].nValue
-	DumpConfigToLog()
-	Domoticz.Heartbeat(60)
-	return True
-
-def onConnect(Status, Description):
-
-	return True
-
-def onMessage(Data, Status, Extra):
-
-	return True
-
-def onCommand(Unit, Command, Level, Hue):
-
-	Domoticz.Debug("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))
-
-	Command = Command.strip()
-	action, sep, params = Command.partition(' ')
-	action = action.capitalize()
-
-	if (action == 'On'):
-		try:
-			plug = SmartPlug(Parameters["Address"])
-			plug.on()
-			UpdateDevice(1,1,'On')
-			plug.disconnect()
-		except btle.BTLEException as err:
-			Domoticz.Log('error when setting plug %s on (code %d)' % (Parameters["Address"], err.code))
-	elif (action == 'Off'):
-		try:
-			plug = SmartPlug(Parameters["Address"])
-			plug.off()
-			UpdateDevice(1,0,'Off')
-			plug.disconnect()
-		except btle.BTLEException as err:
-			Domoticz.Log('error when setting plug %s on (code %d)' % (Parameters["Address"], err.code))
-	return True
-
-def onNotification(Data):
-	Domoticz.Log("Notification: " + str(Data))
-	return
-
-def onHeartbeat():
-	global SMPstate, SMPconso
-	try:
-		plug = SmartPlug(Parameters["Address"])
-		(SMPstate, SMPconso) = plug.status_request()
-	except btle.BTLEException as err:
-		Domoticz.Log('error when requesting stat to plug %s (code %d)' % (Parameters["Address"], err.code))
-	plug.disconnect()
-	SMPstate = 'on' if SMPstate else 'off'
-	Domoticz.Log('plug state = %s' % SMPstate)
-	if (SMPstate == 'off'): UpdateDevice(1,0,'Off')
-	else: UpdateDevice(1,1,'On')
-	Domoticz.Log('plug power = %d W' % SMPconso)
-	UpdateDevice(2,0,str(SMPconso))
-
-
-	return True
-
-def onDisconnect():
-
-	return True
+	global _plugin
+	_plugin.onStart()
 
 def onStop():
+	global _plugin
+	_plugin.onStop()
 
-	return True
+def onConnect(Status, Description):
+	global _plugin
+	_plugin.onConnect(Status, Description)
+
+def onMessage(Data, Status, Extra):
+	global _plugin
+	_plugin.onMessage(Data, Status, Extra)
+
+def onCommand(Unit, Command, Level, Hue):
+	global _plugin
+	_plugin.onCommand(Unit, Command, Level, Hue)
+
+def onDisconnect():
+	global _plugin
+	_plugin.onDisconnect()
+
+def onHeartbeat():
+	global _plugin
+	_plugin.onHeartbeat()
+
+# xml built in parser threw import error on expat so just do it manually
+def extractTagValue(tagName, XML):
+	startPos = XML.find(tagName)
+	endPos = XML.find(tagName, startPos+1)
+	if ((startPos == -1) or (endPos == -1)): Domoticz.Error("'"+tagName+"' not found in supplied XML")
+	return XML[startPos+len(tagName)+1:endPos-2]
 
 # Generic helper functions
 def DumpConfigToLog():
@@ -119,8 +163,15 @@ def DumpConfigToLog():
 		Domoticz.Debug("Device nValue:	" + str(Devices[x].nValue))
 		Domoticz.Debug("Device sValue:   '" + Devices[x].sValue + "'")
 		Domoticz.Debug("Device LastLevel: " + str(Devices[x].LastLevel))
-	return
+	return	
 
+def UpdateDevice(Unit, nValue, sValue):
+	# Make sure that the Domoticz device still exists (they can be deleted) before updating it 
+	if (Unit in Devices):
+		if (Devices[Unit].nValue != nValue) or (Devices[Unit].sValue != sValue):
+			Devices[Unit].Update(nValue, str(sValue))
+			Domoticz.Log("Update "+str(nValue)+":'"+str(sValue)+"' ("+Devices[Unit].Name+")")
+	return
 
 class SmartPlug(btle.Peripheral):
 	def __init__(self, addr):
@@ -207,10 +258,3 @@ class NotificationDelegate(btle.DefaultDelegate):
 					self.programs.append({ "name" : name.decode('iso-8859-1').strip('\0'), "flags":flags, "start":"{0:02d}:{1:02d}".format(start_hour, start_minute), "end":"{0:02d}:{1:02d}".format(end_hour, end_minute)})
 				program_offset += 22
 
-def UpdateDevice(Unit, nValue, sValue):
-    # Make sure that the Domoticz device still exists (they can be deleted) before updating it 
-    if (Unit in Devices):
-        if (Devices[Unit].nValue != nValue) or (Devices[Unit].sValue != sValue):
-            Devices[Unit].Update(nValue, str(sValue))
-            Domoticz.Log("Update "+str(nValue)+":'"+str(sValue)+"' ("+Devices[Unit].Name+")")
-    return
