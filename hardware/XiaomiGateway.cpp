0176f5fcf66f466edd865d919cb3a862a5bc0a2c
Corbin Munce <corbinmunce@outlook.com>
2017-02-03 19:30:02 +1000

    Save svalue to database for LimitlessLights (#1197)

--- hardware/XiaomiGateway.cpp
+++ hardware/XiaomiGateway.cpp
@@ -877,13 +877,15 @@ void XiaomiGateway::xiaomi_udp_server::handle_receive(const boost::system::error
 				showmessage = false;
 			}
 			else if (cmd == "heartbeat") {
-				//update the token and gateway ip address.
+				if (model == "gateway") {
+					//update the token and gateway ip address.
 #ifdef _DEBUG
-				_log.Log(LOG_STATUS, "XiaomiGateway: Token Received - %s", root["token"].asString().c_str());
+					_log.Log(LOG_STATUS, "XiaomiGateway: Token Received - %s", root["token"].asString().c_str());
 #endif
-				m_XiaomiGateway->UpdateToken(root["token"].asString());
-				m_gatewayip = root["ip"].asString();
-				showmessage = false;
+					m_XiaomiGateway->UpdateToken(root["token"].asString());
+					m_gatewayip = root["ip"].asString();
+					showmessage = false;
+				}
 			}
 			else {
 				_log.Log(LOG_STATUS, "XiaomiGateway: unknown cmd received: %s", cmd.c_str());
