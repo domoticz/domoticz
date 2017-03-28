277aae95896db881478a2f4db83f0b2414501dce
DebugBill <DebugBill@thauvin.org>
2017-03-28 10:19:00 +0200

    in progress

--- hardware/TeleinfoBase.h
+++ hardware/TeleinfoBase.h
@@ -34,6 +34,7 @@ class CTeleinfoBase : public CDomoticzHardwareBase
 	protected:
 		typedef struct _tTeleinfo
 		{
+			std::string ADCO;
 			std::string PTEC;
 			std::string OPTARIF;
 			uint32_t ISOUSC;
