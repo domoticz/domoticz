28b72822cf41da4001409bf66c3d8ddfad85b63f
flatsiedatsie <unfold@gmail.com>
2017-04-19 11:14:03 +0200

    wrong folder

--- www/FrontpageController.js
+++
@@ -1,9 +0,0 @@
-define(['app'], function (app) {
-	app.controller('FrontpageController', [ '$scope', '$rootScope', '$location', '$http', '$interval', function($scope,$rootScope,$location,$http,$interval) {
-		init();
-
-		function init()
-		{
-		};
-	} ]);
-});
