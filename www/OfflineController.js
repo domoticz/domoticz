c7eb600b658be7ae8ff54ee43c7730c8775ffb3d
flatsiedatsie <unfold@gmail.com>
2017-04-19 11:15:13 +0200

    wrong folder

--- www/OfflineController.js
+++
@@ -1,9 +0,0 @@
-define(['app'], function (app) {
-	app.controller('OfflineController', [ '$scope', '$location', '$http', '$interval', function($scope,$location,$http,$interval) {
-		init();
-
-		function init()
-		{
-		};
-	} ]);
-});
