98e2a94fb40d1f4e1a58ed8672a0ca580779299a
DebugBill <DebugBill@thauvin.org>
2017-04-24 16:55:47 +0200

    Revert beautify

--- httpclient/HTTPClient.cpp
+++ httpclient/HTTPClient.cpp
@@ -7,39 +7,33 @@
 #include <fstream>
 
 #ifndef O_LARGEFILE
-#define O_LARGEFILE 0
+	#define O_LARGEFILE 0
 #endif
 
 extern std::string szUserDataFolder;
 
-bool        HTTPClient::m_bCurlGlobalInitialized = false;
-long        HTTPClient::m_iConnectionTimeout=10;
-								 //max, time that a download has to be finished?
-long        HTTPClient::m_iTimeout=90;
-std::string HTTPClient::m_sUserAgent="domoticz/1.0";
+bool		HTTPClient::m_bCurlGlobalInitialized = false;
+long		HTTPClient::m_iConnectionTimeout=10;
+long		HTTPClient::m_iTimeout=90; //max, time that a download has to be finished?
+std::string	HTTPClient::m_sUserAgent="domoticz/1.0";
 
-size_t write_curl_data(void *contents, size_t size, size_t nmemb, void *userp)
-{
+size_t write_curl_data(void *contents, size_t size, size_t nmemb, void *userp) {
 	size_t realsize = size * nmemb;
 	std::vector<unsigned char>* pvHTTPResponse = (std::vector<unsigned char>*)userp;
 	pvHTTPResponse->insert(pvHTTPResponse->end(),(unsigned char*)contents,(unsigned char*)contents+realsize);
 	return realsize;
 }
 
-
-size_t write_curl_data_file(void *contents, size_t size, size_t nmemb, void *userp)
-{
+size_t write_curl_data_file(void *contents, size_t size, size_t nmemb, void *userp) {
 	size_t realsize = size * nmemb;
 	std::ofstream *outfile=(std::ofstream*)userp;
 	outfile->write((const char*)contents,realsize);
 	return realsize;
 }
 
-
 bool HTTPClient::CheckIfGlobalInitDone()
 {
-	if (!m_bCurlGlobalInitialized)
-	{
+	if (!m_bCurlGlobalInitialized) {
 		CURLcode res=curl_global_init(CURL_GLOBAL_ALL);
 		if (res!=CURLE_OK)
 			return false;
@@ -48,7 +42,6 @@ bool HTTPClient::CheckIfGlobalInitDone()
 	return true;
 }
 
-
 void HTTPClient::Cleanup()
 {
 	if (m_bCurlGlobalInitialized)
@@ -56,7 +49,6 @@ void HTTPClient::Cleanup()
 		curl_global_cleanup();
 	}
 }
-
 
 void HTTPClient::SetGlobalOptions(void *curlobj)
 {
@@ -67,16 +59,14 @@ void HTTPClient::SetGlobalOptions(void *curlobj)
 	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 	curl_easy_setopt(curl, CURLOPT_USERAGENT, m_sUserAgent.c_str());
 	curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, m_iConnectionTimeout);
-	curl_easy_setopt(curl, CURLOPT_TIMEOUT,m_iTimeout);
+	curl_easy_setopt(curl, CURLOPT_TIMEOUT,m_iTimeout); 
 	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
-								 //allow self signed certificates
-	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, false);
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, false); //allow self signed certificates
 	curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
 	std::string domocookie = szUserDataFolder + "domocookie.txt";
 	curl_easy_setopt(curl, CURLOPT_COOKIEFILE, domocookie.c_str());
 	curl_easy_setopt(curl, CURLOPT_COOKIEJAR, domocookie.c_str());
 }
-
 
 //Configuration functions
 void HTTPClient::SetConnectionTimeout(const long timeout)
@@ -84,18 +74,15 @@ void HTTPClient::SetConnectionTimeout(const long timeout)
 	m_iConnectionTimeout=timeout;
 }
 
-
 void HTTPClient::SetTimeout(const long timeout)
 {
 	m_iTimeout=timeout;
 }
 
-
 void HTTPClient::SetUserAgent(const std::string &useragent)
 {
 	m_sUserAgent=useragent;
 }
-
 
 bool HTTPClient::GETBinary(const std::string &url, const std::vector<std::string> &ExtraHeaders, std::vector<unsigned char> &response, const int TimeOut)
 {
@@ -115,8 +102,7 @@ bool HTTPClient::GETBinary(const std::string &url, const std::vector<std::string
 		}
 
 		struct curl_slist *headers=NULL;
-		if (ExtraHeaders.size()>0)
-		{
+		if (ExtraHeaders.size()>0) {
 			std::vector<std::string>::const_iterator itt;
 			for (itt=ExtraHeaders.begin(); itt!=ExtraHeaders.end(); ++itt)
 			{
@@ -124,57 +110,53 @@ bool HTTPClient::GETBinary(const std::string &url, const std::vector<std::string
 			}
 		}
 
-		if (headers!=NULL)
-		{
-			curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+		if (headers!=NULL) {
+			curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers); 
 		}
 
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&response);
 		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-		curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1);
-		res = curl_easy_perform(curl);
-		if(res == CURLE_HTTP_RETURNED_ERROR)
-		{
-			long response_code;
-			curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-			switch(response_code)
-			{
-				case 400:
-					_log.Log(LOG_ERROR,"HTTP 400: Bad Request");
-					break;
-				case 401:
-					_log.Log(LOG_ERROR,"HTTP 401: Unauthorized. Authentication is required and has failed or has not yet been provided");
-					break;
-				case 403:
-					_log.Log(LOG_ERROR,"HTTP 403: Forbidden. The request is valid, but the server is refusing action");
-					break;
-				case 404:
-					_log.Log(LOG_ERROR,"HTTP 404: Not Found");
-					break;
-				case 500:
-					_log.Log(LOG_ERROR,"HTTP 500: Internal Server Error");
-					break;
-				case 503:
-					_log.Log(LOG_ERROR,"HTTP 503: Service Unavailable");
-					break;
-			}
-		}
+                curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1);
+                res = curl_easy_perform(curl);
+                if(res == CURLE_HTTP_RETURNED_ERROR)
+                {
+                        long response_code;
+                        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+                        switch(response_code)
+                        {
+                                case 400:
+                                        _log.Log(LOG_ERROR,"HTTP 400: Bad Request");
+                                        break;
+                                case 401:
+                                        _log.Log(LOG_ERROR,"HTTP 401: Unauthorized. Authentication is required and has failed or has not yet been provided");
+                                        break;
+                                case 403:
+                                        _log.Log(LOG_ERROR,"HTTP 403: Forbidden. The request is valid, but the server is refusing action");
+                                        break;
+                                case 404:
+                                        _log.Log(LOG_ERROR,"HTTP 404: Not Found");
+                                        break;
+                                case 500:
+                                        _log.Log(LOG_ERROR,"HTTP 500: Internal Server Error");
+                                        break;
+                                case 503:
+                                        _log.Log(LOG_ERROR,"HTTP 503: Service Unavailable");
+                                        break;
+                        }
+                }
 		curl_easy_cleanup(curl);
 
-		if (headers!=NULL)
-		{
-								 /* free the header list */
-			curl_slist_free_all(headers);
+		if (headers!=NULL) {
+			curl_slist_free_all(headers); /* free the header list */
 		}
 
 		return (res==CURLE_OK);
 	}
 	catch (...)
 	{
-		return false;
+		return false;		
 	}
 }
-
 
 bool HTTPClient::GETBinaryToFile(const std::string &url, const std::string &outputfile)
 {
@@ -207,10 +189,9 @@ bool HTTPClient::GETBinaryToFile(const std::string &url, const std::string &outp
 	}
 	catch (...)
 	{
-		return false;
+		return false;		
 	}
 }
-
 
 bool HTTPClient::POSTBinary(const std::string &url, const std::string &postdata, const std::vector<std::string> &ExtraHeaders, std::vector<unsigned char> &response, const bool bFollowRedirect)
 {
@@ -233,8 +214,7 @@ bool HTTPClient::POSTBinary(const std::string &url, const std::string &postdata,
 		curl_easy_setopt(curl, CURLOPT_POST, 1);
 
 		struct curl_slist *headers=NULL;
-		if (ExtraHeaders.size()>0)
-		{
+		if (ExtraHeaders.size()>0) {
 			std::vector<std::string>::const_iterator itt;
 			for (itt=ExtraHeaders.begin(); itt!=ExtraHeaders.end(); ++itt)
 			{
@@ -242,19 +222,16 @@ bool HTTPClient::POSTBinary(const std::string &url, const std::string &postdata,
 			}
 		}
 
-		if (headers!=NULL)
-		{
-			curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+		if (headers!=NULL) {
+			curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers); 
 		}
 
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postdata.c_str());
 		res = curl_easy_perform(curl);
 		curl_easy_cleanup(curl);
 
-		if (headers!=NULL)
-		{
-								 /* free the header list */
-			curl_slist_free_all(headers);
+		if (headers!=NULL) {
+			curl_slist_free_all(headers); /* free the header list */
 		}
 
 		return (res==CURLE_OK);
@@ -264,7 +241,6 @@ bool HTTPClient::POSTBinary(const std::string &url, const std::string &postdata,
 		return false;
 	}
 }
-
 
 bool HTTPClient::PUTBinary(const std::string &url, const std::string &postdata, const std::vector<std::string> &ExtraHeaders, std::vector<unsigned char> &response)
 {
@@ -283,9 +259,9 @@ bool HTTPClient::PUTBinary(const std::string &url, const std::string &postdata, 
 		//curl_easy_setopt(curl, CURLOPT_PUT, 1);
 		curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
 
+
 		struct curl_slist *headers=NULL;
-		if (ExtraHeaders.size()>0)
-		{
+		if (ExtraHeaders.size()>0) {
 			std::vector<std::string>::const_iterator itt;
 			for (itt=ExtraHeaders.begin(); itt!=ExtraHeaders.end(); ++itt)
 			{
@@ -293,19 +269,16 @@ bool HTTPClient::PUTBinary(const std::string &url, const std::string &postdata, 
 			}
 		}
 
-		if (headers!=NULL)
-		{
-			curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+		if (headers!=NULL) {
+			curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers); 
 		}
 
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postdata.c_str());
 		res = curl_easy_perform(curl);
 		curl_easy_cleanup(curl);
 
-		if (headers!=NULL)
-		{
-								 /* free the header list */
-			curl_slist_free_all(headers);
+		if (headers!=NULL) {
+			curl_slist_free_all(headers); /* free the header list */
 		}
 
 		return (res==CURLE_OK);
@@ -315,7 +288,6 @@ bool HTTPClient::PUTBinary(const std::string &url, const std::string &postdata, 
 		return false;
 	}
 }
-
 
 bool HTTPClient::DeleteBinary(const std::string &url, const std::string &postdata, const std::vector<std::string> &ExtraHeaders, std::vector<unsigned char> &response)
 {
@@ -334,9 +306,9 @@ bool HTTPClient::DeleteBinary(const std::string &url, const std::string &postdat
 		//curl_easy_setopt(curl, CURLOPT_PUT, 1);
 		curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "DELETE");
 
+
 		struct curl_slist *headers = NULL;
-		if (ExtraHeaders.size() > 0)
-		{
+		if (ExtraHeaders.size() > 0) {
 			std::vector<std::string>::const_iterator itt;
 			for (itt = ExtraHeaders.begin(); itt != ExtraHeaders.end(); ++itt)
 			{
@@ -344,8 +316,7 @@ bool HTTPClient::DeleteBinary(const std::string &url, const std::string &postdat
 			}
 		}
 
-		if (headers != NULL)
-		{
+		if (headers != NULL) {
 			curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 		}
 
@@ -353,10 +324,8 @@ bool HTTPClient::DeleteBinary(const std::string &url, const std::string &postdat
 		res = curl_easy_perform(curl);
 		curl_easy_cleanup(curl);
 
-		if (headers != NULL)
-		{
-								 /* free the header list */
-			curl_slist_free_all(headers);
+		if (headers != NULL) {
+			curl_slist_free_all(headers); /* free the header list */
 		}
 
 		return (res == CURLE_OK);
@@ -366,7 +335,6 @@ bool HTTPClient::DeleteBinary(const std::string &url, const std::string &postdat
 		return false;
 	}
 }
-
 
 bool HTTPClient::GET(const std::string &url, std::string &response, const bool bIgnoreNoDataReturned)
 {
@@ -384,7 +352,6 @@ bool HTTPClient::GET(const std::string &url, std::string &response, const bool b
 	return true;
 }
 
-
 bool HTTPClient::GET(const std::string &url, const std::vector<std::string> &ExtraHeaders, std::string &response)
 {
 	response = "";
@@ -397,7 +364,6 @@ bool HTTPClient::GET(const std::string &url, const std::vector<std::string> &Ext
 	return true;
 }
 
-
 bool HTTPClient::POST(const std::string &url, const std::string &postdata, const std::vector<std::string> &ExtraHeaders, std::string &response, const bool bFollowRedirect)
 {
 	response = "";
@@ -405,11 +371,10 @@ bool HTTPClient::POST(const std::string &url, const std::string &postdata, const
 	if (!POSTBinary(url,postdata,ExtraHeaders, vHTTPResponse, bFollowRedirect))
 		return false;
 	if (vHTTPResponse.empty())
-		return true;			 //empty response possible
+		return true; //empty response possible
 	response.insert(response.begin(), vHTTPResponse.begin(), vHTTPResponse.end());
 	return true;
 }
-
 
 bool HTTPClient::PUT(const std::string &url, const std::string &postdata, const std::vector<std::string> &ExtraHeaders, std::string &response)
 {
@@ -418,11 +383,10 @@ bool HTTPClient::PUT(const std::string &url, const std::string &postdata, const 
 	if (!PUTBinary(url,postdata,ExtraHeaders, vHTTPResponse))
 		return false;
 	if (vHTTPResponse.empty())
-		return true;			 //empty response possible
+		return true; //empty response possible
 	response.insert(response.begin(), vHTTPResponse.begin(), vHTTPResponse.end());
 	return true;
 }
-
 
 bool HTTPClient::Delete(const std::string &url, const std::string &postdata, const std::vector<std::string> &ExtraHeaders, std::string &response)
 {
@@ -431,7 +395,7 @@ bool HTTPClient::Delete(const std::string &url, const std::string &postdata, con
 	if (!DeleteBinary(url, postdata, ExtraHeaders, vHTTPResponse))
 		return false;
 	if (vHTTPResponse.empty())
-		return true;			 //empty response possible
+		return true; //empty response possible
 	response.insert(response.begin(), vHTTPResponse.begin(), vHTTPResponse.end());
 	return true;
 }
