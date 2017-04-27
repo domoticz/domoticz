cd85308f10e6190aac8d84d2c1d33fe448a5f00d
flatsiedatsie <unfold@gmail.com>
2017-04-27 15:29:11 +0200

    CSS optimisations
    Small changes
    - pop-up background now matches settings.
    - Status output not via flex-row with wrap (useful if an icon is also added, like in the merger feature).

---
+++ www/styles/default/custom.js
@@ -0,0 +1,23 @@
+
+if($("#copyright").css("z-index") == 1){
+console.log("BINGO BONGO");
+    alert("merging much?");
+}
+//alert("bongo");
+
+console.log("JS LOADED HOERA!");
+
+$("body").css("background-color:red!important;background-image:none!important");
+$("#name span").css("background-color:red!important");
+
+//http://192.168.1.34:8080/json.htm?type=graph&sensor=temp&idx=202&range=day
+
+
+/*
+$( "p" ).click(function() {
+  $( this ).slideUp();
+});
+
+
+
+*/
