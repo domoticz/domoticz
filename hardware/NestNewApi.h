67e3fc4e62c97b2d6c7fa4221d168d2f5158b72b
github@dotms.nl <github@dotms.nl>
2017-05-03 22:35:39 +0200

    Basic initial version of new Nest Api device plugin.

---
+++ hardware/NestNewApi.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "DomoticzHardware.h"
+#include <iostream>
+#include "hardwaretypes.h"
+#include <map>
+
+class CNestNewApi : public CDomoticzHardwareBase
+{
+	struct _tNestThemostat
+	{
+		std::string Serial;
+		std::string StructureID;
+		std::string Name;
+	};
+public:
+	CNestNewApi(const int ID, const std::string &APIKey);
+	~CNestNewApi(void);
+	bool WriteToHardware(const char *pdata, const unsigned char length);
+	void SetSetpoint(const int idx, const float temp);
+	void SetProgramState(const int newState);
+private:
+	void SendSetPointSensor(const unsigned char Idx, const float Temp, const std::string &defaultname);
+	void UpdateSwitch(const unsigned char Idx, const bool bOn, const std::string &defaultname);
+	void UpdateSmokeSensor(const unsigned char Idx, const bool bOn, const std::string &defaultname);
+	bool SetAway(const unsigned char Idx, const bool bIsAway);
+	bool Login();
+	void Logout();
+
+	std::string m_UserName;
+	std::string m_Password;
+	std::string m_TransportURL;
+	std::string m_AccessToken;
+	std::string m_UserID;
+//	std::string m_Serial;
+	//std::string m_StructureID;
+	volatile bool m_stoprequested;
+	boost::shared_ptr<boost::thread> m_thread;
+	std::map<int, _tNestThemostat> m_thermostats;
+	bool m_bDoLogin;
+
+	std::string m_NewApiAccessToken;
+
+	void Init();
+	bool StartHardware();
+	bool StopHardware();
+	void Do_Work();
+	void GetMeterDetails();
+};
+
