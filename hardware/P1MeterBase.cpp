952cd7e8ca62ceed22d45f4a1d368bf4b6cc2061
gordonb3 <gordon@bosvangennip.nl>
2017-05-22 10:31:07 +0200

    variable names in smartmeter class do not all require a 'p1' prefix

--- hardware/P1MeterBase.cpp
+++ hardware/P1MeterBase.cpp
@@ -95,43 +95,44 @@ P1MeterBase::~P1MeterBase(void)
 
 void P1MeterBase::Init()
 {
+	m_p1version=2; // initialize DSMR version @2
 	m_linecount=0;
 	m_exclmarkfound=0;
-	l_exclmarkfound=0;
 	m_CRfound=0;
 	m_bufferpos=0;
-	l_bufferpos=0;
 	m_lastgasusage=0;
 	m_lastSharedSendGas=0;
 	m_lastUpdateTime=0;
 
-	m_p1voltagel1=0;
-	m_p1voltagel2=0;
-	m_p1voltagel3=0;
+	l_exclmarkfound=0;
+	l_bufferpos=0;
+
+	m_voltagel1=0;
+	m_voltagel2=0;
+	m_voltagel3=0;
 
 	memset(&m_buffer,0,sizeof(m_buffer));
 	memset(&l_buffer,0,sizeof(l_buffer));
 
-	memset(&m_p1power,0,sizeof(m_p1power));
-	memset(&m_p1gas,0,sizeof(m_p1gas));
+	memset(&m_power,0,sizeof(m_power));
+	memset(&m_gas,0,sizeof(m_gas));
 
-	m_p1power.len=sizeof(P1Power)-1;
-	m_p1power.type=pTypeP1Power;
-	m_p1power.subtype=sTypeP1Power;
-	m_p1power.ID=1;
+	m_power.len=sizeof(P1Power)-1;
+	m_power.type=pTypeP1Power;
+	m_power.subtype=sTypeP1Power;
+	m_power.ID=1;
 
-	m_p1gas.len=sizeof(P1Gas)-1;
-	m_p1gas.type=pTypeP1Gas;
-	m_p1gas.subtype=sTypeP1Gas;
-	m_p1gas.ID=1;
-	m_p1gas.gasusage=0;
+	m_gas.len=sizeof(P1Gas)-1;
+	m_gas.type=pTypeP1Gas;
+	m_gas.subtype=sTypeP1Gas;
+	m_gas.ID=1;
+	m_gas.gasusage=0;
 
-	m_p1version=2; // initialize DSMR version @2
-	m_p1gmbchan=0;
-	m_p1gasprefix="0-n";
-	m_p1gasts="";
-	m_p1gasclockskew=0;
-	m_p1gasoktime=0;
+	m_gasmbuschannel=0;
+	m_gasprefix="0-n";
+	m_gastimestamp="";
+	m_gasclockskew=0;
+	m_gasoktime=0;
 }
 
 bool P1MeterBase::MatchLine()
@@ -169,7 +170,7 @@ bool P1MeterBase::MatchLine()
 				found=1;
 			break;
 		case DEVTYPE:
-			if(m_p1gmbchan==0){
+			if(m_gasmbuschannel==0){
 				vString=(const char*)t->key+3;
 				if (strncmp(vString.c_str(), (const char*)&l_buffer+3, strlen(t->key)-3) == 0)
 					found=1;
@@ -178,14 +179,14 @@ bool P1MeterBase::MatchLine()
 			}
 			break;
 		case GAS:
-			if(strncmp((m_p1gasprefix+(t->key+3)).c_str(), (const char*)&l_buffer, strlen(t->key)) == 0){
+			if(strncmp((m_gasprefix+(t->key+3)).c_str(), (const char*)&l_buffer, strlen(t->key)) == 0){
 				found=1;
 			}
 			if (m_p1version>=4)
 				i+=100; // skip matches with any DSMR v2 gas lines
 			break;
 		case LINE17:
-			if(strncmp((m_p1gasprefix+(t->key+3)).c_str(), (const char*)&l_buffer, strlen(t->key)) == 0){
+			if(strncmp((m_gasprefix+(t->key+3)).c_str(), (const char*)&l_buffer, strlen(t->key)) == 0){
 				m_linecount = 17;
 				found=1;
 			}
@@ -203,58 +204,58 @@ bool P1MeterBase::MatchLine()
 			time_t atime=mytime(NULL);
 			if (difftime(atime,m_lastUpdateTime)>=m_ratelimit) {
 				m_lastUpdateTime=atime;
-				sDecodeRXMessage(this, (const unsigned char *)&m_p1power, "Power", 255);
-				if (m_p1voltagel1) {
-					SendVoltageSensor(0, 1, 255, m_p1voltagel1, "Voltage L1");
-					if (m_p1voltagel2)
-						SendVoltageSensor(0, 2, 255, m_p1voltagel2, "Voltage L2");
-					if (m_p1voltagel3)
-						SendVoltageSensor(0, 3, 255, m_p1voltagel3, "Voltage L3");
+				sDecodeRXMessage(this, (const unsigned char *)&m_power, "Power", 255);
+				if (m_voltagel1) {
+					SendVoltageSensor(0, 1, 255, m_voltagel1, "Voltage L1");
+					if (m_voltagel2)
+						SendVoltageSensor(0, 2, 255, m_voltagel2, "Voltage L2");
+					if (m_voltagel3)
+						SendVoltageSensor(0, 3, 255, m_voltagel3, "Voltage L3");
 				}
-				if ( (m_p1gas.gasusage>0)&&( (m_p1gas.gasusage!=m_lastgasusage)||(difftime(atime,m_lastSharedSendGas)>=300) ) ){
+				if ( (m_gas.gasusage>0)&&( (m_gas.gasusage!=m_lastgasusage)||(difftime(atime,m_lastSharedSendGas)>=300) ) ){
 					//only update gas when there is a new value, or 5 minutes are passed
-					if (m_p1gasclockskew>=300){ // just accept it - we cannot sync to our clock
+					if (m_gasclockskew>=300){ // just accept it - we cannot sync to our clock
 						m_lastSharedSendGas=atime;
-						m_lastgasusage=m_p1gas.gasusage;
-						sDecodeRXMessage(this, (const unsigned char *)&m_p1gas, "Gas", 255);
+						m_lastgasusage=m_gas.gasusage;
+						sDecodeRXMessage(this, (const unsigned char *)&m_gas, "Gas", 255);
 					}
-					else if (atime>=m_p1gasoktime){
+					else if (atime>=m_gasoktime){
 						struct tm ltime;
 						localtime_r(&atime, &ltime);
 						char myts[13];
 						sprintf(myts,"%02d%02d%02d%02d%02d%02dW",ltime.tm_year%100,ltime.tm_mon+1,ltime.tm_mday,ltime.tm_hour,ltime.tm_min,ltime.tm_sec);
 						if (ltime.tm_isdst)
 						myts[12]='S';
-						if (strncmp((const char*)&myts,m_p1gasts.c_str(),m_p1gasts.length())>=0){
+						if (strncmp((const char*)&myts,m_gastimestamp.c_str(),m_gastimestamp.length())>=0){
 							m_lastSharedSendGas=atime;
-							m_lastgasusage=m_p1gas.gasusage;
-							m_p1gasoktime+=300;
-							sDecodeRXMessage(this, (const unsigned char *)&m_p1gas, "Gas", 255);
+							m_lastgasusage=m_gas.gasusage;
+							m_gasoktime+=300;
+							sDecodeRXMessage(this, (const unsigned char *)&m_gas, "Gas", 255);
 						}
 						else // gas clock is ahead
 						{
 							struct tm gastm;
-							gastm.tm_year = atoi(m_p1gasts.substr(0, 2).c_str()) + 100;
-							gastm.tm_mon = atoi(m_p1gasts.substr(2, 2).c_str()) - 1;
-							gastm.tm_mday = atoi(m_p1gasts.substr(4, 2).c_str());
-							gastm.tm_hour = atoi(m_p1gasts.substr(6, 2).c_str());
-							gastm.tm_min = atoi(m_p1gasts.substr(8, 2).c_str());
-							gastm.tm_sec = atoi(m_p1gasts.substr(10, 2).c_str());
-							if (m_p1gasts.length()==12)
+							gastm.tm_year = atoi(m_gastimestamp.substr(0, 2).c_str()) + 100;
+							gastm.tm_mon = atoi(m_gastimestamp.substr(2, 2).c_str()) - 1;
+							gastm.tm_mday = atoi(m_gastimestamp.substr(4, 2).c_str());
+							gastm.tm_hour = atoi(m_gastimestamp.substr(6, 2).c_str());
+							gastm.tm_min = atoi(m_gastimestamp.substr(8, 2).c_str());
+							gastm.tm_sec = atoi(m_gastimestamp.substr(10, 2).c_str());
+							if (m_gastimestamp.length()==12)
 								gastm.tm_isdst = -1;
-							else if (m_p1gasts[12]=='W')
+							else if (m_gastimestamp[12]=='W')
 								gastm.tm_isdst = 0;
 							else
 								gastm.tm_isdst = 1;
 
 							time_t gtime=mktime(&gastm);
-							m_p1gasclockskew=difftime(gtime,atime);
-							if (m_p1gasclockskew>=300){
+							m_gasclockskew=difftime(gtime,atime);
+							if (m_gasclockskew>=300){
 								_log.Log(LOG_ERROR, "unable to synchronize to the gas meter clock because it is more than 5 minutes ahead of my time");
 							}
 							else {
-								m_p1gasoktime=gtime;
-								_log.Log(LOG_STATUS, "Gas meter clock is %i seconds ahead - wait for my clock to catch up", (int)m_p1gasclockskew);
+								m_gasoktime=gtime;
+								_log.Log(LOG_STATUS, "Gas meter clock is %i seconds ahead - wait for my clock to catch up", (int)m_gasclockskew);
 							}
 						}
 					}
@@ -303,63 +304,63 @@ bool P1MeterBase::MatchLine()
 			case P1TYPE_MBUSDEVICETYPE:
 				temp_usage = (unsigned long)(strtod(value,&validate));
 				if (temp_usage == 3) {
-					m_p1gmbchan = (char)l_buffer[2];
-					m_p1gasprefix[2]=m_p1gmbchan;
-				_log.Log(LOG_STATUS,"P1: Found gas meter on M-Bus channel %c", m_p1gmbchan);
+					m_gasmbuschannel = (char)l_buffer[2];
+					m_gasprefix[2]=m_gasmbuschannel;
+				_log.Log(LOG_STATUS,"P1: Found gas meter on M-Bus channel %c", m_gasmbuschannel);
 				}
 				break;
 			case P1TYPE_POWERUSAGE1:
 				temp_usage = (unsigned long)(strtod(value,&validate)*1000.0f);
-				m_p1power.powerusage1 = temp_usage;
+				m_power.powerusage1 = temp_usage;
 				break;
 			case P1TYPE_POWERUSAGE2:
 				temp_usage = (unsigned long)(strtod(value,&validate)*1000.0f);
-				m_p1power.powerusage2=temp_usage;
+				m_power.powerusage2=temp_usage;
 				break;
 			case P1TYPE_POWERDELIV1:
 				temp_usage = (unsigned long)(strtod(value,&validate)*1000.0f);
-				m_p1power.powerdeliv1=temp_usage;
+				m_power.powerdeliv1=temp_usage;
 				break;
 			case P1TYPE_POWERDELIV2:
 				temp_usage = (unsigned long)(strtod(value,&validate)*1000.0f);
-				m_p1power.powerdeliv2=temp_usage;
+				m_power.powerdeliv2=temp_usage;
 				break;
 			case P1TYPE_USAGECURRENT:
 				temp_usage = (unsigned long)(strtod(value,&validate)*1000.0f);	//Watt
 				if (temp_usage < 17250)
 				{
-					m_p1power.usagecurrent = temp_usage;
+					m_power.usagecurrent = temp_usage;
 				}
 				break;
 			case P1TYPE_DELIVCURRENT:
 				temp_usage = (unsigned long)(strtod(value,&validate)*1000.0f);	//Watt;
 				if (temp_usage < 17250)
 				{
-					m_p1power.delivcurrent = temp_usage;
+					m_power.delivcurrent = temp_usage;
 				}
 				break;
 			case P1TYPE_VOLTAGEL1:
 				temp_volt = strtof(value,&validate);
 				if (temp_volt < 300)
-					m_p1voltagel1 = temp_volt; //Voltage L1;
+					m_voltagel1 = temp_volt; //Voltage L1;
 				break;
 			case P1TYPE_VOLTAGEL2:
 				temp_volt = strtof(value,&validate);
 				if (temp_volt < 300)
-					m_p1voltagel2 = temp_volt; //Voltage L2;
+					m_voltagel2 = temp_volt; //Voltage L2;
 				break;
 			case P1TYPE_VOLTAGEL3:
 				temp_volt = strtof(value,&validate);
 				if (temp_volt < 300)
-					m_p1voltagel3 = temp_volt; //Voltage L3;
+					m_voltagel3 = temp_volt; //Voltage L3;
 				break;
 			case P1TYPE_GASTIMESTAMP:
-				m_p1gasts = std::string(value);
+				m_gastimestamp = std::string(value);
 				break;
 			case P1TYPE_GASUSAGE:
 			case P1TYPE_GASUSAGEDSMR4:
 				temp_usage = (unsigned long)(strtod(value,&validate)*1000.0f);
-				m_p1gas.gasusage = temp_usage;
+				m_gas.gasusage = temp_usage;
 				break;
 			}
 
@@ -371,9 +372,9 @@ bool P1MeterBase::MatchLine()
 
 			if (t->type == P1TYPE_GASUSAGEDSMR4){ // need to get timestamp from this line as well
 				vString=(const char*)&l_buffer+11;
-				m_p1gasts=vString.substr(0,13);
+				m_gastimestamp=vString.substr(0,13);
 #ifdef _DEBUG
-				_log.Log(LOG_NORM,"P1: Key: gastimestamp, Value: %s", m_p1gasts);
+				_log.Log(LOG_NORM,"P1: Key: gastimestamp, Value: %s", m_gastimestamp);
 #endif
 			}
 		}
