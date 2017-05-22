215edaa22cec641b53d18a564868e2b7e6f05180
hvbommel <henri@vanbml.nl>
2017-05-22 14:20:56 +0200

    Sysfs gpio: int to char

--- hardware/SysfsGPIO.h
+++ hardware/SysfsGPIO.h
@@ -8,19 +8,19 @@
 
 struct gpio_info
 {
-	uint8_t	value;		// GPIO pin Value
-	uint8_t	direction;	// GPIO IN or OUT
-	uint8_t	active_low;	// GPIO ActiveLow
-	uint8_t	edge;		// GPIO int Edge
-	uint8_t	db_state;	// Database Value
-	uint8_t	db_state;	// Database Value id1;			// Device id1
-	uint8_t	db_state;	// Database Value id2;			// Device id2
-	uint8_t	db_state;	// Database Valueid3;			// Device id3
-	uint8_t	db_state;	// Database Valueid4;			// Device id4
-	uint8_t	db_state;	// Database Valueid_valid;		// Device valid
-	uint8_t	db_state;	// Database Valueint request_update; // Request update
-	int	read_value_fd;	// Fast read fd
-	int	pin_number;		// GPIO Pin number
+	int		pin_number;		// GPIO Pin number
+	char	value;			// GPIO pin Value
+	char	direction;		// GPIO IN or OUT
+	char	active_low;		// GPIO ActiveLow
+	char	edge;			// GPIO int Edge
+	int		read_value_fd;	// Fast read fd
+	char	db_state;		// Database Value
+	int		id1;			// Device id1
+	int		id2;			// Device id2
+	int		id3;			// Device id3
+	int		id4;			// Device id4
+	char	id_valid;		// Device valid
+	char	request_update;	// Request update
 };
 
 class CSysfsGPIO : public CDomoticzHardwareBase
