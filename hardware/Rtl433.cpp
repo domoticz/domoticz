e0b96e07ddf128070bd9e7094d57b483823c141a
gizmocuz <info@domoticz.com>
2017-05-28 16:37:15 +0200

    rtl433 fixed previous code change

--- hardware/Rtl433.cpp
+++ hardware/Rtl433.cpp
@@ -105,7 +105,7 @@ void CRtl433::Do_Work()
 		std::string szCommand = "C:\\rtl_433.exe " + szFlags;
 		m_hPipe = _popen(szCommand.c_str(), "r");
 #else
-		szCommand = "rtl_433 " + szFlags + " > /dev/null";
+		std::string szCommand = "rtl_433 " + szFlags + " > /dev/null";
 		pipe = popen(szCommand.c_str(), "r");
 #endif
 		if (m_hPipe == NULL)
