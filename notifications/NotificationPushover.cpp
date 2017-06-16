a8c7daeb92888bd874caeeac1d04880c2a8a82a5
jvandenbroek <jvandenbroek@gmail.com>
2017-06-16 17:49:52 +0200

    Pushover priority and sound in Custom Message field

--- notifications/NotificationPushover.cpp
+++ notifications/NotificationPushover.cpp
@@ -1,7 +1,9 @@
 #include "stdafx.h"
 #include "NotificationPushover.h"
 #include "../httpclient/HTTPClient.h"
+#include "../httpclient/UrlEncode.h"
 #include "../main/Logger.h"
+#include "../main/Helper.h"
 
 CNotificationPushover::CNotificationPushover() : CNotificationBase(std::string("pushover"), OPTIONS_URL_SUBJECT | OPTIONS_URL_BODY | OPTIONS_URL_PARAMS)
 {
@@ -25,12 +27,24 @@ bool CNotificationPushover::SendMessageImplementation(
 	const bool bFromNotification)
 {
 	std::string cSubject = (Subject == Text) ? "Domoticz" : Subject;
+	std::string cText = Text;
+	std::string cSound = Sound;
+	int cPriority = Priority;
 
 	bool bRet;
 	std::string sResult;
 	std::stringstream sPostData;
 
-	sPostData << "token=" << _apikey << "&user=" << _apiuser << "&priority=" << Priority << "&title=" << cSubject << "&message=" << Text;
+	std::vector<std::string> splitresults;
+	StringSplit(CURLEncode::URLDecode(Text), "#", splitresults);
+	if (splitresults.size() > 2)
+	{
+		cText = splitresults[0];
+		cPriority = atoi(splitresults[1].c_str());
+		cSound = splitresults[2];
+	}
+
+	sPostData << "token=" << _apikey << "&user=" << _apiuser << "&priority=" << cPriority << "&title=" << cSubject << "&message=" << cText;
 
 	size_t posDevice = ExtraData.find("|Device=");
 	if (posDevice != std::string::npos) {
@@ -41,11 +55,11 @@ bool CNotificationPushover::SendMessageImplementation(
 		}
 	}
 
-	if (Sound != "") {
-		sPostData << "&sound=" << Sound;
+	if (cSound != "") {
+		sPostData << "&sound=" << cSound;
 	}
 
-	if (Priority == 2) {
+	if (cPriority == 2) {
 		sPostData << "&retry=300&expire=3600";
 	}
 	std::vector<std::string> ExtraHeaders;
