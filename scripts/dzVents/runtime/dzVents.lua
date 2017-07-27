c64ebd2533981facd6e4a0ecab583399cba69ec5
Danny Bloemendaal <danny@bloemeland.nl>
2017-07-27 15:43:04 +0200

    removed debugging code.

--- scripts/dzVents/runtime/dzVents.lua
+++ scripts/dzVents/runtime/dzVents.lua
@@ -22,9 +22,6 @@ if (tonumber(globalvariables['dzVents_log_level']) == utils.LOG_DEBUG) then
 	local persistence = require('persistence')
 	persistence.store(currentPath .. '/domoticzData.lua', domoticzData)
 end
---
-local persistence = require('persistence')
-persistence.store(currentPath .. '/domoticzData.lua', domoticzData)
 
 commandArray = {}
 
