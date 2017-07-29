707a0b1d3f8c37a267b4341360a769ad13ac49f9
dnpwwo <kendel.boul@gmail.com>
2017-07-29 21:23:03 +1000

    Revert "Final commit"
    This reverts commit 3a06549f3bc23b908ee412f9de41c102a30a5daf.

--- plugins/Kodi/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-# File intentionally left blank
--- plugins/SNMP/pysnmp/carrier/asynsock/dgram/unix.py
+++
@@ -1,7 +0,0 @@
-#
-# This file is part of pysnmp software.
-#
-# Copyright (c) 2005-2017, Ilya Etingof <etingof@gmail.com>
-# License: http://pysnmp.sf.net/license.html
-#
-from pysnmp.carrier.asyncore.dgram.unix import *
