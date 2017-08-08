9fc9096359f21431e5c03d8db1c78bd59cccc8d3
Bram Gerritsen <bgerritsen@gmail.com>
2017-08-08 20:23:32 +0200

    - Make the code more DRY - Move all device settings and configuration settings to top - Add possibility to disable certain sensors - Add LUX sensor - Add dry run option - Add the option to deactive the automatic script by a custom switch

--- scripts/dzVents/examples/sunscreen.lua
+++ scripts/dzVents/examples/sunscreen.lua
@@ -1,73 +1,102 @@
+local sensors = {
+    temperature = {
+        active = true,
+        device = 'Temperature',
+        closeRule = function(device)
+            return device.temperature <= 15
+        end
+    },
+    wind = {
+        active = true,
+        device = 'Wind',
+        closeRule = function(device)
+            return device.speed >= 50 or device.gust >= 150
+        end
+    },
+    rain = {
+        active = true,
+        device = 'Rain',
+        closeRule = function(device)
+            return device.rain > 0
+        end
+    },
+    uv = {
+        active = true,
+        device = 'UV',
+        closeRule = function(device)
+            return device.uv <= 3
+        end
+    },
+    lux = {
+        active = false,
+        device = 'Lux',
+        closeRule = function(device)
+            return device.lux <= 500
+        end
+    }
+}
+
+local sunscreenDevice = 'Sunscreen'
+local dryRun = true
+-- local manualOverrideSwitch = 'Disable auto sunscreen'
+
 return {
     active = true,
     on = {
         timer = {'every minute'}
     },
+    logging = {
+        level = domoticz.LOG_DEBUG,
+        marker = 'Sunscreen'
+    },
     execute = function(domoticz)
-        -- Define your devices
-        local sunscreen = domoticz.devices('Sunscreen')
-        local temperature = domoticz.devices('Temperature')
-        local wind = domoticz.devices('Wind')
-        local rain = domoticz.devices('Rain')
-        local uv = domoticz.devices('UV')
-        
-        -- Tresholds
-        local minTemperature = 15
-        local maxWindgust = 150
-        local maxWindspeed = 50
-        local minUv = 3
-        
-        -- Sunscreen must be closed at nighttime
+
+        if (manualOverrideSwitch ~= nil and domoticz.devices(manualOverrideSwitch).state == 'On') then
+            return
+        end
+
+        local sunscreen = domoticz.devices(sunscreenDevice)
+
+        -- Sunscreen must always be up during nighttime
         if (domoticz.time.isNightTime) then
             if (sunscreen.state == 'Open') then
-                domoticz.log('Sunscreen#Sunscreen up --> It is night')
-                sunscreen.switchOff()
+                if (not dryRun) then
+                    sunscreen.switchOff()
+                end
+                domoticz.log('Closing sunscreen, It is night', domoticz.LOG_INFO)
             end
             return
         end
-        
-        -- Check rain sensor
-        if (rain.rain > 0) then
-            if (sunscreen.state == 'Open') then
-                domoticz.log('Sunscreen#Sunscreen up --> It is raining')
-                sunscreen.switchOff()
+
+        -- Check all sensor tresholds and if any exeeded close sunscreen
+        for sensorType, sensor in pairs(sensors) do
+
+            if (sensor['active'] == true) then
+
+                local device = domoticz.devices(sensor['device'])
+                local closeRule = sensor['closeRule']
+
+                domoticz.log('Checking sensor: ' .. sensorType, domoticz.LOG_DEBUG)
+
+                if (closeRule(device) and sunscreen.state == 'Open') then
+                    if (not dryRun) then
+                        sunscreen.switchOff()
+                    end
+                    domoticz.log(sensorType .. ' treshold exceeded, closing sunscreen', domoticz.LOG_INFO)
+                    -- Return early when we exeed any tresholds
+                    return
+                end
+            else
+                domoticz.log('Sensor not active skipping: ' .. sensorType, domoticz.LOG_DEBUG)
             end
-            return
         end
-        
-        -- Check UV sensor
-        if (uv.uv <= minUv) then
-            if (sunscreen.state == 'Open') then
-                domoticz.log('Sunscreen#Sunscreen up --> Sunshine not too bright')
-                sunscreen.switchOff()
-            end
-            return
-        end
-        
-        -- Check temperature sensor
-        if (temperature.temperature <= minTemperature) then
-            if (sunscreen.state == 'Open') then
-                domoticz.log('Sunscreen#Sunscreen up --> It is too cold')
-                sunscreen.switchOff()
-            end
-            return
-        end
-        
-        -- Check wind sensor
-        if (wind.speed >= maxWindspeed or wind.gust >= maxWindgust) then
-            if (sunscreen.state == 'Open') then
-                domoticz.log('Sunscreen#Sunscreen up --> Windspeed too high')
-                sunscreen.switchOff()
-            end
-            return
-        end
-        
-        -- All thresholds OK, lower sunscreen
+
+        -- All tresholds OK, sunscreen may be lowered
         if (sunscreen.state == 'Closed') then
-            domoticz.log('Sunscreen#Sun is shining, all thresholds OK, lowering sunscreen')
-            sunscreen.switchOn()
-        else
-            domoticz.log('Sunscreen#Sunscreen OK --> No action')
+            domoticz.log('Sun is shining, all thresholds OK, lowering sunscreen')
+            if (not dryRun) then
+                sunscreen.switchOn()
+            end
         end
     end
 }
