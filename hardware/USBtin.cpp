5b8dfc32c5551ea73ae7ea42a108bf29dd7df1b8
Xavier <xavierponcetb@wanadoo.fr>
2017-10-26 19:18:17 +0200

    [dev] save W

--- hardware/USBtin.cpp
+++ hardware/USBtin.cpp
@@ -24,6 +24,40 @@ History :
 #include <boost/bind.hpp>
 #include <ctime>
 
+#define USBTIN_BAUD_RATE         115200
+#define USBTIN_PARITY            boost::asio::serial_port_base::parity::none
+#define USBTIN_CARACTER_SIZE      8
+#define USBTIN_FLOW_CONTROL      boost::asio::serial_port_base::flow_control::none
+#define USBTIN_STOP_BITS         boost::asio::serial_port_base::stop_bits::one
+
+#define	TIME_3sec				3000000
+#define	TIME_1sec				1000000
+#define	TIME_500ms				500000
+#define	TIME_200ms				200000
+#define	TIME_100ms				100000
+#define	TIME_10ms				10000
+#define	TIME_5ms				5000
+
+#define	USBTIN_CR							0x0D
+#define	USBTIN_BELSIGNAL					0x07
+#define	USBTIN_FIRMWARE_VERSION				0x76
+#define	USBTIN_HARDWARE_VERSION				0x56
+#define	USBTIN_SERIAL_NUMBER				0x4E
+#define	USBTIN_EXT_TRAME_RECEIVE			0x54
+#define	USBTIN_NOR_TRAME_RECEIVE			0x74
+#define	USBTIN_EXT_RTR_TRAME_RECEIVE		0x52
+#define	USBTIN_NOR_RTR_TRAME_RECEIVE		0x72
+#define USBTIN_GOODSENDING_NOR_TRAME		0x5A
+#define USBTIN_GOODSENDING_EXT_TRAME		0x7A
+
+#define	USBTIN_FW_VERSION	"v"
+#define	USBTIN_HW_VERSION	"V"
+#define USBTIN_RETRY_DELAY  30
+
+#define NoCanDefine		0x00
+#define	Multibloc_V8	0x01
+#define FreeCan			0x02
+
 using namespace std;
 
 USBtin::USBtin(const int ID, const std::string& devname,unsigned int BusCanType,unsigned int DebugMode) :
