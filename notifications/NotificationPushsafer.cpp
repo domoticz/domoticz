fff958145d195803dc6b8b39945979cbe496b4ef
Kevin Siml <appzer@users.noreply.github.com>
2017-11-12 15:07:58 +0100

    Update NotificationPushsafer.cpp

--- notifications/NotificationPushsafer.cpp
+++ notifications/NotificationPushsafer.cpp
@@ -87,7 +87,7 @@ bool CNotificationPushsafer::SendMessageImplementation(
 			}
 			else
 			{
-				_log.Log(LOG_ERROR, "Pushsafer: can't download image > wrong url");
+				_log.Log(LOG_STATUS, "Pushsafer: can't download image > wrong url");
 				sPostData << "&k=" << _apikey << "&d=" << PS_d << "&i=" << PS_i << "&s=" << PS_s << "&v=" << PS_v << "&t=" << PS_t << "&m=" << PS_m << "&u=" << PS_u << "&ut=" << PS_ut << "&l=" << PS_l;
 			}
 		}
@@ -98,22 +98,15 @@ bool CNotificationPushsafer::SendMessageImplementation(
 	}
 	else
 	{
-		if (std::to_string(_apiuser) != '')
+		if (HTTPClient::GETBinary(CURLEncode::URLDecode(_apiuser.c_str()), ExtraHeadersBinary, camimage, 10))
 		{
-			if (HTTPClient::GETBinary(CURLEncode::URLDecode(_apiuser.c_str()), ExtraHeadersBinary, camimage, 10))
-			{
-				std::string base64ImageString(camimage.begin(), camimage.end());
-				base64ImageString = base64_encode((const unsigned char*)base64ImageString.c_str(), base64ImageString.size());
-				sPostData << "i=12&k=" << _apikey << "&t=" << cSubject << "&m=" << Text << "&p=data:image/jpeg;base64," << base64ImageString;
-			}
-			else
-			{
-				_log.Log(LOG_ERROR, "Pushsafer: can't download image > wrong url");
-				sPostData << "i=12&k=" << _apikey << "&t=" << cSubject << "&m=" << Text;
-			}
+			std::string base64ImageString(camimage.begin(), camimage.end());
+			base64ImageString = base64_encode((const unsigned char*)base64ImageString.c_str(), base64ImageString.size());
+			sPostData << "i=12&k=" << _apikey << "&t=" << cSubject << "&m=" << Text << "&p=data:image/jpeg;base64," << base64ImageString;
 		}
 		else
 		{
+			_log.Log(LOG_STATUS, "Pushsafer: can't download image > wrong url");
 			sPostData << "i=12&k=" << _apikey << "&t=" << cSubject << "&m=" << Text;
 		}
 	}
