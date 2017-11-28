e0783fdd04e5bc6bf631ba642d96e4584b8beb97
Joost van den Broek <jvandenbroek@gmail.com>
2017-11-28 22:03:38 +0100

    HardwareMonitor monitor own process memory usage

--- hardware/HardwareMonitor.cpp
+++ hardware/HardwareMonitor.cpp
@@ -72,7 +72,6 @@ extern std::string szInternalVoltageCommand;
 extern bool bHasInternalCurrent;
 extern std::string szInternalCurrentCommand;
 
-
 #define round(a) ( int ) ( a + .5 )
 
 CHardwareMonitor::CHardwareMonitor(const int ID)
@@ -367,6 +366,14 @@ void CHardwareMonitor::UpdateSystemSensor(const std::string& qType, const int di
 		float curr = static_cast<float>(atof(devValue.c_str()));
 		SendCurrent(doffset + dindex, curr, devName);
 	}
+#if defined (__linux__)
+	else if (qType == "Process")
+	{
+		doffset = 1500;
+		float usage = static_cast<float>(atof(devValue.c_str()));
+		SendCustomSensor(0, doffset + dindex, 255, usage, devName, "MB");
+	}
+#endif
 	return;
 }
 
@@ -532,6 +539,32 @@ void CHardwareMonitor::RunWMIQuery(const char* qTable, const std::string &qType)
 			(((double) tp.tv_usec) * 0.000001 );
 	}
 
+#if defined(__linux__)
+	float CHardwareMonitor::GetProcessMemUsage()
+	{
+		pid_t pid = getpid();
+		std::stringstream sPid;
+		sPid << pid;
+		std::ifstream mfile("/proc/" + sPid.str() + "/status");
+		if (!mfile.is_open())
+			return -1;
+		uint32_t VmRSS = -1;
+		uint32_t VmSwap = -1;
+		std::string token;
+		while (mfile >> token)
+		{
+			if (token == "VmRSS:")
+				mfile >> VmRSS;
+			else if (token == "VmSwap:")
+				mfile >> VmSwap;
+
+			// ignore rest of the line
+			mfile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+		return (VmRSS + VmSwap) / 1000.f;
+	}
+#endif
+
 	float GetMemUsageLinux()
 	{
 #if defined(__FreeBSD__)
@@ -629,6 +662,14 @@ void CHardwareMonitor::RunWMIQuery(const char* qTable, const std::string &qType)
 #endif
 		sprintf(szTmp,"%.2f",memusedpercentage);
 		UpdateSystemSensor("Load", 0, "Memory Usage", szTmp);
+#ifdef __linux__
+		float memProcess = GetProcessMemUsage();
+		if (memProcess != -1)
+		{
+			sprintf(szTmp, "%.2f", memProcess);
+			UpdateSystemSensor("Process", 0, "Process Usage", szTmp);
+		}
+#endif
 	}
 
 	void CHardwareMonitor::FetchUnixCPU()
