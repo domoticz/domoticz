7a79fdb36b88b590efa5f15e423e302240b933d8
Danny Bloemendaal <danny@bloemeland.nl>
2017-12-05 20:58:47 +0100

    Switch light on when setting the Kelvin level

--- dzVents/runtime/device-adapters/rgbw_device.lua
+++ dzVents/runtime/device-adapters/rgbw_device.lua
@@ -21,6 +21,9 @@ return {
 		if (device.deviceSubType == 'RGBWW') then
 			function device.setKelvin(kelvin)
 				local url
+				if (not device.active) then
+					device.switchOn()
+				end
 				url = domoticz.settings['Domoticz url'] ..
 						'/json.htm?type=command&param=setkelvinlevel&idx=' .. device.id .. '&kelvin=' .. tonumber(kelvin)
 				return domoticz.openURL(url)
