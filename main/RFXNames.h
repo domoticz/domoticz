8d9c975ff6c02e9fd4cda24e689a5e7212a9c966
dotms <github@dotms.nl>
2017-12-28 19:11:47 +0100

    Moved Nest OAuthAPI enum entry to a free spot.

--- main/RFXNames.h
+++ main/RFXNames.h
@@ -184,10 +184,9 @@ enum _eHardwareTypes {
 	HTYPE_RelayNet,				//100
 	HTYPE_KMTronicUDP,			//101
 	HTYPE_SysfsGpio,			//102
-	HTYPE_Rtl433,                           //103
+	HTYPE_Rtl433,               //103
 	HTYPE_OnkyoAVTCP,			//104
-	HTYPE_SysfsGPIO,			//102
-	HTYPE_Nest_OAuthAPI,		//103
+	HTYPE_Nest_OAuthAPI,		//105
 	HTYPE_END
 };
 
