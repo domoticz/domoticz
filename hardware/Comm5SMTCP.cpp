88faa600c875ad0e2ba192f5846c4ccd0037a25a
Thiago A. Correa <thiago.correa@gmail.com>
2018-01-17 18:35:24 -0200

    Add comments to cpp to describe the hardware and provide URL to it.

--- hardware/Comm5SMTCP.cpp
+++ hardware/Comm5SMTCP.cpp
@@ -9,8 +9,6 @@
 
 #include <iostream>
 
-#include <boost/lexical_cast.hpp>
-
 /* 
     This driver allows Domoticz to read sensor data from SM-XXXX sensors from Comm5 Technology
 	The SM-1200 sensor provides: Temperature, Humidity, Barometric and Luminosity data. 
