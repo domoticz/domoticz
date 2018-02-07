f07b51af0b52374138c39f2b7c5dd7db444d9451
Spugna85 <Spugna85@users.noreply.github.com>
2018-02-07 08:19:23 +0100

    OpenWebNet: change wait time between energy totalizer request (#2104)
    * OpenWebNet: bug fix and burglar alarm battery, network and maintenance status and fix warnings
    
    Fix wrong automatic creation of auxiliary device and update status of
    dry contact, introduced with the development of CEN PLUS commands.
    
    'Unit' must be left free for the interface, so the alarm zone is used as
    the 'where' and not as 'interface' in UpdateAlarm function.
    After this update, the alarm device will have a different ID.
    Adding devices to battery status, network status and alarm maintenance.
    
    * OpenWebNet: fix close socket command problem
    
    * OpenWebNet: GEN, AREA and GROUP devices

--- hardware/OpenWebNetTCP.cpp
+++ hardware/OpenWebNetTCP.cpp
@@ -36,6 +36,9 @@ License: Public domain
 #define OPENWEBNET_BUFFER_SIZE          1024
 #define OPENWEBNET_SOCKET_SUCCESS       0
 
+#define	SCAN_TIME_REQ_AUTO_UPDATE_POWER	(14400) // 4hour = 240min = 14400sec
+#define SCAN_TIME_REQ_ENERGY_TOTALIZER	(900)	// 15min = 900sec
+
 #define OPENWEBNET_GROUP_ID				0x00008000
 
 #define OPENWEBNET_AUTOMATION					"AUTOMATION"
@@ -50,7 +53,7 @@ License: Public domain
 #define OPENWEBNET_AUXILIARY					"AUXILIARY"
 #define OPENWEBNET_DRY_CONTACT					"DRYCONTACT"
 #define OPENWEBNET_ENERGY_MANAGEMENT			"ENERGY MANAGEMENT"
-
+#define OPENWEBNET_SOUND_DIFFUSION				"SOUND DIFFUSION"
 
 /**
     Create new hardware OpenWebNet instance
@@ -85,6 +88,7 @@ bool COpenWebNetTCP::StartHardware()
 	m_stoprequested = false;
 	m_bIsStarted = true;
 	mask_request_status = 0x1; // Set scan all devices
+	LastScanTimeEnergy = LastScanTimeEnergyTot = 0;	// Force first request command
 
 	//Start monitor thread
 	m_monitorThread = boost::shared_ptr<boost::thread>(new boost::thread(boost::bind(&COpenWebNetTCP::MonitorFrames, this)));
@@ -1075,11 +1079,13 @@ void COpenWebNetTCP::UpdateDeviceValue(vector<bt_openwebnet>::iterator iter)
 		case WHO_SCENARIO_SCHEDULER_SWITCH:             // 15
 		case WHO_AUDIO:                                 // 16
 		case WHO_SCENARIO_PROGRAMMING:                  // 17
+		case WHO_SOUND_DIFFUSION:						// 22
 		case WHO_LIHGTING_MANAGEMENT:                   // 24
 		case WHO_ZIGBEE_DIAGNOSTIC:                     // 1000
 		case WHO_AUTOMATIC_DIAGNOSTIC:                  // 1001
 		case WHO_THERMOREGULATION_DIAGNOSTIC_FAILURES:  // 1004
 		case WHO_DEVICE_DIAGNOSTIC:                     // 1013
+		case WHO_ENERGY_MANAGEMENT_DIAGNOSTIC:			// 1018
 			_log.Log(LOG_ERROR, "COpenWebNetTCP: Who=%s not yet supported!", who.c_str());
 			return;
 		default:
@@ -1564,14 +1570,13 @@ void COpenWebNetTCP::Do_Work()
 
 		sleep_seconds(OPENWEBNET_HEARTBEAT_DELAY);
 
-		if ((mytime(NULL) - LastScanTimeEnergy) > 300)
+		if ((mytime(NULL) - LastScanTimeEnergy) > SCAN_TIME_REQ_AUTO_UPDATE_POWER)
 		{
-			
-			requestAutomaticUpdatePower(300 / 60);
+			requestAutomaticUpdatePower(255); // automatic update for 255 minutes
 			LastScanTimeEnergy = mytime(NULL);
 		}
 
-		if ((mytime(NULL) - LastScanTimeEnergyTot) > 30)
+		if ((mytime(NULL) - LastScanTimeEnergyTot) > SCAN_TIME_REQ_ENERGY_TOTALIZER)
 		{
 			requestEnergyTotalizer();
 			LastScanTimeEnergyTot = mytime(NULL);
