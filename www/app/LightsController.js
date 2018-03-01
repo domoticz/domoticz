d1f9521538a1b14330375a90c53aa01950dbb219
lechunlx <lechubox@o2.pl>
2018-03-01 17:14:07 +0100

    Update LightsController.js

--- www/app/LightsController.js
+++ www/app/LightsController.js
@@ -3670,7 +3670,7 @@ define(['app'], function (app) {
 				$("#dialog-addmanuallightdevice #lighting2params").show();
 				$("#dialog-addmanuallightdevice #lighting3params").show();
 			}
-			else if ((lighttype >= 200) && (lighttype < 300) && (lighttype != 250)) {
+			else if ((lighttype >= 200) && (lighttype < 300) && (lighttype != 250) && (lighttype != 226)) {
 				//Blinds
 				$("#dialog-addmanuallightdevice #blindsparams").show();
 				var bShow1 = (lighttype == 205) || (lighttype == 206) || (lighttype == 207) || (lighttype == 210) || (lighttype == 211);
@@ -3986,14 +3986,24 @@ define(['app'], function (app) {
 				var unitcode = "0";
 				mParams += "&id=" + ID.toUpperCase() + "&unitcode=" + unitcode;
 			}
-			else if ((lighttype == 250) || (lighttype == 226)) {
+			else if (lighttype == 226) {
 				var ID =
-					$("#dialog-addmanuallightdevice #lightparams2 #combocmd1 option:selected").text() +
+					"00" +
 					$("#dialog-addmanuallightdevice #lightparams2 #combocmd2 option:selected").text() +
 					$("#dialog-addmanuallightdevice #lightparams2 #combocmd3 option:selected").text() +
-					$("#dialog-addmanuallightdevice #lightparams2 #combocmd4 option:selected").text()+
+					$("#dialog-addmanuallightdevice #lightparams2 #combocmd4 option:selected").text() +
 					"00";
-				mParams += "&id=" + ID;
+				mParams += "&id=" + ID.toUpperCase();
+                                mParams += "&unitcode=" + $("#dialog-addmanuallightdevice #lightparams2 #combounitcode option:selected").val();
+                        }
+			else if (lighttype == 250) {
+				var ID =
+					"00" +
+					$("#dialog-addmanuallightdevice #lightparams2 #combocmd2 option:selected").text() +
+					$("#dialog-addmanuallightdevice #lightparams2 #combocmd3 option:selected").text() +
+					$("#dialog-addmanuallightdevice #lightparams2 #combocmd4 option:selected").text() +
+					"00";
+				mParams += "&id=" + ID.toUpperCase();
                                 mParams += "&unitcode=" + $("#dialog-addmanuallightdevice #lightparams3 #combounitcode option:selected").val();
                         }
 			else {
