bf44a6e8cce808e81599ee03560616f1fdc07d7a
Guillaume Zin <guillaume.zin@gmail.com>
2018-03-10 12:10:50 +0100

    Remove useless include in NotificationHelper.cpp

--- notifications/NotificationHelper.cpp
+++ notifications/NotificationHelper.cpp
@@ -3,7 +3,6 @@
 #include "../main/Helper.h"
 #include "../main/SQLHelper.h"
 #include "../main/localtime_r.h"
-#include "../main/RFXtrx.h"
 #include "../hardware/hardwaretypes.h"
 #include "NotificationHelper.h"
 #include "NotificationProwl.h"
