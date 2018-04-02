5f4b685888c3091da89a20e961d2e1e80852cbe3
Danny Bloemendaal <danny@bloemeland.nl>
2018-04-02 15:54:41 +0200

    dzVents 2.4.2 (#2264)
    * Prepare for 2.4.2
    - Fixed RGBW device adapter
    - Fixed EvoHome device adapter
    - Changed param ordering opentherm gateway command (https://www.domoticz.com/forum/viewtopic.php?f=59&t=21620&p=170469#p170469)

--- dzVents/runtime/tests/testUtils.lua
+++ dzVents/runtime/tests/testUtils.lua
@@ -38,7 +38,7 @@ describe('event helpers', function()
 			end
 
 			utils.log('abc', utils.LOG_ERROR)
-			assert.is_same('Error (2.4.1): abc', printed)
+			assert.is_same('Error (2.4.2): abc', printed)
 		end)
 
 		it('shoud log INFO by default', function()
