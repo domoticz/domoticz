a74f7c4a5e7e03d9ff9b9fde0ce07c77a8ccdd8d
Rob Peters <ROb@DVBControl.com>
2018-05-24 11:42:36 +0200

    Added GIF return extension for floorplans

--- main/WebServer.cpp
+++ main/WebServer.cpp
@@ -10802,6 +10802,8 @@ namespace http {
 					oname += ".jpg";
 				else if (result[0][0][0] == 'B')
 					oname += ".bmp";
+				else if (result[0][0][0] == 'G')
+					oname += ".gif";
 			}
 			reply::add_header_attachment(&rep, oname);
 		}
