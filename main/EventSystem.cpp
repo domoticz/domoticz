9d865e8c96b3518f95c2680fec464707cc27ab95
Rob Peters <ROb@DVBControl.com>
2018-06-20 12:02:15 +0200

    Fixed uninitialized variable bug

--- main/EventSystem.cpp
+++ main/EventSystem.cpp
@@ -1942,7 +1942,7 @@ void CEventSystem::EvaluateDatabaseEvents(const _tEventQueue &item)
 			{
 				if (it->Interpreter == "Blockly")
 				{
-					std::size_t found;
+					std::size_t found = std::string::npos;
 					if ((item.reason == REASON_DEVICE) && (item.id > 0))
 					{
 						std::stringstream sstr;
@@ -1969,6 +1969,7 @@ void CEventSystem::EvaluateDatabaseEvents(const _tEventQueue &item)
 						sstr << "variable[" << item.id << "]";
 						found = it->Conditions.find(sstr.str());
 					}
+
 					if (found != std::string::npos)
 						lua_state = ParseBlocklyLua(lua_state, *it);
 				}
