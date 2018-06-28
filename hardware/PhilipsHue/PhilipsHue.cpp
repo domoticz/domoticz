e46055b780482b7a3fdd9ff6ad9b597da429f87a
Rob Peters <info@domoticz.com>
2018-06-28 08:27:24 +0200

    Corrected PhilipsHue code (removed to much)

--- hardware/PhilipsHue/PhilipsHue.cpp
+++ hardware/PhilipsHue/PhilipsHue.cpp
@@ -335,7 +335,7 @@ bool CPhilipsHue::SwitchLight(const int nodeID, const string &LCmd, const int sv
 	if (nodeID < 1000)
 	{
 		//Light
-		std::map<int, _tHueLightState>::iterator ittLight = m_lights.find(nodeID);
+		auto && ittLight = m_lights.find(nodeID);
 		if (ittLight != m_lights.end())
 		{
 			pState = &ittLight->second;
@@ -344,7 +344,7 @@ bool CPhilipsHue::SwitchLight(const int nodeID, const string &LCmd, const int sv
 	else if (nodeID < 2000)
 	{
 		//Group
-		std::map<int, _tHueGroup>::iterator ittGroup = m_groups.find(nodeID - 1000);
+		auto && ittGroup = m_groups.find(nodeID - 1000);
 		if (ittGroup != m_groups.end())
 		{
 			pState = &ittGroup->second.gstate;
@@ -827,18 +827,21 @@ bool CPhilipsHue::GetLights(const Json::Value &root)
 
 			_tHueLightState tlight;
 			_eHueLightType LType;
+			bool bDoSend = true;
 			LightStateFromJSON(light["state"], tlight, LType);
 
-			std::map<int, _tHueLightState>::iterator myLight = m_lights.find(lID);
+			auto myLight = m_lights.find(lID);
 			if (myLight != m_lights.end())
 			{
-				if (!StatesSimilar(myLight->second, tlight))
-				{
-					//_log.Log(LOG_STATUS, "HueBridge state change: tbri = %d, level = %d", tbri, tlight.level);
-					myLight->second = tlight;
-					std::string modelid = light["modelid"].asString();
-					InsertUpdateSwitch(lID, LType, tlight, light["name"].asString(), "", modelid, true);
-				}
+				if (StatesSimilar(myLight->second, tlight))
+					bDoSend = false;
+			}
+			if (bDoSend)
+			{
+				//_log.Log(LOG_STATUS, "HueBridge state change: tbri = %d, level = %d", tbri, tlight.level);
+				m_lights[lID] = tlight;
+				std::string modelid = light["modelid"].asString();
+				InsertUpdateSwitch(lID, LType, tlight, light["name"].asString(), "", modelid, true);
 			}
 		}
 	}
@@ -862,17 +865,20 @@ bool CPhilipsHue::GetGroups(const Json::Value &root)
 
 			_tHueLightState tstate;
 			_eHueLightType LType;
+			bool bDoSend = true;
 			LightStateFromJSON(group["action"], tstate, LType); //TODO: Verify there is no crash with "bad" key
 
-			std::map<int, _tHueGroup>::iterator myGroup = m_groups.find(gID);
+			auto myGroup = m_groups.find(gID);
 			if (myGroup != m_groups.end())
 			{
-				if (!StatesSimilar(myGroup->second.gstate, tstate))
-				{
-					myGroup->second.gstate = tstate;
-					string Name = "Group " + group["name"].asString();
-					InsertUpdateSwitch(1000 + gID, LType, tstate, Name, "", "", m_add_groups);
-				}
+				if (StatesSimilar(myGroup->second.gstate, tstate))
+					bDoSend = false;
+			}
+			if (bDoSend)
+			{
+				m_groups[gID].gstate = tstate;
+				string Name = "Group " + group["name"].asString();
+				InsertUpdateSwitch(1000 + gID, LType, tstate, Name, "", "", m_add_groups);
 			}
 		}
 	}
