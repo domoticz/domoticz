4af9efabe81f03b7fdd3f992e8ac9755987ed3b5
Alexandre Bertrand <alexandre.bertrand@heosys.net>
2018-07-12 20:46:58 +0200

    Update dzVents version to 2.4.7

--- main/dzVents.cpp
+++ main/dzVents.cpp
@@ -16,7 +16,7 @@ extern http::server::CWebServerHelper m_webservers;
 CdzVents CdzVents::m_dzvents;
 
 CdzVents::CdzVents(void) :
-	m_version("2.4.6")
+	m_version("2.4.7")
 {
 	m_bdzVentsExist = false;
 }
