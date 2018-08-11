domoticz/domoticz

commit 0f04a3dddc2350f18a5684ee46f4c1a8cad20b5d

Moved IsWSL to Helper file

main/Helper.cpp
@@ -1137,3 +1137,41 @@ int SetThreadName(std::thread::native_handle_type thread, const char *name)
 #endif
 }
 #endif
+
+#if defined(__linux__)
+bool IsWSL(void)
+{
+	// Detect WSL according to https://github.com/Microsoft/WSL/issues/423#issuecomment-221627364
+	bool is_wsl = false;
+
+	char buf[1024];
+
+	int status_fd = open("/proc/sys/kernel/osrelease", O_RDONLY);
+	if (status_fd == -1)
+		return is_wsl;
+
+	ssize_t num_read = read(status_fd, buf, sizeof(buf) - 1);
+
+	if (num_read > 0)
+	{
+		buf[num_read] = 0;
+		is_wsl |= (strstr(buf, "Microsoft") != NULL);
+		is_wsl |= (strstr(buf, "WSL") != NULL);
+	}
+
+	status_fd = open("/proc/version", O_RDONLY);
+	if (status_fd == -1)
+		return is_wsl;
+
+	num_read = read(status_fd, buf, sizeof(buf) - 1);
+
+	if (num_read > 0)
+	{
+		buf[num_read] = 0;
+		is_wsl |= (strstr(buf, "Microsoft") != NULL);
+		is_wsl |= (strstr(buf, "WSL") != NULL);
+	}
+
+	return is_wsl;
+}
+#endif

main/Helper.h
@@ -77,3 +77,8 @@ int GenerateRandomNumber(const int range);
 int GetDirFilesRecursive(const std::string &DirPath, std::map<std::string, int> &_Files);
 
 int SetThreadName(std::thread::native_handle_type thread, const char *name);
+
+#if defined(__linux__)
+	bool IsWSL(void); //Detects if running under Windows Subsystem for Linux (WSL)
+#endif
+

main/domoticz.cpp
@@ -45,45 +45,6 @@
 	#include <string.h> 
 #endif
 
-#if defined(__linux__)
-static bool IsWSL(void)
-{
-	// Detect WSL according to https://github.com/Microsoft/WSL/issues/423#issuecomment-221627364
-	bool is_wsl = false;
-
-	char buf[1024];
-
-	int status_fd = open("/proc/sys/kernel/osrelease", O_RDONLY);
-	if (status_fd == -1)
-		return is_wsl;
-
-	ssize_t num_read = read(status_fd, buf, sizeof(buf)-1);
-
-	if (num_read > 0)
-	{
-		buf[num_read] = 0;
-		is_wsl |= (strstr(buf, "Microsoft") != NULL);
-		is_wsl |= (strstr(buf, "WSL") != NULL);
-	}
-
-	status_fd = open("/proc/version", O_RDONLY);
-	if (status_fd == -1)
-		return is_wsl;
-
-	num_read = read(status_fd, buf, sizeof(buf)-1);
-
-	if (num_read > 0)
-	{
-		buf[num_read] = 0;
-		is_wsl |= (strstr(buf, "Microsoft") != NULL);
-		is_wsl |= (strstr(buf, "WSL") != NULL);
-	}
-
-	return is_wsl;
-}
-
-#endif
-
 const char *szHelp =
 "Usage: Domoticz -www port\n"
 "\t-version display version number\n"

commit cd40766533b5f5e1169fd7544b718a35b8f2ea82

Fix build error introduced in 0f04a3d

main/Helper.cpp
@@ -37,6 +37,12 @@
 #include <time.h>
 #endif
 
+// Includes for IsWSL()
+#if defined(__linux__) || defined(__linux) || defined(linux)
+#include <fcntl.h>
+#include <unistd.h>
+#endif
+
 void StringSplit(std::string str, const std::string &delim, std::vector<std::string> &results)
 {
 	results.clear();