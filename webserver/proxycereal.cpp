b3a02fb83dfa0befb50af573966d19f129d4a618
Rene Kluwen <rene@kluwen.com>
2018-11-09 18:17:43 +0100

    Serialization functions + test in proxycereal.cpp

--- webserver/proxycereal.cpp
+++ webserver/proxycereal.cpp
@@ -1,8 +1,40 @@
+// for doing the actual serialization
 #include "stdafx.h"
 #include "proxycereal.h"
 
+#define PDUSTRING(name)
+#define PDULONG(name)
+#define PROXYPDU(name, members) case ePDU_##name: { /* start a new scope */ std::shared_ptr<ProxyPdu_##name> pdu(new ProxyPdu_##name()); iarchive(pdu); base = pdu; } break;
+std::shared_ptr<CProxyPduBase> FromString(const std::string &str) {
+	std::stringstream stream(str);
+	cereal::PortableBinaryInputArchive iarchive(stream);
+	std::shared_ptr<CProxyPduBase> base;
+	pdu_enum theEnum;
+	iarchive(theEnum);
+	switch (theEnum) {
+#include "proxydef.def"
+	default:
+		// pdu type not found
+		base = NULL;
+		break;
+	}
+	return base;
+};
+#undef PDUSTRING
+#undef PDULONG
+#undef PROXYPDU
+
 
 void a() {
-	CPDU_ASSIGNKEY a;
-
+	ProxyPdu_ASSIGNKEY b;
+	b.m_newapi = "123";
+	std::string s = b.ToString();
+	int l = s.length();
+	std::shared_ptr<CProxyPduBase> base2 = FromString(s);
+	std::string r = base2->pdu_name();
+	std::shared_ptr<CProxyPduBase> base = FromString(s);
+	std::string t = base->pdu_name();
+	ProxyPdu_ASSIGNKEY *key = dynamic_cast<ProxyPdu_ASSIGNKEY *>(base.get());
+	std::string q = key->m_newapi;
+	int c = 1;
 }
--- webserver/proxycereal.h
+++ webserver/proxycereal.h
@@ -1,26 +1,70 @@
 #pragma once
 
 #include <string>
+#include <sstream>
+// type support
+#include "../cereal/types/common.hpp"
+#include "../cereal/types/map.hpp"
+#include "../cereal/types/vector.hpp"
+#include "../cereal/types/string.hpp"
+#include "../cereal/types/map.hpp"
+#include "../cereal/types/complex.hpp"
+#include "../cereal/types/memory.hpp"
+// Include the polymorphic serialization and registration mechanisms
+#include "../cereal/types/polymorphic.hpp"
+// the archiver
 #include "../cereal/archives/portable_binary.hpp"
+#include <string>
+#include <memory>
 
 #define NOARG
-#define PDUSTRING(name) std::string m_##name;
-#define PDULONG(name) long m_##name;
-#define PROXYPDU(name, members) class C##name##_onlymembers { public: members };
+#define PDUSTRING(name)
+#define PDULONG(name)
+#define PROXYPDU(name, members) ePDU_##name,
+typedef enum enum_pdu {
+#include "proxydef.def"
+ePDU_END
+} pdu_enum;
+#undef PDUSTRING
+#undef PDULONG
+#undef PROXYPDU
+
+class CProxyPduBase {
+public:
+	pdu_enum myEnum;
+	CProxyPduBase() { myEnum = ePDU_NONE; };
+	virtual pdu_enum pdu_type() { return myEnum; };
+	virtual std::string pdu_name() { return "NONE"; };
+	template <class Archive> void serialize(Archive &ar, std::uint32_t const version) { };
+};
+
+#define PDUSTRING(name) std::string m_##name = "";
+#define PDULONG(name) long m_##name = 0;
+#define PROXYPDU(name, members) class ProxyPdu_##name##_onlymembers : public CProxyPduBase { public: members };
+#include "proxydef.def"
+#undef PDUSTRING
+#undef PDULONG
+#undef PROXYPDU
+
+#define PDUSTRING(name) ar & CEREAL_NVP(m_##name);
+#define PDULONG(name) ar & CEREAL_NVP(m_##name);
+#define PROXYPDU(name, members) class ProxyPdu_##name : public ProxyPdu_##name##_onlymembers { public: \
+    ProxyPdu_##name() { myEnum = ePDU_##name ; }; \
+	virtual pdu_enum pdu_type() { return ePDU_##name; }; \
+	virtual std::string pdu_name() { return #name; }; \
+	template <class Archive> void serialize(Archive &ar, std::uint32_t const version) { members }; \
+	std::string ToString() { \
+		std::stringstream os; { /* start a new scope */ \
+		cereal::PortableBinaryOutputArchive oarchive(os); \
+		std::shared_ptr<ProxyPdu_##name##> pt(std::shared_ptr<ProxyPdu_##name##>(this, [](ProxyPdu_##name## *) {})); \
+		oarchive(myEnum); \
+		oarchive(pt); } \
+		return os.str(); \
+	}; \
+};
 
 #include "proxydef.def"
 
 #undef PDUSTRING
 #undef PDULONG
 #undef PROXYPDU
-
-#define PDUSTRING(name) ar & CEREAL_NVP(name);
-#define PDULONG(name) ar & CEREAL_NVP(name);
-#define PROXYPDU(name, members) class C##name## : public C##name##_onlymembers { public: template <class Archive> void Serialize(Archive &ar) { members } };
-
-#include "proxydef.def"
-
-#undef PDUSTRING
-#undef PDULONG
-#undef PROXYPDU
-
