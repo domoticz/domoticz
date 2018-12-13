c7e556ab00875fa06a8d93f1881dea29ec8334a2
Rob Peters <info@domoticz.com>
2018-12-13 08:17:50 +0100

    Added support for NodeOn Soft Button TSB-2-1-01

--- hardware/EnOceanESP3.cpp
+++ hardware/EnOceanESP3.cpp
@@ -1943,77 +1943,114 @@ void CEnOceanESP3::ParseRadioDatagram()
 				unsigned char func = (m_buffer[1] >> 2) & 0x3F;
 				unsigned char type = ((m_buffer[2] >> 3) & 0x1F) | ((m_buffer[1] & 0x03) << 5);
 
-				_log.Log(LOG_NORM, "EnOcean message VLD: func: %02X Type: %02X", func, type);
-				if(func == 0x01)
+				if ((m_buffer[1] == 0x61) || (m_buffer[1] == 0x63))
 				{
-					// D2-01
-					switch(type)
+					_log.Log(LOG_NORM, "EnOcean message VLD %02X", m_buffer[1]);
+
+					unsigned char DATA_BYTE0 = m_buffer[2]; //1 = simple press, 2=double press, 3=long press, 4=press release
+
+					unsigned char ID_BYTE3 = m_buffer[3];
+					unsigned char ID_BYTE2 = m_buffer[4];
+					unsigned char ID_BYTE1 = m_buffer[5];
+					unsigned char ID_BYTE0 = m_buffer[6];
+
+					unsigned long id = (ID_BYTE3 << 24) + (ID_BYTE2 << 16) + (ID_BYTE1 << 8) + ID_BYTE0;
+					SendGeneralSwitch(id, DATA_BYTE0, 255, 1, 0, "Switch", 12);
+					return;
+				}
+				else
+				{
+					_log.Log(LOG_NORM, "EnOcean message VLD: func: %02X Type: %02X", func, type);
+					if (func == 0x01)
 					{
+						// D2-01 Electr. switches/dimmers, Energy Meas. / Local Ctrl
+						switch (type)
+						{
 						case 0x0C:	// D2-01-0C
-									{
-										unsigned char channel = m_buffer[2] & 0x7;
+						{
+							unsigned char channel = m_buffer[2] & 0x7;
 
-										unsigned char dim_power = m_buffer[3] & 0x7F;		// 0=off, 0x64=100%
+							unsigned char dim_power = m_buffer[3] & 0x7F;		// 0=off, 0x64=100%
 
-										unsigned char ID_BYTE3=m_buffer[4];
-										unsigned char ID_BYTE2=m_buffer[5];
-										unsigned char ID_BYTE1=m_buffer[6];
-										unsigned char ID_BYTE0=m_buffer[7];
-										long id = (ID_BYTE3 << 24) + (ID_BYTE2 << 16) + (ID_BYTE1 << 8) + ID_BYTE0;
+							unsigned char ID_BYTE3 = m_buffer[4];
+							unsigned char ID_BYTE2 = m_buffer[5];
+							unsigned char ID_BYTE1 = m_buffer[6];
+							unsigned char ID_BYTE0 = m_buffer[7];
+							long id = (ID_BYTE3 << 24) + (ID_BYTE2 << 16) + (ID_BYTE1 << 8) + ID_BYTE0;
 
-										// report status only if it is a known device else we may have an incorrect profile
-										char szDeviceID[20];
-										std::vector<std::vector<std::string> > result;
-										sprintf(szDeviceID,"%08X",(unsigned int)id);
+							// report status only if it is a known device else we may have an incorrect profile
+							char szDeviceID[20];
+							std::vector<std::vector<std::string> > result;
+							sprintf(szDeviceID, "%08X", (unsigned int)id);
 
-										result = m_sql.safe_query("SELECT ID, Manufacturer, Profile, [Type] FROM EnoceanSensors WHERE (HardwareID==%d) AND (DeviceID=='%q')", m_HwdID, szDeviceID);
-										if (result.size()<1)
-										{
-											_log.Log(LOG_NORM, "EnOcean: Need Teach-In for %s", szDeviceID);
-											return;
-										}
+							result = m_sql.safe_query("SELECT ID, Manufacturer, Profile, [Type] FROM EnoceanSensors WHERE (HardwareID==%d) AND (DeviceID=='%q')", m_HwdID, szDeviceID);
+							if (result.size() < 1)
+							{
+								_log.Log(LOG_NORM, "EnOcean: Need Teach-In for %s", szDeviceID);
+								return;
+							}
 
-										RBUF tsen;
-										memset(&tsen,0,sizeof(RBUF));
-										tsen.LIGHTING2.packetlength=sizeof(tsen.LIGHTING2)-1;
-										tsen.LIGHTING2.packettype=pTypeLighting2;
-										tsen.LIGHTING2.subtype=sTypeAC;
-										tsen.LIGHTING2.seqnbr=0;
+							RBUF tsen;
+							memset(&tsen, 0, sizeof(RBUF));
+							tsen.LIGHTING2.packetlength = sizeof(tsen.LIGHTING2) - 1;
+							tsen.LIGHTING2.packettype = pTypeLighting2;
+							tsen.LIGHTING2.subtype = sTypeAC;
+							tsen.LIGHTING2.seqnbr = 0;
 
-										tsen.LIGHTING2.id1=(BYTE)ID_BYTE3;
-										tsen.LIGHTING2.id2=(BYTE)ID_BYTE2;
-										tsen.LIGHTING2.id3=(BYTE)ID_BYTE1;
-										tsen.LIGHTING2.id4=(BYTE)ID_BYTE0;
-										tsen.LIGHTING2.level=dim_power;
-										tsen.LIGHTING2.rssi=12;
+							tsen.LIGHTING2.id1 = (BYTE)ID_BYTE3;
+							tsen.LIGHTING2.id2 = (BYTE)ID_BYTE2;
+							tsen.LIGHTING2.id3 = (BYTE)ID_BYTE1;
+							tsen.LIGHTING2.id4 = (BYTE)ID_BYTE0;
+							tsen.LIGHTING2.level = dim_power;
+							tsen.LIGHTING2.rssi = 12;
 
-										tsen.LIGHTING2.unitcode = channel + 1;
-										tsen.LIGHTING2.cmnd     = (dim_power>0) ? light2_sOn : light2_sOff;
+							tsen.LIGHTING2.unitcode = channel + 1;
+							tsen.LIGHTING2.cmnd = (dim_power > 0) ? light2_sOn : light2_sOff;
 
 #ifdef ENOCEAN_BUTTON_DEBUG
-										_log.Log(LOG_NORM, "EnOcean message: 0x%02X Node 0x%08x UnitID: %02X cmd: %02X ",
-											DATA_BYTE3,
-											id,
-											tsen.LIGHTING2.unitcode,
-											tsen.LIGHTING2.cmnd
-										);
+							_log.Log(LOG_NORM, "EnOcean message: 0x%02X Node 0x%08x UnitID: %02X cmd: %02X ",
+								DATA_BYTE3,
+								id,
+								tsen.LIGHTING2.unitcode,
+								tsen.LIGHTING2.cmnd
+							);
 #endif //ENOCEAN_BUTTON_DEBUG
 
-										// Never learn device from D2-01-0C because subtype may be incorrect
-										sDecodeRXMessage(this, (const unsigned char *)&tsen.LIGHTING2, NULL, 255);
+							// Never learn device from D2-01-0C because subtype may be incorrect
+							sDecodeRXMessage(this, (const unsigned char *)&tsen.LIGHTING2, NULL, 255);
 
-										// Note: if a device uses simultaneously RPS and VLD (ex: nodon inwall module), it can be partially initialized.
-										//			Domoticz will show device status but some functions may not work because EnoceanSensors table has no info on this device (until teach-in is performed)
-										//       If a device has local control (ex: nodon inwall module with physically attached switched), domoticz will record the local control as unit 0.
-										//       Ex: nodon inwall 2 channels will show 3 entries. Unit 0 is the local switch, 1 is the first channel, 2 is the second channel.
-										//			(I only have attached a switch on the first channel, I have no idea which unit number a switch on the 2nd channel will have)
-										return;
-									}
-									break;
+							// Note: if a device uses simultaneously RPS and VLD (ex: nodon inwall module), it can be partially initialized.
+							//			Domoticz will show device status but some functions may not work because EnoceanSensors table has no info on this device (until teach-in is performed)
+							//       If a device has local control (ex: nodon inwall module with physically attached switched), domoticz will record the local control as unit 0.
+							//       Ex: nodon inwall 2 channels will show 3 entries. Unit 0 is the local switch, 1 is the first channel, 2 is the second channel.
+							//			(I only have attached a switch on the first channel, I have no idea which unit number a switch on the 2nd channel will have)
+							return;
+						}
+						break;
+						}
+					}
+					else if (func == 0x02)
+					{
+						// D2-02 Temp. Sensor, Light, Occupancy, SmokeType
+					}
+					else if (func == 0x03)
+					{
+						// D2-03
+						switch (type)
+						{
+						case 0x00:	// D3-03-00 Light, Switching and Blind Control Type
+							break;
+						case 0x0A:	// D3-03-0A Push Button – Single Button
+							while (1 == 0);
+							break;
+						case 0x10:	// D3-03-10 Mechanical Handle
+							break;
+						case 0x20:	// D3-03-20 Beacon with Vibration Detection
+							break;
+						}
 					}
 				}
 			}
-
 		default:
 			_log.Log(LOG_NORM, "EnOcean: Unhandled RORG (%02x)", m_buffer[0]);
 			break;
