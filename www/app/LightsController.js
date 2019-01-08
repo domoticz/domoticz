4f6b4a69e416bd950549e871bfb4d6a6d2b3a5f4
gizmocuz <gizmocuz@users.noreply.github.com>
2019-01-08 09:11:51 +0100

    recorded lighttypes in logical order

--- www/app/LightsController.js
+++ www/app/LightsController.js
@@ -1951,7 +1951,7 @@ define(['app'], function (app) {
 				}
 				else {
 					$("#dialog-addmanuallightdevice #lighting2params #combocmd1").hide();
-					if ((lighttype == 57) || (lighttype == 65) || (lighttype == 100) || (lighttype == 55)) {
+					if ((lighttype == 55) || (lighttype == 57) || (lighttype == 65) || (lighttype == 100)) {
 						$("#dialog-addmanuallightdevice #lighting2params #combocmd2").hide();
 						$("#dialog-addmanuallightdevice #lighting2params #combocmd2").val(0);
 						if ((lighttype != 65) && (lighttype != 100) && (lighttype != 55)) {
