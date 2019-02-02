9f1b22677ed1fe25d67606db1e18bfd18a665f72
Daniel Evans <dd_evans@hotmail.com>
2019-02-02 16:15:43 +0000

    Added Evohome SYNC command

--- hardware/EvohomeRadio.cpp
+++ hardware/EvohomeRadio.cpp
@@ -46,8 +46,8 @@ enum evoCommands
 	cmdExternalSensor = 0x0002,
 	cmdDeviceInfo = 0x0418,
 	cmdBatteryInfo = 0x1060,
+	cmdSync = 0x1F09,
 	//0x10a0 //DHW settings sent between controller and DHW sensor can also be requested by the gateway <1:DevNo><2(uint16_t):SetPoint><1:Overrun?><2:Differential>
-	//0x1F09 //Unknown fixed message FF070D
 	//0x0005
 	//0x0006
 	//0x0404
@@ -102,6 +102,7 @@ CEvohomeRadio::CEvohomeRadio(const int ID, const std::string &UserContID)
 	RegisterDecoder(cmdExternalSensor, boost::bind(&CEvohomeRadio::DecodeExternalSensor, this, _1));
 	RegisterDecoder(cmdDeviceInfo, boost::bind(&CEvohomeRadio::DecodeDeviceInfo, this, _1));
 	RegisterDecoder(cmdBatteryInfo, boost::bind(&CEvohomeRadio::DecodeBatteryInfo, this, _1));
+	RegisterDecoder(cmdSync, boost::bind(&CEvohomeRadio::DecodeSync, this, _1));
 }
 
 
@@ -1613,6 +1614,28 @@ bool CEvohomeRadio::DecodeBatteryInfo(CEvohomeMsg &msg)
 	return true;
 }
 
+bool CEvohomeRadio::DecodeSync(CEvohomeMsg &msg) //0x1F09
+{
+	char tag[] = "SYNC"; //these messages communicate the time until the next scheduled controller message sequence which allows devices to sleep
+
+	if (msg.payloadsize != 1 && msg.payloadsize != 3)
+	{
+		Log(false, LOG_ERROR, "evohome: %s: unexpected payload size: %d", tag, msg.payloadsize);
+		return false;
+	}
+	else
+	{
+		if (msg.payloadsize == 1)
+		{
+			Log(true, LOG_STATUS, "evohome: %s: Type 0x%02x", msg.payload[0]);
+		}
+		else
+		{
+			Log(true, LOG_STATUS, "evohome: %s: Type 0x%02x (%d)", msg.payload[0], msg.payload[1] << 8 | msg.payload[2]);
+		}
+		return true;
+	}
+}
 
 void CEvohomeRadio::AddSendQueue(const CEvohomeMsg &msg)
 {
