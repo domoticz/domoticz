4db378a40198f9a3576a418678b50009ca1f9123
Rob Peters <ROb@DVBControl.com>
2019-02-06 12:44:15 +0100

    Removed boost UUID dependency in project

--- webserver/cWebem.cpp
+++ webserver/cWebem.cpp
@@ -1326,30 +1326,6 @@ namespace http {
 			reply::add_header(&rep, "Set-Cookie", sstr.str(), false);
 		}
 
-
-		const std::string hexCHARS = "0123456789abcdef";
-		std::string GenerateUUID() // DCE/RFC 4122
-		{
-			std::srand((unsigned int)std::time(nullptr));
-			std::string uuid = std::string(36, ' ');
-
-			uuid[8] = '-';
-			uuid[13] = '-';
-			uuid[14] = '4'; //M
-			uuid[18] = '-';
-			//uuid[19] = ' '; //N Variant 1 UUIDs (10xx N=8..b, 2 bits)
-			uuid[23] = '-';
-
-			for (size_t ii = 0; ii < uuid.size(); ii++)
-			{
-				if (uuid[ii] == ' ')
-				{
-					uuid[ii] = hexCHARS[(ii == 19) ? (8 + (std::rand() & 0x03)) : std::rand() & 0x0F];
-				}
-			}
-			return uuid;
-		}
-
 		std::string cWebemRequestHandler::generateSessionID()
 		{
 			// Session id should not be predictable
