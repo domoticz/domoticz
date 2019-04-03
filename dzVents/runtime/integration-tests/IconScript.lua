1e12fb4d981ed345ad14aeb9fb99156814cbbaf9
Danny Bloemendaal <danny@bloemeland.nl>
2019-04-03 07:49:20 +0200

    Update IconScript.lua
    Fixed white space and indenting. Please keep things tidy!

--- dzVents/runtime/integration-tests/IconScript.lua
+++ dzVents/runtime/integration-tests/IconScript.lua
@@ -1,8 +1,8 @@
 return {
 	on =    {
-				devices         = { 'vdSetIconSwitch'   },
-				httpResponses   = { 'setIcon'          },
-			},
+		devices  = { 'vdSetIconSwitch' },
+		httpResponses  = { 'setIcon' },
+	},
 
 	execute = function(dz, item, info)
 		
@@ -10,22 +10,20 @@ return {
 		
 		local function getCustomImage()
 			local url = dz.settings['Domoticz url'] ..  '/json.htm?type=devices&rid=' .. item.id
-			dz.openURL  (
-							{
-								url = url,
-								method = 'GET',
-								callback = 'setIcon' ,
-							}
-						).afterSec(4)
+			dz.openURL  ({
+				url = url,
+				method = 'GET',
+				callback = 'setIcon' ,
+			}).afterSec(4)
 		end
-
 
 		if item.isHTTPResponse then
 			rt = item.json.result[1]
-				dz.log("CustomImage".. rt.Name .. " is " .. rt.CustomImage,dz.LOG_FORCE)
-				iconSwitch.setDescription(rt.CustomImage)
+			dz.log("CustomImage".. rt.Name .. " is " .. rt.CustomImage,dz.LOG_FORCE)
+			iconSwitch.setDescription(rt.CustomImage)
 			return
 		end
+		
 		getCustomImage()
 	end
 }
