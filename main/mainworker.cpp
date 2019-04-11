bc04367cc08e037a17e444a5795bdd10eef358a2
dextercd <dexter.c.dopping@gmail.com>
2019-04-11 23:07:02 +0200

    fix comparisons

--- main/mainworker.cpp
+++ main/mainworker.cpp
@@ -10965,7 +10965,7 @@ void MainWorker::decode_Weather(const int HwdID, const _eHardwareTypes HwdType, 
 		}
 		pRFXDevice->SendWind(windID, BatteryLevel, intDirection, (float)intSpeed, (float)intGust, temp, chill, true, bHaveChill, procResult.DeviceName, SignalLevel);
 
-		if (subType = sTypeWEATHER2)
+		if (subType == sTypeWEATHER2)
 		{
 			int Humidity = (int)pResponse->WEATHER.humidity;
 			unsigned char HumidityStatus = pResponse->WEATHER.humidity_status;
@@ -10976,7 +10976,7 @@ void MainWorker::decode_Weather(const int HwdID, const _eHardwareTypes HwdType, 
 		}
 
 		//Rain
-		if ((subType = sTypeWEATHER1) || (subType = sTypeWEATHER2))
+		if ((subType == sTypeWEATHER1) || (subType == sTypeWEATHER2))
 		{
 			float TotalRain = 0;
 
@@ -10992,14 +10992,14 @@ void MainWorker::decode_Weather(const int HwdID, const _eHardwareTypes HwdType, 
 		}
 
 		//UV
-		if (subType = sTypeWEATHER2)
+		if (subType == sTypeWEATHER2)
 		{
 			float UV = (float)pResponse->WEATHER.uv;
 			pRFXDevice->SendUVSensor(windID, 1, BatteryLevel, UV, procResult.DeviceName, SignalLevel);
 		}
 
 		//Solar
-		if (subType = sTypeWEATHER2)
+		if (subType == sTypeWEATHER2)
 		{
 			float radiation = (float)((pResponse->WEATHER.solarhigh * 256) + pResponse->WEATHER.solarlow);
 			pRFXDevice->SendSolarRadiationSensor(windID, BatteryLevel, radiation, procResult.DeviceName);
