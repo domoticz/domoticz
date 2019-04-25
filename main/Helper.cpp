a58b8d672f057dca75215442514960bf439e232a
Thiago A. Correa <thiago.correa@gmail.com>
2019-04-24 23:04:14 -0300

    Add serial port for Freescale imx6 devices

--- main/Helper.cpp
+++ main/Helper.cpp
@@ -194,7 +194,7 @@ std::vector<std::string> GetSerialPorts(bool &bUseDirectPath)
 	}
 
 #else
-	//scan /dev for /dev/ttyUSB* or /dev/ttyS* or /dev/tty.usbserial* or /dev/ttyAMA* or /dev/ttySAC*
+	//scan /dev for /dev/ttyUSB* or /dev/ttyS* or /dev/tty.usbserial* or /dev/ttyAMA* or /dev/ttySAC* or /dev/ttymxc*
 	//also scan /dev/serial/by-id/* on Linux
 
 	bool bHaveTtyAMAfree=false;
@@ -238,6 +238,11 @@ std::vector<std::string> GetSerialPorts(bool &bUseDirectPath)
 				ret.push_back("/dev/" + fname);
 			}
 			else if (fname.find("ttySAC") != std::string::npos)
+			{
+				bUseDirectPath = true;
+				ret.push_back("/dev/" + fname);
+			}
+			else if (fname.find("ttymxc") != std::string::npos)
 			{
 				bUseDirectPath = true;
 				ret.push_back("/dev/" + fname);
