3bf5489bb0501ad7870ae746e3fd508ca38bed16
Rens van Waardenberg <Rens.vanWaardenberg@gmail.com>
2019-05-04 19:55:33 +0200

    fix lay-out 	modified:   dzVents/runtime/HTTPResponse.lua 	modified:   dzVents/runtime/misc/testdzVents.sh

--- dzVents/runtime/HTTPResponse.lua
+++ dzVents/runtime/HTTPResponse.lua
@@ -3,48 +3,49 @@ local utils = require('Utils')
 
 local function HTTPResponce(domoticz, responseData)
 
-    local self = {}
-     
-    self.headers = responseData.headers or {}
-    self.baseType = domoticz.BASETYPE_HTTP_RESPONSE
-    self.data = responseData.data or nil
-    self.statusCode = _.get(responseData, {'statusCode'}, 0)
-    self._contentType = _.get(self.headers, {'Content-Type'}, '')
-    
-    self.isJSON = false
+	local self = {}
+	 
+	self.headers = responseData.headers or {}
+	self.baseType = domoticz.BASETYPE_HTTP_RESPONSE
+	self.data = responseData.data or nil
 
-    if self.headers.status then
-        self.statusCode = tonumber((self.headers.status):match("%s+(%S+)")) 
-    end
-    
-    self.ok = false
-    if (self.statusCode >= 200 and self.statusCode <= 299) then
-        self.ok = true
-    end
+	self.statusCode = _.get(responseData, {'statusCode'}, 0)
+	self._contentType = _.get(self.headers, {'Content-Type'}, '')
 
-    self.isHTTPResponse = true
-    self.isDevice = false
-    self.isScene = false
-    self.isGroup = false
-    self.isTimer = false
-    self.isVariable = false
-    self.isSecurity = false
+	self.isJSON = false
 
-    self.callback = responseData.callback
-    self.trigger = responseData.callback
+	if self.headers.status then
+		self.statusCode = tonumber((self.headers.status):match("%s+(%S+)"))
+	end
 
-    if (string.match(self._contentType, 'application/json') and self.data) then
-        local json = utils.fromJSON(self.data)
+	self.ok = false
+	if (self.statusCode >= 200 and self.statusCode <= 299) then
+		self.ok = true
+	end
 
-        if (json) then
-            self.isJSON = true
-            self.json = json
-        end
-    end
+	self.isHTTPResponse = true
+	self.isDevice = false
+	self.isScene = false
+	self.isGroup = false
+	self.isTimer = false
+	self.isVariable = false
+	self.isSecurity = false
 
-    utils.log('HTTPResponse: headers = ' .. _.str(self.headers), utils.LOG_DEBUG)
+	self.callback = responseData.callback
+	self.trigger = responseData.callback
 
-    return self
+	if (string.match(self._contentType, 'application/json') and self.data) then
+		local json = utils.fromJSON(self.data)
+
+		if (json) then
+			self.isJSON = true
+			self.json = json
+		end
+	end
+
+	utils.log('HTTPResponse: headers = ' .. _.str(self.headers), utils.LOG_DEBUG)
+
+	return self
 end
 
 return HTTPResponce
