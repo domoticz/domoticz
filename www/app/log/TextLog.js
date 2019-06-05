2ca5f48f5273c6464e3ad1aad315ef6664d61406
Olifant1990 <25199588+Olifant1990@users.noreply.github.com>
2019-06-05 15:15:32 +0200

    Line endings supported in text log (#3297)
    * Line endings supported in text log

--- www/app/log/TextLog.js
+++ www/app/log/TextLog.js
@@ -20,7 +20,11 @@ define(['app', 'log/components/DeviceTextLogTable'], function (app) {
                 type: 'textlog',
                 idx: vm.deviceIdx
             }).then(function (data) {
-                vm.log = data.result
+                for (var i = 0; i < data.result.length; i++) {
+                    var dataTemp = data.result[i]['Data'].replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1<br />$2');
+					data.result[i]['Data'] = dataTemp;
+                }
+                vm.log = data.result;
             });
         }
 
