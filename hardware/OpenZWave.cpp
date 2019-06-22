65b67b2112cfc9a45b66abcb0d9d043f3b8dce5a
Peter Gebruers <pgdev4@gmail.com>
2019-06-22 18:22:56 +0200

    Fixes for "Unknown" in Name field. Fix Lux value scale error. Both issues are related to the update of OpenZWave Commit 4478eea2 by Justin Hammond, 06/17/2019 introducing improvements to the MultiSensor CC

--- hardware/OpenZWave.cpp
+++ hardware/OpenZWave.cpp
@@ -1510,6 +1510,10 @@ void COpenZWave::AddValue(const OpenZWave::ValueID& vID, const NodeInfo* pNodeIn
 	std::string vLabel = m_pManager->GetValueLabel(vID);
 	std::string vUnits = m_pManager->GetValueUnits(vID);
 
+	if (vLabel.compare("Unknown") == 0){
+		_log.Log(LOG_STATUS, "OpenZWave: Value_Added: Warning: OpenZWave returned ValueLabel \"Unknown\" on Node: %d (0x%02x), CommandClass: %s, Label: %s, Instance: %d, Index: %d", static_cast<int>(NodeID), static_cast<int>(NodeID), cclassStr(commandclass), vLabel.c_str(), vOrgInstance, vOrgIndex);
+	}
+
 	if (commandclass == COMMAND_CLASS_CONFIGURATION)
 	{
 #ifdef _DEBUG
@@ -1814,7 +1818,10 @@ void COpenZWave::AddValue(const OpenZWave::ValueID& vID, const NodeInfo* pNodeIn
 		}
 		else if (vOrgIndex == ValueID_Index_SensorMultiLevel::Luminance)
 		{
-			if (vUnits != "lux")
+			// TODO decide what to do with sensors that report %.
+			// - Do we now what 100% means? Is it always 1000 Lux?
+			// - This comparison is pretty stable, but is it stable enough? (vUnits == "%")
+			if (vUnits == "%")
 			{
 				//convert from % to Lux (where max is 1000 Lux)
 				fValue = (1000.0f / 100.0f) * fValue;
