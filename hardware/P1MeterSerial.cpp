84a7829bf2efb9908f9c3df3db32d476ee61f728
gordonb3 <gordon@bosvangennip.nl>
2019-09-05 17:27:54 +0200

    P1MeterBase: incorrect match on number value 1 instead of char value '1'

--- hardware/P1MeterSerial.cpp
+++ hardware/P1MeterSerial.cpp
@@ -24,24 +24,23 @@
 
 #ifdef DEBUG_P1_R
 //Belgium
-const char *szP1Test=R"p1_test(!DA4F
-/FLU5\253770234_A
+const char *szP1Test=R"p1_test(/FLU5\253770234_A
 
 0-0:96.1.4(50213)
 0-0:96.1.1(3153414731313030303037313930)
 0-0:1.0.0(190905142315S)
-1-0:1.8.1(000244.844kWh)
-1-0:1.8.2(000226.027kWh)
-1-0:2.8.1(000139.553kWh)
-1-0:2.8.2(000045.390kWh)
+1-0:1.8.1(000244.844*kWh)
+1-0:1.8.2(000226.027*kWh)
+1-0:2.8.1(000139.553*kWh)
+1-0:2.8.2(000045.390*kWh)
 0-0:96.14.0(0001)
-1-0:1.7.0(00.000kW)
-1-0:2.7.0(00.198kW)
-1-0:32.7.0(240.0V)
-1-0:31.7.0(002A)
+1-0:1.7.0(00.000*kW)
+1-0:2.7.0(00.198*kW)
+1-0:32.7.0(240.0*V)
+1-0:31.7.0(002*A)
 0-0:96.3.10(1)
-0-0:17.0.0(999.9kW)
-1-0:31.4.0(999A)
+0-0:17.0.0(999.9*kW)
+1-0:31.4.0(999*A)
 0-0:96.13.0()
 0-1:24.1.0(003)
 0-1:96.1.1(37464C4F32313139303137303532)
@@ -150,8 +149,7 @@ bool P1MeterSerial::StartHardware()
 	sOnConnected(this);
 
 #ifdef DEBUG_P1_R
-	//Why does this not work ?
-	//ParseP1Data((const uint8_t*)szP1Test, static_cast<int>(strlen(szP1Test)), m_bDisableCRC, m_ratelimit);
+	ParseP1Data((const uint8_t*)szP1Test, static_cast<int>(strlen(szP1Test)), m_bDisableCRC, m_ratelimit);
 #endif
 
 	return true;
