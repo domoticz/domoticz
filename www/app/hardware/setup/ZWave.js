8961b767c9f5974e0ae495fd1f69b42c3c25f729
Rob Peters <info@domoticz.com>
2019-11-04 09:08:00 +0100

    OZW, using Base64 encoder that supports Unicode as some configuration files contain Unicode

--- www/app/hardware/setup/ZWave.js
+++ www/app/hardware/setup/ZWave.js
@@ -501,7 +501,7 @@ define(['app'], function (app) {
 
 				$list.each(function () {
 					var id = $(this).prop("id");      // get id
-					var value = encodeURIComponent(btoa($(this).prop("value")));      // get value
+					var value = encodeURIComponent(Base64.encode($(this).prop("value")));      // get value
 
 					valueList += id + "_" + value + "_";
 				});
@@ -514,7 +514,7 @@ define(['app'], function (app) {
 				// Now loop through list of controls
 				$list.each(function () {
 					var id = $(this).prop("id");      // get id
-					var value = encodeURIComponent(btoa($(this).find(":selected").text()));      // get value
+					var value = encodeURIComponent(Base64.encode($(this).find(":selected").text()));      // get value
 					valueList += id + "_" + value + "_";
 				});
 			}
