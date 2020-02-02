f6af0fa0c71278a1fb3a655fadb1252376cd4bb6
Rob Peters <info@domoticz.com>
2020-02-02 12:21:53 +0100

    YouLess making sure S0 has a value in the JSON output, else try the ASCII

--- hardware/YouLess.cpp
+++ hardware/YouLess.cpp
@@ -168,11 +168,15 @@ bool CYouLess::GetP1Details()
 	if (!root["cs0"].empty())
 	{
 		//S0 Meter
+		double mcntr = root["cs0"].asDouble();
 		double musage = root["ps0"].asDouble();
-		SendKwhMeter(m_HwdID, 1, 255, musage, root["cs0"].asDouble(), "S0");
-		m_bHaveP1OrS0 = true;
+		if (mcntr != 0)
+		{
+			SendKwhMeter(m_HwdID, 1, 255, musage, mcntr, "S0");
+			m_bHaveP1OrS0 = true;
+		}
 	}
-	return true;
+	return m_bHaveP1OrS0;
 }
 
 void CYouLess::GetMeterDetails()
@@ -180,8 +184,6 @@ void CYouLess::GetMeterDetails()
 	if (m_bCheckP1 || m_bHaveP1OrS0)
 	{
 		if (GetP1Details())
-			return;
-		if (m_bHaveP1OrS0)
 			return;
 	}
 
@@ -218,7 +220,12 @@ void CYouLess::GetMeterDetails()
 		pcurrent=pcurrent.substr(0,fpos);
 	stdreplace(pcurrent,",","");
 
-	m_meter.powerusage=atol(pusage.c_str());
-	m_meter.usagecurrent=atol(pcurrent.c_str());
-	sDecodeRXMessage(this, (const unsigned char *)&m_meter, NULL, 255);
+	unsigned long lpusage = atol(pusage.c_str());
+	unsigned long lpcurrent = atol(pcurrent.c_str());
+	if (lpusage)
+	{
+		m_meter.powerusage = lpusage;
+		m_meter.usagecurrent = lpcurrent;
+		sDecodeRXMessage(this, (const unsigned char*)&m_meter, NULL, 255);
+	}
 }
