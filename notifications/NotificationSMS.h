ac9584c0bc843950d3be3cab5307b473f05435d7
Rob Peters <info@domoticz.com>
2020-03-22 10:24:01 +0100

    Corrected whitespace errors, small formatting

--- notifications/NotificationSMS.h
+++ notifications/NotificationSMS.h
@@ -9,12 +9,12 @@ public:
 protected:
 	virtual bool SendMessageImplementation(
 		const uint64_t Idx,
-		const std::string &Name,
-		const std::string &Subject,
-		const std::string &Text,
-		const std::string &ExtraData,
+		const std::string& Name,
+		const std::string& Subject,
+		const std::string& Text,
+		const std::string& ExtraData,
 		const int Priority,
-		const std::string &Sound,
+		const std::string& Sound,
 		const bool bFromNotification);
 private:
 	/* config vars */
