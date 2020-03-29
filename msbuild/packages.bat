be3289fbd40f6b83351c0eb79dad9a8497c5d6c2
Rob Peters <info@domoticz.com>
2020-03-29 11:41:44 +0200

    Call VCPKG integrate

--- msbuild/packages.bat
+++ msbuild/packages.bat
@@ -3,5 +3,5 @@ git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
 git pull
 call bootstrap-vcpkg.bat -disableMetrics
-rem vcpkg integrate project (This is already done for you!)
+vcpkg integrate project
 vcpkg install boost cereal curl jsoncpp lua minizip mosquitto openssl pthreads sqlite3 zlib
