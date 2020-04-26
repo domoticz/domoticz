af3017acba54eca53e8152f1c9ae499f7decf67c
kiddigital <kiddigital@gmail.com>
2020-04-26 11:21:05 +0200

    Fixed compiler warning on incorrect logline

--- hardware/Buienradar.cpp
+++ hardware/Buienradar.cpp
@@ -395,7 +395,7 @@ void CBuienRadar::GetMeterDetails()
 	int stationID = root["stationid"].asInt();
 	if (stationID != m_iStationID)
 	{
-		Log(LOG_ERROR, "Invalid data received (invalid stationid) or no data returned!", stationID, m_iStationID);
+		Log(LOG_ERROR, "Invalid data received (invalid stationid %i / %i ) or no data returned!", stationID, m_iStationID);
 		return;
 	}
 
