920f5ab01d115dfa7d4345974459764ee7d26a20
p-hagen <hagenpcjm@gmailcom>
2020-08-28 18:18:22 +0200

    Annatherm now accepts external changes for presets

--- hardware/AnnaThermostat.h
+++ hardware/AnnaThermostat.h
@@ -39,6 +39,7 @@ private:
 	std::string m_ThermostatID;
 	std::string m_ProximityID;
 	std::string m_PresetID;
+	std::float_t m_Preset_Value;
 	std::shared_ptr<std::thread> m_thread;
 	AnnaLocationInfo m_AnnaLocation;
 };
