a7a98312c9db4fde78de3badc9c893da3fca8641
p-hagen <hagenpcjm@gmailcom>
2020-09-02 09:10:09 +0200

    Enable Anna preset to be updated automaticlly

--- hardware/AnnaThermostat.cpp
+++ hardware/AnnaThermostat.cpp
@@ -701,16 +701,16 @@ void CAnnaThermostat::GetMeterDetails()
 				std::vector<std::vector<std::string> > result;
 				std::string options_str = m_sql.FormatDeviceOptions(m_sql.BuildDeviceOptions("SelectorStyle:0;LevelNames:Off|Home|Away|Night|Vacation|Frost;LevelOffHidden:true;LevelActions:00|10|20|30|40|50", false));
 				result = m_sql.safe_query("SELECT ID , sValue, Options FROM DeviceStatus WHERE (HardwareID==%d) AND (DeviceID=='%08X') AND (Unit == '%d')", m_HwdID, sAnnaPresets, xcmd.unitcode);
-   	            int bUpdateOptions = -1;
+   				int bUpdateOptions = -1;
 				 	if (!result.empty()){
 						bUpdateOptions = strcmp (options_str.c_str(), result[0][2].c_str());
 							if(bUpdateOptions != 0)
-							{ 
+							{
 								Log(LOG_STATUS, "The layout of %s has been changed - Updating..", PresetName.c_str());
 								//Log(LOG_STATUS, "New value is : %s", result[0][2].c_str());
-               					//Log(LOG_STATUS, "Old value is : %s", result[0][2].c_str());
-               				}
-			        }
+			   					//Log(LOG_STATUS, "Old value is : %s", result[0][2].c_str());
+			   				}
+					}
 				m_mainworker.PushAndWaitRxMessage(this, (const unsigned char *)&xcmd,  PresetName.c_str(), 255);
 				if (result.empty() || bUpdateOptions != 0 )//Switch is new or has older look and feel  so update it
 				{
