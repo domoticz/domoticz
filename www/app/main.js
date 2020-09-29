df95cdf15d8adeb7ea1661d95d2b39090a03bc35
m1ch4 <-->
2020-09-29 22:47:54 +0200

    Update angular 1.6.9 -> 1.8.0

--- www/app/main.js
+++ www/app/main.js
@@ -5,7 +5,7 @@ require.config({
 		'angular-route': '../js/angular-route.min',
 		'angular-animate': '../js/angular-animate.min',
 		'ngSanitize': '../js/angular-sanitize.min',
-		'angular-md5': '../js/angular-md5-min',
+		'angular-md5': '../js/angular-md5.min',
 		'ng-grid': '../js/ng-grid.min',
 		'ng-grid-flexible-height': '../js/ng-grid-flexible-height',
 		'highcharts-ng': '../js/highcharts-ng.min',
