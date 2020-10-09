d3853be4e699e8a678a6b9bd8837533bdac12f85
kiddigital <kiddigital@gmail.com>
2020-10-09 12:51:22 +0200

    Small fixes and UI updates (still not perfect and fully working as intended :( )

--- www/app/ForecastController.js
+++ www/app/ForecastController.js
@@ -15,17 +15,43 @@ define(['app'], function (app) {
 			}).then(function successCallback(response) {
 				var data = response.data;
 				var htmlcontent = '<b>Error loading config or incorrect config data!</b>';
+
 				if (typeof data.status != 'undefined' && data.status == 'OK') {
-					if (typeof data.Forecasturl != 'undefined' && data.Forecasturl != '') {
-						htmlcontent = data.Forecasturl;
-					} else if (typeof data.Forecastdata != 'undefined' ) {
-						htmlcontent = 'Using forecastdevice ' + data.Forecastdata;
-					} else {
-						htmlcontent = 'Neither a forecast URL or valid forecast data provided!';
+					var fallbackurl = "//forecast.io/embed/#lat=" + data.Latitude + "&lon=" + data.Longitude + "&units=ca&color=#00aaff";
+					var fallbackhtml = '<iframe style="background: #fff; height:245px;" class="cIFrame" id="IMain" src="' + fallbackurl + '"></iframe>';
+					if (typeof data.Forecastdata != 'undefined' ) {
+						if (data.Forecastdevicetype == 83) { // OpenWeatherMap
+							htmlcontent = 'Using forecastdevice ' + data.Forecastdata;
+							htmlcontent = '<script src="//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/d3.min.js"></script>';
+							htmlcontent += '<script>window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];';
+							htmlcontent += 'window.myWidgetParam.push({id: 21,cityid: ' + data.Forecastdata.cityid + ',appid: "' + data.Forecastdata.appid + '",units: "metric",containerid: "openweathermap-widget-21",  });';
+							htmlcontent += ' (function() {var script = document.createElement("script");script.async = true;script.charset = "utf-8";script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";';
+							htmlcontent += 'var s = document.getElementsByTagName("script")[0];s.parentNode.insertBefore(script, s);  })();</script>';
+							$('#openweathermap-widget-21').html(htmlcontent);
+							$('#openweathermap-widget-21').i18n();
+							$scope.showOWM = true;
+						} else {
+							htmlcontent = 'Forecastdata for devicetype ' + data.Forecastdevicetype + ' is not supported!';
+							$scope.showFallback = true;
+						}
+					} else if (typeof data.Forecasturl != 'undefined' && data.Forecasturl != '') {
+						if (data.Forecasturl.substr(0,4) == 'http') {
+							htmlcontent = '<iframe class="cIFrame" id="IMain" src="' + data.Forecasturl + '"></iframe>';
+						} else {
+							htmlcontent = data.Forecasturl;
+						}
+						$scope.showFallback = true;
+					} else  {
+						htmlcontent = fallbackhtml;
+						$scope.showFallback = true;
 					}
+					$('#fallback').html(htmlcontent);
+					$('#fallback').i18n();
+				} else {
+					$scope.showFallback = true;
+					$('#fallback').html(htmlcontent);
+					$('#fallback').i18n();
 				}
-				$('#maincontent').html(htmlcontent);
-				$('#maincontent').i18n();
 			});
 		};
 	}]);
