0f0c9197cfa4801878be84b84387087e6794bb14
brucemiranda <brucemiranda@gmail.com>
2021-02-12 22:04:06 +0000

    Update LightsController.js

--- www/app/LightsController.js
+++ www/app/LightsController.js
@@ -271,8 +271,8 @@ define(['app', 'livesocket'], function (app) {
 				strstatus = "Economy";
 			else if (strstatus == "DayOff")//FIXME better way to convert?
 				strstatus = "Day Off";
-                        else if (strstatus == "DayOffWithEco")//FIXME better way to convert?
-                                strstatus = "Day Off With Eco";
+			else if (strstatus == "DayOffWithEco")//FIXME better way to convert?
+				strstatus = "Day Off With Eco";
  			else if (strstatus == "HeatingOff")//FIXME better way to convert?
 				strstatus = "Heating Off";
 			return strstatus;
