5e673c1272e9f9af9fdf7c5e58d952c8ae4c0e87
dnpwwo <kendel.boul@gmail.com>
2021-06-15 10:22:37 +1000

    Bugfix: Domoticz crashes when 'listening' connections don't have an event target specified

--- hardware/plugins/PluginTransports.cpp
+++ hardware/plugins/PluginTransports.cpp
@@ -260,7 +260,8 @@ namespace Plugins {
 			pConnection->Protocol = ((CConnection*)m_pConnection)->Protocol;
 			Py_INCREF(pConnection->Protocol);
 			pConnection->Target = ((CConnection *)m_pConnection)->Target;
-			Py_INCREF(pConnection->Target);
+			if (pConnection->Target)
+				Py_INCREF(pConnection->Target);
 			pConnection->pPlugin = ((CConnection *)m_pConnection)->pPlugin;
 
 			// Add it to the plugins list of connections
@@ -683,7 +684,8 @@ namespace Plugins {
 			pConnection->Protocol = ((CConnection*)m_pConnection)->Protocol;
 			Py_INCREF(pConnection->Protocol);
 			pConnection->Target = ((CConnection *)m_pConnection)->Target;
-			Py_INCREF(pConnection->Target);
+			if (pConnection->Target)
+				Py_INCREF(pConnection->Target);
 			pConnection->pPlugin = ((CConnection *)m_pConnection)->pPlugin;
 
 			// Create Protocol object to handle connection's traffic
