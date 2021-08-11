031c00ac9b20fc1a57658a6b0bdccee761371330
Mark <mark@dotnetniet.com>
2021-08-11 13:02:00 +0200

    Missing closing sign

--- main/SQLHelper.cpp
+++ main/SQLHelper.cpp
@@ -2773,7 +2773,7 @@ bool CSQLHelper::OpenDatabase()
 		}
 		if (dbversion < 148)
 		{
-			if(!DoesColumnExistsInTable("LogLevel", "Hardware")
+			if(!DoesColumnExistsInTable("LogLevel", "Hardware"))
 			{
 				query("ALTER TABLE Hardware ADD COLUMN [LogLevel] INTEGER DEFAULT 7"); // LOG_NORM + LOG_STATUS + LOG_ERROR
 			}
