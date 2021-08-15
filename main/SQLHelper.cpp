0bba183f9d741647cb37ce17458795b62cb2553d
gizmocuz <gizmocuz@users.noreply.github.com>
2021-08-15 09:13:59 +0200

    Revert "Updating from dbversion < 134 fails, hardware table already has LogLevel column"

--- main/SQLHelper.cpp
+++ main/SQLHelper.cpp
@@ -2773,10 +2773,7 @@ bool CSQLHelper::OpenDatabase()
 		}
 		if (dbversion < 148)
 		{
-			if(!DoesColumnExistsInTable("LogLevel", "Hardware"))
-			{
-				query("ALTER TABLE Hardware ADD COLUMN [LogLevel] INTEGER DEFAULT 7"); // LOG_NORM + LOG_STATUS + LOG_ERROR
-			}
+			query("ALTER TABLE Hardware ADD COLUMN [LogLevel] INTEGER DEFAULT 7"); // LOG_NORM + LOG_STATUS + LOG_ERROR
 		}
 	}
 	else if (bNewInstall)
