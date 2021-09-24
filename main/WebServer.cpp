b7635f43117346b0260873bd72954d38855c0148
Rob Peters <Info@Domoticz.com>
2021-09-24 08:07:38 +0200

    MQTT-Auto, Correcting blinds status in gui

--- main/WebServer.cpp
+++ main/WebServer.cpp
@@ -9205,13 +9205,17 @@ namespace http
 							root["result"][ii]["Level"] = LastLevel;
 							int iLevel = round((float(maxDimLevel) / 100.0F) * LastLevel);
 							root["result"][ii]["LevelInt"] = iLevel;
-							if (lstatus == "On")
+							if ((iLevel > 0) && (iLevel < maxDimLevel))
 							{
-								lstatus = (switchtype == STYPE_BlindsPercentage) ? "Closed" : "Open";
+								lstatus = std_format("%d %%", iLevel);
+							}
+							else if (lstatus == "On")
+							{
+								lstatus = ((switchtype == STYPE_BlindsPercentage) || (switchtype == STYPE_BlindsPercentageWithStop)) ? "Closed" : "Open";
 							}
 							else if (lstatus == "Off")
 							{
-								lstatus = (switchtype == STYPE_BlindsPercentage) ? "Open" : "Closed";
+								lstatus = ((switchtype == STYPE_BlindsPercentage) || (switchtype == STYPE_BlindsPercentageWithStop)) ? "Open" : "Closed";
 							}
 							else if (lstatus == "Stop")
 							{
@@ -13266,12 +13270,14 @@ namespace http
 							ldata = (ldata == "On") ? "Open" : "Closed";
 							break;
 						case STYPE_BlindsPercentage:
+						case STYPE_BlindsPercentageWithStop:
 							if ((ldata == "On") || (ldata == "Off"))
 							{
 								ldata = (ldata == "On") ? "Closed" : "Open";
 							}
 							break;
 						case STYPE_BlindsPercentageInverted:
+						case STYPE_BlindsPercentageInvertedWithStop:
 							if ((ldata == "On") || (ldata == "Off"))
 							{
 								ldata = (ldata == "On") ? "Open" : "Closed";
