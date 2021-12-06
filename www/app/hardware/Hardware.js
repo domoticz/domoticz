bfff4897db146dcbb0a1b0331ba7ddc8a3d0f09a
Adam Hill <adam@voip-x.co.uk>
2021-12-06 10:32:42 +0000

    Hardware.js: Updated validateInteger to handle negative numbers.
    Also now needs a space at the start of the field name ( if
    supplied ) so that "n "<fldName> can also be used if the fldName
    starts with a vowel sound.

--- www/app/hardware/Hardware.js
+++ www/app/hardware/Hardware.js
@@ -33,22 +33,22 @@ define(['app'], function (app) {
 			var betmsg = "!";
 			if(!fldName)
 				fldName = "number";
-			if(minVal >= 0) {
-				if(maxVal >= 0) {
+			if(typeof minVal == 'number') {
+				if(typeof maxVal == 'number') {
 					betmsg = " between " + minVal + " and " + maxVal + "!";
 				}
 				else {
 					betmsg = " above " + minVal + "!";
 				}
 			}
-			else if(maxVal >= 0) {
+			else if(typeof maxVal == 'number') {
 				betmsg = " below " + maxVal + "!";
 			}
 			if (isNaN(testno)) {
 				msg = 'Please enter a valid ' + fldName + betmsg;
 			}
-			else if(((minVal >= 0)?(testno < minVal):false) || ((maxVal >= 0)?(testno > maxVal):false)) {
-				msg = 'Please enter a ' + fldName + betmsg;
+			else if(((typeof minVal == 'number')?(testno < minVal):false) || ((typeof maxVal == 'number')?(testno > maxVal):false)) {
+				msg = 'Please enter a' + fldName + betmsg;
 			}
 			if (msg != "") {
 				ShowNotify($.t(msg), 2500, true);
@@ -718,7 +718,7 @@ define(['app'], function (app) {
 				}
 				else if ((text.indexOf("Daikin") >= 0)) {
 					Mode1 = $("#hardwarecontent #divdaikin #updatefrequencydaikin").val();
-					if(!validateInteger(Mode1, 300, 10, 3600, "Poll Interval"))
+					if(!validateInteger(Mode1, 300, 10, 3600, " Poll Interval"))
 						return;
 				}
 				if (text.indexOf("Eco Devices") >= 0) {
@@ -2180,7 +2180,7 @@ define(['app'], function (app) {
 				var Mode3 = "";
 				if (text.indexOf("Daikin") >= 0) {
 					Mode1 = $("#hardwarecontent #divdaikin #updatefrequencydaikin").val();
-					if (!validateInteger(Mode1, 300, 10, 3600, "Poll Interval"))
+					if (!validateInteger(Mode1, 300, 10, 3600, " Poll Interval"))
 						return;
 				}
 				if (text.indexOf("MySensors Gateway with MQTT") >= 0) {
