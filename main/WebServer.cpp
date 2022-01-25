domoticz/domoticz

commit f64304efb7c74a7f37f237a06c16c8a5544a14f7

Allow hardware to declare manual switches they are able to handle. (#4923)
* Allow hardware to declare manual switches they are able to handle.
(Implement it for RFPlayer)

main/WebServer.cpp
@@ -3832,8 +3832,19 @@ namespace http
 						int ID = atoi(sd[0].c_str());
 						std::string Name = sd[1];
 						_eHardwareTypes Type = (_eHardwareTypes)atoi(sd[2].c_str());
-
-						if ((Type == HTYPE_RFXLAN) || (Type == HTYPE_RFXtrx315) || (Type == HTYPE_RFXtrx433) || (Type == HTYPE_RFXtrx868) || (Type == HTYPE_EnOceanESP2) ||
+						CDomoticzHardwareBase *pBaseHardware = reinterpret_cast<CDomoticzHardwareBase *>(m_mainworker.GetHardware(ID));
+						Json::Value proot;
+						Json::Reader reader;
+						auto jsonConfiguration = pBaseHardware->GetManualSwitchesJsonConfiguration();
+						bool res = reader.parse(jsonConfiguration, proot);
+						if (!jsonConfiguration.empty() && res)
+						{
+							root["result"][ii]["idx"] = ID;
+							root["result"][ii]["Name"] = Name;
+							root["result"][ii]["config"] = proot;
+							ii++;
+						}
+						else if ((Type == HTYPE_RFXLAN) || (Type == HTYPE_RFXtrx315) || (Type == HTYPE_RFXtrx433) || (Type == HTYPE_RFXtrx868) || (Type == HTYPE_EnOceanESP2) ||
 						    (Type == HTYPE_EnOceanESP3) || (Type == HTYPE_Dummy) || (Type == HTYPE_Tellstick) || (Type == HTYPE_EVOHOME_SCRIPT) ||
 						    (Type == HTYPE_EVOHOME_SERIAL) || (Type == HTYPE_EVOHOME_WEB) || (Type == HTYPE_EVOHOME_TCP) || (Type == HTYPE_RaspberryGPIO) ||
 						    (Type == HTYPE_RFLINKUSB) || (Type == HTYPE_RFLINKTCP) || (Type == HTYPE_ZIBLUEUSB) || (Type == HTYPE_ZIBLUETCP) || (Type == HTYPE_OpenWebNetTCP) ||
@@ -4287,7 +4298,12 @@ namespace http
 				std::string sunitcode;
 				std::string devid;
 
-				if (lighttype < 20)
+				CDomoticzHardwareBase * pBaseHardware = m_mainworker.GetHardware(atoi(hwdid.c_str()));
+				if (pBaseHardware != nullptr && !pBaseHardware->GetManualSwitchesJsonConfiguration().empty())
+				{
+					pBaseHardware->GetManualSwitchParameters(req.parameters, switchtype, lighttype, dtype, subtype, devid, sunitcode);
+				}
+				else if (lighttype < 20)
 				{
 					dtype = pTypeLighting1;
 					subtype = lighttype;
@@ -4820,7 +4836,6 @@ namespace http
 					}
 				}
 				// ----------- If needed convert to GeneralSwitch type (for o.a. RFlink) -----------
-				CDomoticzHardwareBase *pBaseHardware = reinterpret_cast<CDomoticzHardwareBase *>(m_mainworker.GetHardware(atoi(hwdid.c_str())));
 				if (pBaseHardware != nullptr)
 				{
 					if ((pBaseHardware->HwdType == HTYPE_RFLINKUSB) || (pBaseHardware->HwdType == HTYPE_RFLINKTCP))
@@ -4885,7 +4900,73 @@ namespace http
 				std::string sunitcode;
 				std::string devid;
 				std::string StrParam1;
+				
+				CDomoticzHardwareBase *pBaseHardware = m_mainworker.GetHardware(atoi(hwdid.c_str()));
+				if (pBaseHardware == nullptr)
+				{
+					root["message"] = "Hardware does not exists!";
+					return;
+				}
+				if (!pBaseHardware->GetManualSwitchesJsonConfiguration().empty())
+				{
+					pBaseHardware->GetManualSwitchParameters(req.parameters,switchtype, lighttype,  dtype, subtype, devid, sunitcode);
+					// check if switch is unique
+					result = m_sql.safe_query("SELECT Name FROM DeviceStatus WHERE (HardwareID=='%q' AND DeviceID=='%q' AND Unit=='%q' AND Type==%d AND SubType==%d)",
+								  hwdid.c_str(), devid.c_str(), sunitcode.c_str(), dtype, subtype);
+					if (!result.empty())
+					{
+						root["message"] = "Switch already exists!";
+						return;
+					}
 
+					bool bActEnabledState = m_sql.m_bAcceptNewHardware;
+					m_sql.m_bAcceptNewHardware = true;
+					std::string devname;
+					m_sql.UpdateValue(atoi(hwdid.c_str()), devid.c_str(), atoi(sunitcode.c_str()), dtype, subtype, 0, -1, 0, devname);
+					m_sql.m_bAcceptNewHardware = bActEnabledState;
+
+					// set name and switchtype
+					result = m_sql.safe_query("SELECT ID FROM DeviceStatus WHERE (HardwareID=='%q' AND DeviceID=='%q' AND Unit=='%q' AND Type==%d AND SubType==%d)", hwdid.c_str(),
+								  devid.c_str(), sunitcode.c_str(), dtype, subtype);
+					if (result.empty())
+					{
+						root["message"] = "Error finding switch in Database!?!?";
+						return;
+					}
+					std::string ID = result[0][0];
+
+					m_sql.safe_query("UPDATE DeviceStatus SET Used=1, Name='%q', SwitchType=%d WHERE (ID == '%q')", name.c_str(), switchtype, ID.c_str());
+
+					if (lighttype == 407)
+					{
+						// Openwebnet Bus Custom
+						m_sql.safe_query("UPDATE DeviceStatus SET StrParam1='%s' WHERE (ID == '%q')", StrParam1.c_str(), ID.c_str());
+					}
+
+					m_mainworker.m_eventsystem.GetCurrentStates();
+
+					// Set device options
+					m_sql.SetDeviceOptions(atoi(ID.c_str()), m_sql.BuildDeviceOptions(deviceoptions, false));
+
+					if (!maindeviceidx.empty())
+					{
+						if (maindeviceidx != ID)
+						{
+							// this is a sub device for another light/switch
+							// first check if it is not already a sub device
+							result = m_sql.safe_query("SELECT ID FROM LightSubDevices WHERE (DeviceRowID=='%q') AND (ParentID =='%q')", ID.c_str(), maindeviceidx.c_str());
+							if (result.empty())
+							{
+								// no it is not, add it
+								result = m_sql.safe_query("INSERT INTO LightSubDevices (DeviceRowID, ParentID) VALUES ('%q','%q')", ID.c_str(), maindeviceidx.c_str());
+							}
+						}
+					}
+
+					root["status"] = "OK";
+					root["title"] = "AddSwitch";
+					return;
+				}
 #ifdef ENABLE_PYTHON
 				// check if HW is plugin
 				{
@@ -5464,7 +5545,7 @@ namespace http
 				}
 
 				// ----------- If needed convert to GeneralSwitch type (for o.a. RFlink) -----------
-				CDomoticzHardwareBase *pBaseHardware = m_mainworker.GetHardware(atoi(hwdid.c_str()));
+				//CDomoticzHardwareBase *pBaseHardware = m_mainworker.GetHardware(atoi(hwdid.c_str()));
 				if (pBaseHardware != nullptr)
 				{
 					if ((pBaseHardware->HwdType == HTYPE_RFLINKUSB) || (pBaseHardware->HwdType == HTYPE_RFLINKTCP))

hardware/DomoticzHardware.cpp
@@ -23,6 +23,16 @@ bool CDomoticzHardwareBase::CustomCommand(const uint64_t /*idx*/, const std::str
 	return false;
 }
 
+const std::string& CDomoticzHardwareBase::GetManualSwitchesJsonConfiguration() const
+{
+	return "";
+}
+
+void CDomoticzHardwareBase::GetManualSwitchParameters(const std::multimap<std::string, std::string> & /*Parameters*/, _eSwitchType & /*SwitchTypeInOut*/, int & /*LightTypeInOut*/,
+	 int & /*dTypeOut*/, int &dSubTypeOut, std::string & /*devIDOut*/, std::string & /*sUnitOut*/) const
+{
+}
+
 bool CDomoticzHardwareBase::Start()
 {
 	m_iHBCounter = 0;

hardware/DomoticzHardware.h
@@ -19,7 +19,7 @@ class CDomoticzHardwareBase : public StoppableTask
 {
 	friend class MainWorker;
 
-      public:
+	public:
 	CDomoticzHardwareBase();
 	virtual ~CDomoticzHardwareBase() = default;
 
@@ -29,6 +29,9 @@ class CDomoticzHardwareBase : public StoppableTask
 	bool RestartWithDelay(long seconds);
 	virtual bool WriteToHardware(const char *pdata, unsigned char length) = 0;
 	virtual bool CustomCommand(uint64_t idx, const std::string &sCommand);
+	virtual const std::string& GetManualSwitchesJsonConfiguration() const;
+	virtual void GetManualSwitchParameters(const std::multimap<std::string, std::string> &Parameters, _eSwitchType &SwitchTypeInOut, int &LightTypeInOut,
+		int &dTypeOut, int &dSubTypeOut, std::string &devIDOut, std::string &sUnitOut) const;
 
 	void EnableOutputLog(bool bEnableLog);
 

hardware/ZiBlueBase.cpp
@@ -154,21 +154,22 @@ struct _tZiBlueStringIntHelper
 	int gType;
 };
 
-const _tZiBlueStringIntHelper ziblue_switches[] = {
-	{ "VISONIC 433", sSwitchTypeAB400D },
-	{ "VISONIC 868", sSwitchTypeWaveman },
+	const _tZiBlueStringIntHelper ziblue_switches[] = {
+	{ "VISONIC 433", sSwitchTypeVisonic433 },
+	{ "VISONIC 868", sSwitchTypeVisonic868 },
 	{ "CHACON", sSwitchTypeAC },
 	{ "DOMIA", sSwitchTypeARC },
 	{ "X10", sSwitchTypeX10 },
-	{ "X2D 433", sSwitchTypeEMW200 },
-	{ "X2D 868", sSwitchTypeIMPULS },
-	{ "X2D ELEC", sSwitchTypeRisingSun },
-	{ "X2D GAS", sSwitchTypePhilips },
+	{ "X2D 433", sSwitchTypeX2D433 },
+	{ "X2D 868", sSwitchTypeX2D868 },
+	{ "X2D SHUTTER", sSwitchTypeX2DShutter },
+	{ "X2D ELEC", sSwitchTypeX2DElec },
+	{ "X2D GAS", sSwitchTypeX2DGas },
 	{ "RTS", sSwitchTypeRTS },
 	{ "BLYSS", sSwitchTypeBlyss },
-	{ "PARROT", sSwitchTypeEnergenie },
-	{ "KD101", sSwitchTypeEnergenie5 },
-	{ "FS20 868", sSwitchTypeHomeConfort },
+	{ "PARROT", sSwitchTypeParrot },
+	{ "KD101", sSwitchTypeKD101 },
+	{ "FS20 868", sSwitchTypeFS20 },
 	{ "EDISIO", sSwitchTypeForest },
 	{ "JAMMING SIMU", sSwitchTypeRFCustom },
 	{ "JAMMING", -2 },
@@ -223,6 +224,108 @@ void CZiBlueBase::Init()
 	memset(&m_rfbuffer, 0, sizeof(m_rfbuffer));
 }
 
+static std::string jsonConfig=R"([
+	{"idx":"1","name":"Visonic 433MHz","parameters":[{"name":"housecode","display":"House Code","type":"housecode16"},{"name":"unitcode","display":"Unit Code","type":"unitcode16"}]},
+	{"idx":"2","name":"Visonic 868MHz","parameters":[{"name":"housecode","display":"House Code","type":"housecode16"},{"name":"unitcode","display":"Unit Code","type":"unitcode16"}]},
+	{"idx":"3","name":"Chacon",        "parameters":[{"name":"housecode","display":"House Code","type":"housecode16"},{"name":"unitcode","display":"Unit Code","type":"unitcode16"}]},
+	{"idx":"4","name":"Domia",         "parameters":[{"name":"housecode","display":"House Code","type":"housecode16"},{"name":"unitcode","display":"Unit Code","type":"unitcode16"}]},
+	{"idx":"5","name":"X10",           "parameters":[{"name":"housecode","display":"House Code","type":"housecode16"},{"name":"unitcode","display":"Unit Code","type":"unitcode16"}]},
+	{"idx":"6","name":"X2D 433MHz",    "parameters":[{"name":"housecode","display":"House Code","type":"housecode16"},{"name":"unitcode","display":"Unit Code","type":"unitcode16"}]},
+	{"idx":"7","name":"X2D 868MHz",    "parameters":[{"name":"housecode","display":"House Code","type":"housecode16"},{"name":"unitcode","display":"Unit Code","type":"unitcode16"}]},
+	{"idx":"8","name":"X2D SHUTTER",   "parameters":[{"name":"housecode","display":"House Code","type":"housecode16"},{"name":"unitcode","display":"Unit Code","type":"unitcode16"}]},
+	{"idx":"9","name":"X2D ELEC",      "parameters":[{"name":"housecode","display":"House Code","type":"housecode16"},{"name":"unitcode","display":"Unit Code","type":"unitcode16"}]},
+	{"idx":"10","name":"X2D GAS",      "parameters":[{"name":"housecode","display":"House Code","type":"housecode16"},{"name":"unitcode","display":"Unit Code","type":"unitcode16"}]},
+	{"idx":"11","name":"RTS",          "parameters":[{"name":"housecode","display":"House Code","type":"housecode16"},{"name":"unitcode","display":"Unit Code","type":"unitcode16"},{"name":"qualifier","display":"Qualifier","type":"bool"}]},
+	{"idx":"12","name":"Blyss",        "parameters":[{"name":"housecode","display":"House Code","type":"housecode16"},{"name":"unitcode","display":"Unit Code","type":"unitcode16"}]},
+	{"idx":"13","name":"Parrot",       "parameters":[{"name":"housecode","display":"House Code","type":"housecode16"},{"name":"unitcode","display":"Unit Code","type":"unitcode16"}]},
+	{"idx":"16","name":"KD101",        "parameters":[{"name":"housecode","display":"House Code","type":"housecode16"},{"name":"unitcode","display":"Unit Code","type":"unitcode16"}]},
+	{"idx":"22","name":"FS20 868",     "parameters":[{"name":"housecode","display":"House Code","type":"housecode16"},{"name":"unitcode","display":"Unit Code","type":"unitcode16"}]},
+	{"idx":"23","name":"EDISIO",       "parameters":[{"name":"housecode","display":"House Code","type":"housecode16"},{"name":"unitcode","display":"Unit Code","type":"unitcode16"}]}
+	])";
+const std::string &CZiBlueBase::GetManualSwitchesJsonConfiguration() const
+{
+	return jsonConfig;
+}
+void CZiBlueBase::GetManualSwitchParameters(const std::multimap<std::string, std::string> &Parameters, _eSwitchType & SwitchTypeInOut, int & LightTypeInOut,
+										int & dTypeOut, int &dSubTypeOut, std::string &devIDOut, std::string &sUnitOut) const
+{
+	dTypeOut = pTypeGeneralSwitch;
+	switch (LightTypeInOut)
+	{
+		case SEND_VISONIC_PROTOCOL_433:
+			dSubTypeOut = sSwitchTypeVisonic433;
+			break;
+		case SEND_VISONIC_PROTOCOL_868:
+			dSubTypeOut = sSwitchTypeVisonic868;
+			break;
+		case SEND_CHACON_PROTOCOL_433:
+			dSubTypeOut = sSwitchTypeAC;
+			break;
+		case SEND_DOMIA_PROTOCOL_433:
+			dSubTypeOut = sSwitchTypeARC;
+			break;
+		case SEND_X10_PROTOCOL_433:
+			dSubTypeOut = sSwitchTypeX10;
+			break;
+		case SEND_X2D_PROTOCOL_433:
+			dSubTypeOut = sSwitchTypeX2D433;
+			break;
+		case SEND_X2D_PROTOCOL_868:
+			dSubTypeOut = sSwitchTypeX2D868;
+			break;
+		case SEND_X2D_SHUTTER_PROTOCOL_868:
+			dSubTypeOut = sSwitchTypeX2DShutter;
+			break;
+		case SEND_X2D_HA_ELEC_PROTOCOL_868:
+			dSubTypeOut = sSwitchTypeX2DElec;
+			break;
+		case SEND_X2D_HA_GASOIL_PROTOCOL_868:
+			dSubTypeOut = sSwitchTypeX2DGas;
+			break;
+		case SEND_SOMFY_PROTOCOL_433:
+			dSubTypeOut = sSwitchTypeRTS;
+			break;
+		case SEND_BLYSS_PROTOCOL_433:
+			dSubTypeOut = sSwitchTypeBlyss;
+			break;
+		case SEND_PARROT:
+			dSubTypeOut = sSwitchTypeParrot;
+			break;
+		case SEND_KD101_PROTOCOL_433:
+			dSubTypeOut = sSwitchTypeKD101;
+			break;
+		case SEND_FS20_868:
+			dSubTypeOut = sSwitchTypeFS20;
+			break;
+	}
+	auto it = Parameters.find("housecode");
+	if (it != Parameters.end())
+	{
+		int ID = atoi(it->second.c_str());
+		std::stringstream s_strid;
+		s_strid << std::hex << std::setfill('0') << std::setw(8) << ID;
+		devIDOut = s_strid.str();
+	}
+	it = Parameters.find("unitcode");
+	if (it != Parameters.end())
+	{
+		sUnitOut = it->second;
+	}
+	if (LightTypeInOut == SEND_SOMFY_PROTOCOL_433)
+	{
+		it = Parameters.find("qualifier");
+		if (it != Parameters.end())
+		{
+			int id = atoi(devIDOut.c_str());
+			int qualifier = it->second == "true" ? 1 : 0; 
+			id |= qualifier << 16;
+			std::stringstream temp;
+			temp << std::setfill('0') << std::setw(8) << id;
+			devIDOut = temp.str();
+		}
+	}
+}
+
 void CZiBlueBase::OnConnected()
 {
 	Init();
@@ -247,13 +350,13 @@ bool CZiBlueBase::WriteToHardware(const char *pdata, const unsigned char length)
 
 	if (pSwitch->type != pTypeGeneralSwitch)
 		return false;
-	std::string switchtype = GetGeneralZiBlueFromInt(ziblue_switches, pSwitch->subtype);
-	if (switchtype.empty())
+	std::string protocol = GetGeneralZiBlueFromInt(ziblue_switches, pSwitch->subtype);
+	if (protocol.empty())
 	{
 		Log(LOG_ERROR, "trying to send unknown switch type: %d", pSwitch->subtype);
 		return false;
 	}
-	else if (switchtype == "JAMMING")
+	else if (protocol == "JAMMING")
 	{
 		// JAMMING is read only
 		return false;
@@ -315,83 +418,97 @@ bool CZiBlueBase::WriteToHardware(const char *pdata, const unsigned char length)
 		_tOutgoingFrame oFrame;
 
 		std::stringstream sstr;
-		if (switchtype == "X10")
+		if (protocol == "X10")
 		{
 			oFrame.protocol = SEND_X10_PROTOCOL_433;
 			oFrame.ID_1 = (((pSwitch->id - 65) << 4) + (pSwitch->unitcode - 1));
+			// no return state, so change its value now
+			sDecodeRXMessage(this, (const unsigned char *)&pSwitch, "X10", 100, m_Name.c_str());
 		}
-		else if (switchtype == "CHACON")
+		else if (protocol == "CHACON")
 		{
 			oFrame.protocol = SEND_CHACON_PROTOCOL_433;
 			oFrame.ID_1 = (((pSwitch->id - 65) << 4) + (pSwitch->unitcode - 1));
+			// no return state, so change its value now
+			sDecodeRXMessage(this, (const unsigned char *)&pSwitch, "Chacon", 100, m_Name.c_str());
 		}
-		else if (switchtype == "DOMIA")
+		else if (protocol == "DOMIA")
 		{
 			oFrame.protocol = SEND_DOMIA_PROTOCOL_433;
 			oFrame.ID_1 = (((pSwitch->id - 65) << 4) + (pSwitch->unitcode - 1));
+			// no return state, so change its value now
+			sDecodeRXMessage(this, (const unsigned char *)&pSwitch, "Domia", 100, m_Name.c_str());
 		}
-		else if (switchtype == "RTS")
+		else if (protocol == "RTS")
 		{
 			oFrame.protocol = SEND_SOMFY_PROTOCOL_433;
-			oFrame.ID_1 = ((((pSwitch->id&0xFF)) << 4) + (pSwitch->unitcode - 1));
+			oFrame.ID_1 = (((pSwitch->id - 65) << 4) + (pSwitch->unitcode - 1));
 			oFrame.qualifier = (pSwitch->id & 0x00FF0000) != 0 ? 1 : 0;
+			// no return state, so change its value now
+			sDecodeRXMessage(this, (const unsigned char *)&pSwitch, "RTS", 100, m_Name.c_str());
 		}
-		else if (switchtype == "VISONIC 433")
+		else if (protocol == "VISONIC 433")
 		{
 			oFrame.protocol = SEND_VISONIC_PROTOCOL_433;
 			oFrame.ID_1 = (((pSwitch->id - 65) << 4) + (pSwitch->unitcode - 1));
 		}
-		else if (switchtype == "VISONIC 868")
+		else if (protocol == "VISONIC 868")
 		{
 			oFrame.protocol = SEND_VISONIC_PROTOCOL_868;
 			oFrame.ID_1 = (((pSwitch->id - 65) << 4) + (pSwitch->unitcode - 1));
 		}
-		else if (switchtype == "X2D 433")
+		else if (protocol == "X2D 433")
 		{
 			oFrame.protocol = SEND_X2D_PROTOCOL_433;
 			oFrame.ID_1 = (((pSwitch->id - 65) << 4) + (pSwitch->unitcode - 1));
 		}
-		else if (switchtype == "X2D 868")
+		else if (protocol == "X2D 868")
 		{
 			oFrame.protocol = SEND_X2D_PROTOCOL_868;
 			oFrame.ID_1 = (((pSwitch->id - 65) << 4) + (pSwitch->unitcode - 1));
 		}
-		else if (switchtype == "X2D ELEC")
+		else if (protocol == "X2D SHUTTER")
+		{
+			oFrame.protocol = SEND_X2D_SHUTTER_PROTOCOL_868;
+			oFrame.ID_1 = (((pSwitch->id - 65) << 4) + (pSwitch->unitcode - 1));
+		}
+		else if (protocol == "X2D ELEC")
 		{
 			oFrame.protocol = SEND_X2D_HA_ELEC_PROTOCOL_868;
 			oFrame.ID_1 = (((pSwitch->id - 65) << 4) + (pSwitch->unitcode - 1));
 		}
-		else if (switchtype == "X2D GAS")
+		else if (protocol == "X2D GAS")
 		{
 			oFrame.protocol = SEND_X2D_HA_GASOIL_PROTOCOL_868;
 			oFrame.ID_1 = (((pSwitch->id - 65) << 4) + (pSwitch->unitcode - 1));
 		}
-		else if (switchtype == "BLYSS")
+		else if (protocol == "BLYSS")
 		{
 			oFrame.protocol = SEND_BLYSS_PROTOCOL_433;
 			oFrame.ID_1 = ((((pSwitch->id & 0xFF) - 65) << 4) + (pSwitch->unitcode - 1));
 		}
-		else if (switchtype == "PARROT")
+		else if (protocol == "PARROT")
 		{
 			oFrame.protocol = SEND_PARROT;
 			oFrame.ID_1 = (((pSwitch->id - 65) << 4) + (pSwitch->unitcode - 1));
+			sDecodeRXMessage(this, (const unsigned char *)&pSwitch, "Parrot", 100, m_Name.c_str());
 		}
-		else if (switchtype == "KD101")
+		else if (protocol == "KD101")
 		{
 			oFrame.protocol = SEND_KD101_PROTOCOL_433;
 			oFrame.ID_1 = ((((pSwitch->id & 0xFF) - 65) << 4) + (pSwitch->unitcode - 1));
 		}
-		else if (switchtype == "FS20 868")
+		else if (protocol == "FS20 868")
 		{
 			oFrame.protocol = SEND_FS20_868;
 			oFrame.ID_1 = ((((pSwitch->id & 0xFF) - 65) << 4) + (pSwitch->unitcode - 1));
 		}
-		//else if (switchtype == "EDISIO")
-		//{
-		//	oFrame.protocol = SEND_EDISIO;
-		//	oFrame.ID_1 = (((pSwitch->id - 10) << 4) + (pSwitch->unitcode - 1));
-		//}
-		else if (switchtype == "JAMMING SIMU") // active jamming simulation
+		else if (protocol == "EDISIO")
+		{
+			oFrame.protocol = SEND_EDISIO;
+			oFrame.ID_1 = (((pSwitch->id - 10) << 4) + (pSwitch->unitcode - 1));
+		}
+		else if (protocol == "JAMMING SIMU") // active jamming simulation
 		{
 			WriteInt("ZIA++JAMMING SIMULATE\r");
 			return true;
@@ -1021,129 +1138,4 @@ bool CZiBlueBase::ParseBinary(const uint8_t SDQ, const uint8_t *data, size_t len
 	Log(LOG_NORM, "----------------");
 #endif
 	return true;
-}
-void CZiBlueBase::ConvertToGeneralSwitchType(std::string &devid, int &dtype, int &subtype)
-{
-	if (dtype == pTypeLighting1)
-	{
-		dtype = pTypeGeneralSwitch;
-		if (subtype == sTypeIMPULS)
-			subtype = sSwitchTypeIMPULS;
-		if (subtype == sTypeAB400D)
-			subtype = sSwitchTypeAB400D;
-		std::stringstream s_strid;
-		s_strid << std::hex << atoi(devid.c_str());
-		devid = s_strid.str();
-		devid = "000000" + devid;
-	}
-	else if (dtype == pTypeLighting2)
-	{
-		dtype = pTypeGeneralSwitch;
-		if (subtype == sTypeAC)
-			subtype = sSwitchTypeAC;
-		if (subtype == sTypeHEU)
-		{
-			subtype = sSwitchTypeHEU;
-		}
-		if (subtype == sTypeKambrook)
-			subtype = sSwitchTypeKambrook;
-	}
-	else if (dtype == pTypeLighting3)
-	{
-		dtype = pTypeGeneralSwitch;
-		if (subtype == sTypeKoppla)
-			subtype = sSwitchTypeKoppla;
-	}
-	else if (dtype == pTypeLighting4)
-	{
-		dtype = pTypeGeneralSwitch;
-	}
-	else if (dtype == pTypeLighting5)
-	{
-		dtype = pTypeGeneralSwitch;
-		if (subtype == sTypeEMW100)
-		{
-			subtype = sSwitchTypeEMW100;
-			devid = "00" + devid;
-		}
-		if (subtype == sTypeLivolo)
-		{
-			subtype = sSwitchTypeLivolo;
-			devid = "00" + devid;
-		}
-		if (subtype == sTypeLightwaveRF)
-		{
-			subtype = sSwitchTypeLightwaveRF;
-			devid = "00" + devid;
-		}
-		if (subtype == sTypeLivolo1to10)
-		{
-			subtype = sSwitchTypeLivoloAppliance;
-			devid = "00" + devid;
-		}
-		if (subtype == sTypeEurodomest)
-			subtype = sSwitchTypeEurodomest;
-	}
-	else if (dtype == pTypeLighting6)
-	{
-		dtype = pTypeGeneralSwitch;
-		subtype = sSwitchTypeBlyss;
-	}
-	else if (dtype == pTypeChime)
-	{
-		dtype = pTypeGeneralSwitch;
-		if (subtype == sTypeByronSX)
-			subtype = sSwitchTypeByronSX;
-		if (subtype == sTypeSelectPlus)
-			subtype = sSwitchTypeSelectPlus;
-		if (subtype == sTypeByronBY)
-			subtype = sSwitchTypeSelectPlus3;
-		if (subtype == sTypeByronMP001)
-			subtype = sSwitchTypeByronMP001;
-	}
-	else if (dtype == pTypeSecurity1)
-	{
-		dtype = pTypeGeneralSwitch;
-		if (subtype == sTypeSecX10)
-			subtype = sSwitchTypeX10secu;
-		if (subtype == sTypeSecX10M)
-			subtype = sSwitchTypeX10secu;
-		if (subtype == sTypeSecX10R)
-			subtype = sSwitchTypeX10secu;
-	}
-	else if (dtype == pTypeHomeConfort)
-	{
-		dtype = pTypeGeneralSwitch;
-		subtype = sSwitchTypeHomeConfort;
-	}
-	else if (dtype == pTypeBlinds)
-	{
-		dtype = pTypeGeneralSwitch;
-		if (subtype == sTypeBlindsT5)
-			subtype = sSwitchTypeBofu;
-		else if (subtype == sTypeBlindsT6)
-			subtype = sSwitchTypeBrel;
-		else if (subtype == sTypeBlindsT7)
-			subtype = sSwitchTypeDooya;
-		else if (subtype == sTypeBlindsT8)
-			subtype = sSwitchTypeBofu;
-		else if (subtype == sTypeBlindsT9)
-			subtype = sSwitchTypeBrel;
-		else if (subtype == sTypeBlindsT10)
-			subtype = sSwitchTypeDooya;
-		std::stringstream s_strid;
-		s_strid << std::hex << strtoul(devid.c_str(), nullptr, 16);
-		unsigned long deviceid = 0;
-		s_strid >> deviceid;
-		deviceid = (unsigned long)((deviceid & 0xffffff00) >> 8);
-		char szTmp[20];
-		sprintf(szTmp, "%08lX", deviceid);
-		//_log.Log(LOG_ERROR, "RFLink: deviceid: %x", deviceid);
-		devid = szTmp;
-	}
-	else if (dtype == pTypeRFY)
-	{
-		dtype = pTypeGeneralSwitch;
-		subtype = sSwitchTypeRTS;
-	}
 }

hardware/ZiBlueBase.h
@@ -20,12 +20,14 @@ class CZiBlueBase : public CDomoticzHardwareBase
 		ZIBLUE_STATE_DATA_ASCII
 	};
 
-      public:
+	public:
 	CZiBlueBase();
 	~CZiBlueBase() override = default;
+	virtual const std::string& GetManualSwitchesJsonConfiguration() const override;
+	virtual void GetManualSwitchParameters(const std::multimap<std::string, std::string> &Parameters, _eSwitchType & SwitchTypeInOut, int &LightTypeInOut,
+					int & dTypeOut, int &dSubTypeOut,std::string &devIDOut, std::string &sUnitOut) const override;
 	bool WriteToHardware(const char *pdata, unsigned char length) override;
-	static void ConvertToGeneralSwitchType(std::string &devid, int &dtype, int &subtype);
-      private:
+	private:
 	void Init();
 	void OnConnected();
 	void OnDisconnected();

hardware/hardwaretypes.h
@@ -196,6 +196,16 @@
 #define sSwitchTypeFaber 0x77
 #define sSwitchTypeDrayton 0x78
 #define sSwitchTypeV2Phoenix 0x79
+#define sSwitchTypeVisonic433 0x80
+#define sSwitchTypeVisonic868 0x81
+#define sSwitchTypeX2D433 0x82
+#define sSwitchTypeX2D868 0x83
+#define sSwitchTypeX2DShutter 0x84
+#define sSwitchTypeX2DElec 0x85
+#define sSwitchTypeX2DGas 0x86
+#define sSwitchTypeParrot 0x87
+#define sSwitchTypeKD101 0x88
+#define sSwitchTypeFS20 0x89
 
 // Switch commands
 #define gswitch_sOff 0x00

main/mainworker.cpp
@@ -11377,10 +11377,6 @@ bool MainWorker::SwitchLightInt(const std::vector<std::string>& sd, std::string 
 	int dType = atoi(sd[3].c_str());
 	int dSubType = atoi(sd[4].c_str());
 	_eSwitchType switchtype = (_eSwitchType)atoi(sd[5].c_str());
-	if (pHardware->HwdType == HTYPE_ZIBLUEUSB || pHardware->HwdType == HTYPE_ZIBLUETCP)
-	{
-		CZiBlueBase::ConvertToGeneralSwitchType(deviceID, dType, dSubType);
-	}
 
 	m_szLastSwitchUser = User;
 

commit 13d9812927d9307616e7f1d34d34786c10a8b637

Fixed crash caused by PR #4923 (Fixed #5128)

main/WebServer.cpp
@@ -3838,13 +3838,15 @@ namespace http
 						std::string Name = sd[1];
 						_eHardwareTypes Type = (_eHardwareTypes)atoi(sd[2].c_str());
 						CDomoticzHardwareBase *pBaseHardware = reinterpret_cast<CDomoticzHardwareBase *>(m_mainworker.GetHardware(ID));
-
-						Json::Value proot;
-						std::string jsonConfiguration = pBaseHardware->GetManualSwitchesJsonConfiguration();
+						std::string jsonConfiguration;
+						if (pBaseHardware != nullptr)
+						{
+							jsonConfiguration = pBaseHardware->GetManualSwitchesJsonConfiguration();
+						}
 						if (!jsonConfiguration.empty())
 						{
+							Json::Value proot;
 							bool res = ParseJSon(jsonConfiguration, proot);
-
 							if (res)
 							{
 								root["result"][ii]["idx"] = ID;