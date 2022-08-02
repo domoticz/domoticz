1dbbf485571cda26642da5f55dc9c86d0b4b80f9
Rob Peters <rob@dvbcontrol.com>
2022-08-02 16:27:47 +0200

    Fixing slider position when opening/closing inverted blinds

--- main/SQLHelper.cpp
+++ main/SQLHelper.cpp
@@ -5306,10 +5306,23 @@ uint64_t CSQLHelper::UpdateValueInt(
 				|| (switchtype == STYPE_BlindsPercentageInvertedWithStop)
 				)
 			{
-				if (nValue == light2_sOn)
-					llevel = 100;
-				else if (nValue == light2_sOff)
-					llevel = 0;
+				if (
+					(switchtype != STYPE_BlindsPercentageInverted)
+					&& (switchtype != STYPE_BlindsPercentageInvertedWithStop)
+					)
+				{
+					if (nValue == light2_sOn)
+						llevel = 100;
+					else if (nValue == light2_sOff)
+						llevel = 0;
+				}
+				else
+				{
+					if (nValue == light2_sOn)
+						llevel = 0;
+					else if (nValue == light2_sOff)
+						llevel = 100;
+				}
 
 				//update level for device
 				safe_query(
