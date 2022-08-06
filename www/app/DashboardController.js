04283e63a79d177c7b498a237891bf7457486138
ajarzyn <18590439+ajarzyn@users.noreply.github.com>
2022-08-07 01:16:32 +0200

    Thermostat setpoint - change spaces to tabs.

--- www/app/DashboardController.js
+++ www/app/DashboardController.js
@@ -1457,7 +1457,7 @@ define(['app', 'livesocket'], function (app) {
 						else if ((item.Type == "Thermostat") && (item.SubType == "SetPoint")) {
 							status = "";
 							bigtext = item.Data + '\u00B0 ' + $scope.config.TempSign;
-                            $(id + " #img").attr('onclick', 'ShowSetpointPopup(event, ' + item.idx + ', ' + item.Protected + ', ' + item.Data + ')');
+							$(id + " #img").attr('onclick', 'ShowSetpointPopup(event, ' + item.idx + ', ' + item.Protected + ', ' + item.Data + ')');
 						}
 						else if (item.SubType == "Smartwares") {
 							status = item.Data + '\u00B0 ' + $scope.config.TempSign;
