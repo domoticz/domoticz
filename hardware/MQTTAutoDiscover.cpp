0f54b27bb680301f8346165bab22a3671999a412
heggink <herman.eggink@outlook.com>
2023-08-23 22:12:06 +0200

    Update MQTTAutoDiscover.cpp
    Add ignoring condition where a lock receives a state that is mapped in the value template

--- hardware/MQTTAutoDiscover.cpp
+++ hardware/MQTTAutoDiscover.cpp
@@ -3451,6 +3451,10 @@ void MQTTAutoDiscover::InsertUpdateSwitch(_tMQTTASensor* pSensor)
 				szSwitchCmd = "off";
 			else if (pSensor->component_type == "lock" && szSwitchCmd == pSensor->state_locked)
 				szSwitchCmd = "on";
+			else if (pSensor->component_type == "lock" && !pSensor->value_template.empty())
+				// ignore states defined in the value template until valu_telplate support is built in
+				return;
+
 			else
 			{
 				if (level == 0)
