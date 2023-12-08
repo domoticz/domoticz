548639bb0b1d18d949a75e271dc0f93f53df0a19
Rob Peters <Info@Domoticz.com>
2023-12-08 07:55:47 +0100

    Added extra functionality

--- hardware/CounterHelper.h
+++ hardware/CounterHelper.h
@@ -11,6 +11,9 @@ public:
 	~CounterHelper();
 	void Init(const std::string &szUservariableName, CDomoticzHardwareBase *pHardwareBase);
 	void SendKwhMeter(int NodeID, int ChildID, int BatteryLevel, double musage, double mtotal, const std::string& defaultname, int RssiLevel = 12);
+	double SetCounterValue(const double nNewCounbterValue);
+	double GetCounterValue() const { return m_nLastCounterValue; };
+	void Reset();
 private:
 	std::string m_szUservariableName;
 	CDomoticzHardwareBase* m_pHardwareBase = nullptr;
