844de41940e939a15477dff077ad1f3b4405f0e9
Rob Peters <Info@Domoticz.com>
2024-01-02 08:26:13 +0100

    Upgrading AUTH version to make previous version/installation fail

--- tcpserver/TCPClient.cpp
+++ tcpserver/TCPClient.cpp
@@ -47,7 +47,7 @@ namespace tcp {
 				{
 					std::string recstr;
 					recstr.append(buffer_.data(), bytes_transferred);
-					if (recstr.find("AUTH") != std::string::npos)
+					if (recstr.find("AUTHv2") == 0)
 					{
 						//Authentication
 						std::vector<std::string> strarray;
