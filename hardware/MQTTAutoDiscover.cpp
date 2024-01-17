bf4990aad4821856bce91718146435b7d461e5bb
Rob Peters <Info@Domoticz.com>
2024-01-17 07:20:22 +0100

    MQTT-AD: Setup, changed device description

--- hardware/MQTTAutoDiscover.cpp
+++ hardware/MQTTAutoDiscover.cpp
@@ -4738,7 +4738,7 @@ void MQTTAutoDiscover::GetConfig(Json::Value& root)
 			root["result"][ii]["idx"] = itt.first;
 
 			_tMQTTADevice* pDevice = &m_discovered_devices[itt.second.device_identifiers];
-			root["result"][ii]["dev_name"] = (pDevice != nullptr) ? pDevice->name : "?";
+			root["result"][ii]["dev_name"] = itt.second.device_identifiers;
 			root["result"][ii]["name"] = itt.second.name;
 			root["result"][ii]["value"] = itt.second.last_value;
 			root["result"][ii]["unit"] = itt.second.unit_of_measurement;
