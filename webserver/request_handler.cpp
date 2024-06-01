305fec76c244c77ba7a14292cabc4356276bbaeb
Rob Peters <Info@Domoticz.com>
2024-06-01 13:31:39 +0200

    Also add /Custom to the not cached pages

--- webserver/request_handler.cpp
+++ webserver/request_handler.cpp
@@ -481,6 +481,7 @@ void request_handler::handle_request(const request &req, reply &rep, modify_info
 		|| (req.uri.find("views/") != std::string::npos)
 		|| (req.uri.find("js/domoticz") != std::string::npos)
 		|| (req.uri.find("/templates/") != std::string::npos)
+		|| (req.uri.find("/Custom/") != std::string::npos)
 		|| (!bDoCachePages)
 		)
 	{
