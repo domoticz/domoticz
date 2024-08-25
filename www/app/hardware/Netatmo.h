3acf967ba9672c8f58844f7d83dea054f6331459
Louk Kamp <lucky_louk@hotmail.com>
2024-08-25 17:28:19 +0200

    Redo all changes without extern trees

---
+++ www/app/hardware/Netatmo.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "DomoticzHardware.h"
+#include "../main/BaroForecastCalculator.h"
+
+namespace Json
+{
+	class Value;
+} // namespace Json
+
+class CNetatmo : public CDomoticzHardwareBase
+{
+      public:
+	CNetatmo(int ID, const std::string &username, const std::string &password);
+	~CNetatmo() override = default;
+
+	bool WriteToHardware(const char *, unsigned char) override;
+	void SetSetpoint(int idx, float temp);
+	bool SetProgramState(int idx, int newState);
+
+      private:
+	enum _eNetatmoType
+	{
+		NETYPE_WEATHER_STATION = 0,
+		NETYPE_HOMECOACH,
+		NETYPE_ENERGY
+	};
+	std::string m_clientId;
+	std::string m_clientSecret;
+	std::string m_scopes;
+	std::string m_redirectUri;
+	std::string m_authCode;
+	std::string m_username;
+	std::string m_password;
+	std::string m_accessToken;
+	std::string m_refreshToken;
+	std::map<int, std::string> m_thermostatDeviceID;
+	std::map<int, std::string> m_thermostatModuleID;
+	bool m_bPollThermostat;
+	bool m_bPollWeatherData;
+	bool m_bFirstTimeThermostat;
+	bool m_bFirstTimeWeatherData;
+	bool m_bForceSetpointUpdate;
+	time_t m_tSetpointUpdateTime;
+
+	std::shared_ptr<std::thread> m_thread;
+
+	time_t m_nextRefreshTs;
+
+	std::map<int, float> m_RainOffset;
+	std::map<int, float> m_OldRainCounter;
+
+	std::map<int, bool> m_bNetatmoRefreshed;
+
+	void Init();
+	bool StartHardware() override;
+	bool StopHardware() override;
+	void Do_Work();
+	std::string MakeRequestURL(_eNetatmoType NetatmoType);
+	void GetMeterDetails();
+	void GetThermostatDetails();
+	bool ParseStationData(const std::string &sResult, bool bIsThermostat);
+	bool ParseHomeData(const std::string &sResult);
+	bool ParseHomeStatus(const std::string &sResult);
+	bool SetAway(int idx, bool bIsAway);
+	bool SetSchedule(int scheduleId);
+
+	bool Login();
+	bool RefreshToken(bool bForce = false);
+	bool LoadRefreshToken();
+	void StoreRefreshToken();
+	void StoreRequestTokenFlag(bool bFlag = false);
+	bool m_isLogged;
+	bool m_bForceLogin;
+	_eNetatmoType m_weatherType;
+	_eNetatmoType m_energyType;
+
+	int m_ActHome;
+	std::string m_Home_ID;
+	std::string m_ThermostatName;
+	std::map<std::string, std::string> m_RoomNames;
+	std::map<std::string, int> m_RoomIDs;
+	std::map<std::string, std::string> m_ModuleNames;
+	std::map<std::string, int> m_ModuleIDs;
+	std::map<int, std::string> m_ScheduleNames;
+	std::map<int, std::string> m_ScheduleIDs;
+	int m_selectedScheduleID;
+
+	std::map<int, CBaroForecastCalculator> m_forecast_calculators;
+
+	int GetBatteryLevel(const std::string &ModuleType, int battery_percent);
+	bool ParseDashboard(const Json::Value &root, int DevIdx, int ID, const std::string &name, const std::string &ModuleType, int battery_percent, int rf_status);
+};
