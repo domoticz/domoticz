442f1ec9396ea2f8036feca5cf14695fd88556ec
Rob Peters <Info@Domoticz.com>
2024-12-31 13:20:03 +0100

    Changes HTML Sanitizer

--- main/HTMLSanitizer.cpp
+++ main/HTMLSanitizer.cpp
@@ -5,7 +5,9 @@
 namespace
 {
 	// https://html5sec.org/
-	const auto szForbiddenContent = std::array<std::string, 31>{
+	const auto szForbiddenContent = std::array<std::string, 23>{
+		"a",
+		"span",
 		"script",
 		"style",
 		"svg",
@@ -16,7 +18,6 @@ namespace
 		"template",
 		"form",
 		"input",
-		"onerror",
 		"frame",
 		"img",
 		"marquee",
@@ -25,106 +26,55 @@ namespace
 		"embed",
 		"math",
 		"href",
-		"onfocus",
-		"onresize",
-		"onactivate",
-		"onscroll",
-		"onwebkittransitionend",
-		"onanimationstart",
-		"ontoggle",
 		"details",
 		"table",
 		"alert",
-		"onerror",
-		"onmouseover",
 	};
 } // namespace
 
-//Maybe the way we search for 'safe' words is to safe, but better safe then sorry
+//Maybe the way we search for 'safe' words is to safe, but better safe than sorry
 std::string HTMLSanitizer::Sanitize(const std::string& szText)
 {
+	if (szText.empty())
+		return szText;
+
 	std::string ret;
 	std::string tmpstr(szText);
-
-	stdreplace(tmpstr, "<", "");
-	stdreplace(tmpstr, ">", "");
 
 	do
 	{
 		size_t pos_start = tmpstr.find('<');
 		if (pos_start == std::string::npos)
 		{
+			//Done
 			ret += tmpstr;
-			return ret;
+			break;
 		}
+		std::string tag = tmpstr.substr(pos_start);
+		tmpstr = tmpstr.substr(0, pos_start);
+		ret += tmpstr;
 
-		ret += tmpstr.substr(0, pos_start);
-		tmpstr = tmpstr.substr(pos_start);
-
-		size_t pos_end = tmpstr.find('>');
-		if (pos_end == std::string::npos)
+		pos_start = tag.find(' ');
+		if (pos_start == std::string::npos)
 		{
-			ret += tmpstr;
-			return ret;
+			//Done
+			ret += tag;
+			break;
 		}
+		tmpstr = tag.substr(pos_start);
+		tag = tag.substr(1, pos_start - 1);
 
-		std::string tag = tmpstr.substr(0, pos_end + 1);
-		std::string org_tag(tag);
-		tmpstr = tmpstr.substr(pos_end + 1);
-
-		//See if we have a forbidden tag, if yes remove it
-		stdlower(tag);
-
-		bool bHaveForbiddenTag = std::any_of(szForbiddenContent.begin(), szForbiddenContent.end(), [&](const std::string &content) { return tag.find(content) != std::string::npos; });
+		bool bHaveForbiddenTag = false;
+		if (!tag.empty())
+		{
+			//See if we have a forbidden tag, if yes remove it
+			stdlower(tag);
+			bHaveForbiddenTag = std::any_of(szForbiddenContent.begin(), szForbiddenContent.end(), [&](const std::string& content) { return tag == content; });
+		}
 		if (!bHaveForbiddenTag)
-			ret += org_tag;
+			ret += "<" + tag;
 	} while (true);
-	//will never be reached
+
 	return ret;
 }
 
-std::wstring HTMLSanitizer::Sanitize(const std::wstring& szText)
-{
-	std::wstring ret;
-	std::wstring tmpstr(szText);
-
-	stdreplace(tmpstr, L"<", L"");
-	stdreplace(tmpstr, L">", L"");
-
-	do
-	{
-		size_t pos_start = tmpstr.find('<');
-		if (pos_start == std::wstring::npos)
-		{
-			ret += tmpstr;
-			return ret;
-		}
-
-		ret += tmpstr.substr(0, pos_start);
-		tmpstr = tmpstr.substr(pos_start);
-
-		size_t pos_end = tmpstr.find('>');
-		if (pos_end == std::wstring::npos)
-		{
-			ret += tmpstr;
-			return ret;
-		}
-
-		std::wstring tag = tmpstr.substr(0, pos_end + 1);
-		std::wstring org_tag(tag);
-		tmpstr = tmpstr.substr(pos_end + 1);
-
-		//See if we have a forbidden tag, if yes remove it
-		stdlower(tag);
-
-		bool bHaveForbiddenTag = std::any_of(szForbiddenContent.begin(), szForbiddenContent.end(), [&](const std::string &content) {
-			std::wstring wsTmp(content.begin(), content.end());
-			return tag.find(wsTmp) != std::wstring::npos;
-		});
-
-		if (!bHaveForbiddenTag)
-			ret += org_tag;
-	} while (true);
-	//will never be reached
-	return ret;
-}
