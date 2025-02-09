754395b31b0ae15fe891cbb639459bc4deacc127
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2025-02-09 20:43:39 +0100

    add SetDimmerState

--- hardware/Netatmo.h
+++ hardware/Netatmo.h
@@ -47,6 +47,7 @@ class CNetatmo : public CDomoticzHardwareBase
         bool WriteToHardware(const char *, unsigned char) override;
         void SetSetpoint(unsigned long ID, const float temp);
         bool SetProgramState(const int uid, const int newState);
+	bool SetDimmerState(const int uid, const int newState);
         void Get_Respons_API(const m_eNetatmoType& NType, std::string& sResult, std::string& home_id, bool& bRet, Json::Value& root, std::string extra_data);
 
       private:
