67dfeffcd2414b84efb4c5305ec9c199e530df33
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2025-04-08 22:27:57 +0200

    Update Typo

--- hardware/Netatmo.h
+++ hardware/Netatmo.h
@@ -110,11 +110,11 @@ class CNetatmo : public CDomoticzHardwareBase
 	void Get_Picture();
 	void Get_Measure(std::string gateway, std::string module_id, std::string scale, std::string type);
 	void Get_Events(std::string home_id, std::string device_types, std::string event_id, std::string person_id, std::string device_id, std::string module_id, int offset, int size, std::string locale);
-	void Get_Scenarios(std::string home_id, Json::Value& scenarios);
+	void Get_Scenarios(std::string& home_id, Json::Value& scenarios);
 
-	bool ParseScenarios(const std::string& sResult, Json::Value& scenarios, std::string home_id);
-	bool ParseStationData(const std::string &sResult, bool bIsThermostat);
-	bool ParseHomeStatus(const std::string &sResult, Json::Value& root, std::string& home_id);
+	bool ParseScenarios(const std::string& sResult, Json::Value& scenarios, std::string& home_id);
+	bool ParseStationData(const std::string& sResult, bool bIsThermostat);
+	bool ParseHomeStatus(const std::string& sResult, Json::Value& root, std::string& home_id);
 	bool ParseEvents(const std::string& sResult, Json::Value& root );
 
 	bool SetAway(int idx, bool bIsAway);
