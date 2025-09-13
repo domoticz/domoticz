2003bb1433e133237169dcdaebe59c83c70a6dda
kiddigital <kiddigital@gmail.com>
2025-09-13 20:39:22 +0200

    Code moved to own scope and cleaned

--- mcpserver/McpService.hpp
+++ mcpserver/McpService.hpp
@@ -7,8 +7,16 @@
 
 namespace mcp
 {
-    bool getSensorValue(const Json::Value &jsonRequest, Json::Value &jsonRPCRep, const Json::Value &jsonDevices);
-    bool toggleSwitchState(const Json::Value &jsonRequest, Json::Value &jsonRPCRep, const Json::Value &jsonDevices);
-    bool getSwitchState(const Json::Value &jsonRequest, Json::Value &jsonRPCRep, const Json::Value &jsonDevices);
-    bool getDeviceByName(const std::string &sDeviceName, Json::Value &device, const Json::Value &jsonDevices);
+	void McpInitialize(const Json::Value &jsonRequest, Json::Value &jsonRPCRep);
+	void McpToolsList(const Json::Value &jsonRequest, Json::Value &jsonRPCRep);
+	void McpToolsCall(const Json::Value &jsonRequest, Json::Value &jsonRPCRep);
+	void McpResourcesList(const Json::Value &jsonRequest, Json::Value &jsonRPCRep);
+	void McpResourcesRead(const Json::Value &jsonRequest, Json::Value &jsonRPCRep);
+	void McpPromptsList(const Json::Value &jsonRequest, Json::Value &jsonRPCRep);
+	void McpPromptsGet(const Json::Value &jsonRequest, Json::Value &jsonRPCRep);
+
+	bool getSensorValue(const Json::Value &jsonRequest, Json::Value &jsonRPCRep, const Json::Value &jsonDevices);
+	bool toggleSwitchState(const Json::Value &jsonRequest, Json::Value &jsonRPCRep, const Json::Value &jsonDevices);
+	bool getSwitchState(const Json::Value &jsonRequest, Json::Value &jsonRPCRep, const Json::Value &jsonDevices);
+	bool getDeviceByName(const std::string &sDeviceName, Json::Value &device, const Json::Value &jsonDevices);
 }
