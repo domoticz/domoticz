domoticz/domoticz

commit 8451daec5f6a8567f92c38ed98298c1b5a89c12f

Corrected Voltage Setting

www/app/SetupController.js
@@ -379,8 +379,7 @@ define(['app'], function (app) {
 										listExtra.push({"idx": item.idx, "name": item.Name});
 									}
 									else if (item.SubType == "Voltage") {
-										listBatterySoc.push({"idx": item.idx, "name": item.Name});
-										listExtra.push({"idx": item.idx, "name": item.Name});
+										listBatteryVolt.push({"idx": item.idx, "name": item.Name});
 									}
 									else if (item.SubType == "Text") {
 										listText.push({"idx": item.idx, "name": item.Name});
@@ -410,6 +409,7 @@ define(['app'], function (app) {
 						listSolar.sort((a,b) => (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : ((b.name.toLowerCase() > a.name.toLowerCase()) ? -1 : 0));
 						listBatteryWatt.sort((a,b) => (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : ((b.name.toLowerCase() > a.name.toLowerCase()) ? -1 : 0));
 						listBatterySoc.sort((a,b) => (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : ((b.name.toLowerCase() > a.name.toLowerCase()) ? -1 : 0));
+						listBatteryVolt.sort((a,b) => (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : ((b.name.toLowerCase() > a.name.toLowerCase()) ? -1 : 0));
 						listText.sort((a,b) => (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : ((b.name.toLowerCase() > a.name.toLowerCase()) ? -1 : 0));
 						listExtra.sort((a,b) => (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : ((b.name.toLowerCase() > a.name.toLowerCase()) ? -1 : 0));
 						listTemperatureSensors.sort((a,b) => (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : ((b.name.toLowerCase() > a.name.toLowerCase()) ? -1 : 0));

commit 52d2212c5e1d90cff59f6f67957694efec5c216b

fix: restore Voltage devices in Energy Dashboard custom widget dropdowns (Fixes #6630)
Commit 8451daec5 accidentally removed the listExtra.push when moving
Voltage from listBatterySoc to listBatteryVolt, causing Voltage devices
to disappear from the Custom Widget dropdown menus.

www/app/SetupController.js
@@ -380,6 +380,7 @@ define(['app'], function (app) {
 									}
 									else if (item.SubType == "Voltage") {
 										listBatteryVolt.push({"idx": item.idx, "name": item.Name});
+										listExtra.push({"idx": item.idx, "name": item.Name});
 									}
 									else if (item.SubType == "Text") {
 										listText.push({"idx": item.idx, "name": item.Name});