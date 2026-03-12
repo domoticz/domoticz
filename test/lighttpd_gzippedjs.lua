3b1b79afa58ff2258079963bc60949af9555b388
GizMoCuz <info@domoticz.com>
2026-03-12 21:20:02 +0100

    Remove test folder from branch
    The test/ directory was added on this branch but is not part of the
    development branch. Remove it to keep the branch clean.

--- test/lighttpd_gzippedjs.lua
+++
@@ -1,4 +0,0 @@
-if (string.match(lighty.env["physical.path"],".js.gz")) then
-	lighty.header["Content-Type"] = "text/javascript"
-	lighty.header["Content-Encoding"] = "gzip"
-end
--- test/lighttpd_gzippedjson.lua
+++
@@ -1,4 +0,0 @@
-if (string.match(lighty.env["physical.path"],".json.gz")) then
-	lighty.header["Content-Type"] = "application/json;charset=UTF-8"
-	lighty.header["Content-Encoding"] = "gzip"
-end
