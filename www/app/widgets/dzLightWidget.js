971a84c7e8cab315dde22a4268e1468193ae7c96
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2026-04-05 17:09:30 +0200

    Remove 'Orcon' subtype check

--- www/app/widgets/dzLightWidget.js
+++ www/app/widgets/dzLightWidget.js
@@ -382,7 +382,6 @@ define(['app'], function (app) {
                     // Fan subtypes always show Fan48_On.png
                     if (device.SubType && (
                         device.SubType.indexOf('Itho') == 0 ||
-                        device.SubType.indexOf('Orcon') == 0 ||
                         device.SubType.indexOf('Lucci') == 0 ||
                         device.SubType.indexOf('Falmec') == 0 ||
                         device.SubType.indexOf('Westinghouse') == 0
@@ -500,7 +499,7 @@ define(['app'], function (app) {
                     if (!ctrl.isClickable()) return;
 
                     // Fan subtypes - show specialized popups
-                    if (device.SubType && (device.SubType.indexOf('Itho') == 0 || device.SubType.indexOf('Orcon') == 0)) {
+                    if (device.SubType && device.SubType.indexOf('Itho') == 0) {
                         ShowIthoPopup($event || event, device.idx, device.Protected, window.myglobals.ismobile);
                         return;
                     }
